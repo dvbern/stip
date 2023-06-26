683e60d37288eb423fba291c144d2b2065b12f6b
nefr <frederic.nell@dvbern.ch>
2023-06-26 13:23:48 +0200

    KSTIP-123: Aufschaltungsdatum sollte im Juni anstatt July sein fuer die erste Periode

--- src/main/resources/db/migration/V0001__initDaten.sql
+++ src/main/resources/db/migration/V0001__initDaten.sql
@@ -18,7 +18,7 @@
 INSERT INTO gesuchsperiode (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
                                  gueltig_ab, gueltig_bis, einreichfrist, aufschaltdatum)
 VALUES ('3fa85f64-5717-4562-b3fc-2c963f66afa6', '2023-05-31 08:35:52', '2023-05-30 08:35:43', 'Admin', 'Admin', 0,
-        '2023-08-01', '2024-06-30', '2023-12-31', '2023-07-01');
+        '2023-08-01', '2024-06-30', '2023-12-31', '2023-06-01');
 
 INSERT INTO gesuchsperiode (id, timestamp_erstellt, timestamp_mutiert, user_erstellt, user_mutiert, version,
                                  gueltig_ab, gueltig_bis, einreichfrist, aufschaltdatum)
