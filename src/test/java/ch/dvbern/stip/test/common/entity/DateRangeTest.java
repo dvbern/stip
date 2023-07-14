46448740d7853eb4995f6819135af588e31f9d1e
nefr <frederic.nell@dvbern.ch>
2023-07-14 14:30:48 +0200

    KSTIP-295: Test fehlen für DateRange

---
+++ src/test/java/ch/dvbern/stip/test/common/entity/DateRangeTest.java
@@ -0,0 +1,33 @@
+package ch.dvbern.stip.test.common.entity;
+
+import java.time.LocalDate;
+
+import ch.dvbern.stip.api.common.entity.DateRange;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DateRangeTest {
+	private LocalDate von = LocalDate.of(2022,12,1);
+	private LocalDate bis = LocalDate.of(2023,8,31);
+
+	private DateRange dateRange = new DateRange();
+	private DateRange otherDateRange = new DateRange(von, bis);
+	private DateRange sameOtherDateRange = new DateRange(von, bis);
+	@Test
+	public void testDateRangeEquality(){
+		Assertions.assertEquals(dateRange.equals(otherDateRange), false);
+		Assertions.assertEquals(otherDateRange.equals(sameOtherDateRange), true);
+	}
+
+	@Test
+	public void testDateRangeHashCode(){
+		Assertions.assertNotEquals(dateRange.hashCode(), otherDateRange.hashCode());
+		Assertions.assertEquals(otherDateRange.hashCode(), sameOtherDateRange.hashCode());
+	}
+
+	@Test
+	public void testDateRangeEqualitySameHashCodeEquality(){
+		Assertions.assertEquals(dateRange.equals(otherDateRange), dateRange.hashCode() == otherDateRange.hashCode());
+		Assertions.assertEquals(otherDateRange.equals(sameOtherDateRange), otherDateRange.hashCode() == sameOtherDateRange.hashCode());
+	}
+}
