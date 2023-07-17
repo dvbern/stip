ea37352fa0057937341266f1ccee5bd445697770
nefr <frederic.nell@dvbern.ch>
2023-07-17 15:48:31 +0200

    KSTIP-293: mapping precisiert und update weg von lesen bei GesuchFormular gab es noch einen Design Fehler

--- src/main/java/ch/dvbern/stip/api/ausbildung/service/AusbildungMapper.java
+++ src/main/java/ch/dvbern/stip/api/ausbildung/service/AusbildungMapper.java
@@ -9,18 +9,17 @@ import org.mapstruct.*;
 
 @Mapper(config = MappingConfig.class, uses = DateUtil.class)
 public interface AusbildungMapper {
+    @Mapping(source = "ausbildungsgangId", target = "ausbildungsgang.id")
+    @Mapping(source = "ausbildungsstaetteId", target = "ausbildungsstaette.id")
     @Mapping(source = "ausbildungBegin", target = "ausbildungBegin", qualifiedBy = {MonthYearMapper.class, MonthYearToBeginOfMonth.class})
     @Mapping(source = "ausbildungEnd", target = "ausbildungEnd", qualifiedBy =  {MonthYearMapper.class, MonthYearToEndOfMonth.class})
     Ausbildung toEntity(AusbildungDto ausbildungDto);
 
+    @Mapping(source = "ausbildungsgang.id", target = "ausbildungsgangId")
+    @Mapping(source = "ausbildungsstaette.id", target = "ausbildungsstaetteId")
     @Mapping(source = "ausbildungBegin", target = "ausbildungBegin", qualifiedBy = {MonthYearMapper.class, DateToMonthYear.class})
     @Mapping(source = "ausbildungEnd", target = "ausbildungEnd", qualifiedBy = {MonthYearMapper.class, DateToMonthYear.class})
     AusbildungDto toDto(Ausbildung ausbildung);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(source = "ausbildungBegin", target = "ausbildungBegin", qualifiedBy = {MonthYearMapper.class, MonthYearToBeginOfMonth.class})
-    @Mapping(source = "ausbildungEnd", target = "ausbildungEnd", qualifiedBy = {MonthYearMapper.class, MonthYearToEndOfMonth.class})
-    Ausbildung partialUpdate(AusbildungDto ausbildungDto, @MappingTarget Ausbildung ausbildung);
 
     @Mapping(source = "ausbildungsgangId", target = "ausbildungsgang.id")
     @Mapping(source = "ausbildungsstaetteId", target = "ausbildungsstaette.id")
