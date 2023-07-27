86aa313f147ead477530c5a0c05cac76dcdf9de3
Raphael Gosteli <raphael.gosteli@dvbern.ch>
2023-07-27 09:15:01 +0200

    feat(repo): add requireById

--- src/main/java/ch/dvbern/stip/api/common/repo/BaseRepository.java
+++ src/main/java/ch/dvbern/stip/api/common/repo/BaseRepository.java
@@ -2,8 +2,14 @@ package ch.dvbern.stip.api.common.repo;
 
 import ch.dvbern.stip.api.common.entity.AbstractEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.ws.rs.NotFoundException;
 
 import java.util.UUID;
 
 public interface BaseRepository<T extends AbstractEntity> extends PanacheRepositoryBase<T, UUID> {
+
+    default T requireById(UUID id) {
+        return findByIdOptional(id).orElseThrow(NotFoundException::new);
+    }
+
 }
