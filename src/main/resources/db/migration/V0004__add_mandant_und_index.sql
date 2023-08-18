b2f716d1e14ee1383cc45682dbf2c742771e0f9f
nefr <frederic.nell@dvbern.ch>
2023-08-18 08:39:17 +0200

    feat(KSTIP-333): remove not null and default from audit + add per Skript index fuer Fall

--- src/main/resources/db/migration/V0004__add_mandant_und_index.sql
+++ src/main/resources/db/migration/V0004__add_mandant_und_index.sql
@@ -1,69 +1,70 @@
 ALTER TABLE adresse ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_adresse_mandant_id ON adresse (mandant);
-ALTER TABLE adresse_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE adresse_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE ausbildung ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_ausbildung_mandant_id ON ausbildung (mandant);
-ALTER TABLE ausbildung_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE ausbildung_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE ausbildungsgang ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_ausbildungsgang_mandant_id ON ausbildungsgang (mandant);
-ALTER TABLE ausbildungsgang_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE ausbildungsgang_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE ausbildungsstaette ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_ausbildungsstaette_mandant_id ON ausbildungsstaette (mandant);
-ALTER TABLE ausbildungsstaette_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE ausbildungsstaette_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE auszahlung ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_auszahlung_mandant_id ON auszahlung (mandant);
-ALTER TABLE auszahlung_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE auszahlung_aud ADD COLUMN mandant VARCHAR(255);
 
 CREATE INDEX IF NOT EXISTS IX_benutzer_mandant_id ON benutzer (mandant);
+CREATE INDEX IF NOT EXISTS IX_fall_mandant_id ON fall (mandant);
 
 ALTER TABLE dokument ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_dokument_mandant_id ON dokument (mandant);
-ALTER TABLE dokument_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE dokument_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE eltern ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_eltern_mandant_id ON eltern (mandant);
-ALTER TABLE eltern_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE eltern_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE familiensituation ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_familiensituation_mandant_id ON familiensituation (mandant);
-ALTER TABLE familiensituation_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE familiensituation_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE geschwister ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_geschwister_mandant_id ON geschwister (mandant);
-ALTER TABLE geschwister_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE geschwister_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE gesuch ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_gesuch_mandant_id ON gesuch (mandant);
-ALTER TABLE gesuch_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE gesuch_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE gesuch_dokument ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_gesuch_dokument_mandant_id ON gesuch_dokument (mandant);
-ALTER TABLE gesuch_dokument_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE gesuch_dokument_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE gesuch_formular ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_gesuch_formular_mandant_id ON gesuch_formular (mandant);
-ALTER TABLE gesuch_formular_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE gesuch_formular_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE gesuchsperiode ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_gesuchsperiode_mandant_id ON gesuchsperiode (mandant);
-ALTER TABLE gesuchsperiode_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE gesuchsperiode_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE kind ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_kind_mandant_id ON kind (mandant);
-ALTER TABLE kind_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE kind_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE lebenslauf_item ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_lebenslauf_item_mandant_id ON lebenslauf_item (mandant);
-ALTER TABLE lebenslauf_item_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE lebenslauf_item_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE partner ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_partner_mandant_id ON partner (mandant);
-ALTER TABLE partner_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE partner_aud ADD COLUMN mandant VARCHAR(255);
 
 ALTER TABLE person_in_ausbildung ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
 CREATE INDEX IF NOT EXISTS IX_person_in_ausbildung_mandant_id ON person_in_ausbildung (mandant);
-ALTER TABLE person_in_ausbildung_aud ADD COLUMN mandant VARCHAR(255) NOT NULL DEFAULT 'bern';
+ALTER TABLE person_in_ausbildung_aud ADD COLUMN mandant VARCHAR(255);
