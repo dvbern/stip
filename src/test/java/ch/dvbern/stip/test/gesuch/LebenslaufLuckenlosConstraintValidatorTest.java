15c3974b0312989bf68cf91c80c919e7c994fea0
nefr <frederic.nell@dvbern.ch>
2023-08-23 10:07:13 +0200

    feat(KSTIP-336): rollback

--- src/test/java/ch/dvbern/stip/test/gesuch/LebenslaufLuckenlosConstraintValidatorTest.java
+++ src/test/java/ch/dvbern/stip/test/gesuch/LebenslaufLuckenlosConstraintValidatorTest.java
@@ -10,8 +10,6 @@ import ch.dvbern.stip.api.gesuch.entity.LebenslaufLuckenlosConstraintValidator;
 import ch.dvbern.stip.api.lebenslauf.entity.LebenslaufItem;
 import ch.dvbern.stip.api.personinausbildung.entity.PersonInAusbildung;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -43,8 +41,7 @@ class LebenslaufLuckenlosConstraintValidatorTest {
 				, is(true));
 	}
 
-	@ParameterizedTest
-	@ValueSource
+	@Test
 	void lebenslaufLuckenlosStartZuFruehTest() {
 		GesuchFormular gesuchFormular = initFormular();
 		LebenslaufItem lebenslaufItem = new LebenslaufItem();
