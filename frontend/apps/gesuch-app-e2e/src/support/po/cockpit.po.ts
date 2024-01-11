ec624e00e0371429d5d9532fd048cbf4f6a03e26
Severin Spoerri <severin.spoerri@dvbern.ch>
2024-01-11 11:02:05 +0100

    feat(KSTIP-551): added tests

--- frontend/apps/gesuch-app-e2e/src/support/po/cockpit.po.ts
+++ frontend/apps/gesuch-app-e2e/src/support/po/cockpit.po.ts
@@ -8,10 +8,15 @@ export namespace CockpitPO {
   export const getGesuchEdit = () => cy.getBySel('cockpit-gesuch-edit');
   export const getGesuchRemove = () => cy.getBySel('cockpit-gesuch-remove');
   export const getNavDashboard = () => cy.getBySel('cockpit-nav-dashboard');
+  export const getGesuchNew = () => cy.getBySel('cockpit-gesuch-new');
 
   export const openGesuch = () => {
     CockpitPO.getGesuchEdit().first().click();
     getStepTitle().should('contain.text', 'Person in Ausbildung');
     SharedPersonInAusbildungPO.getFormPersonName().should('exist');
   };
+
+  // export const newGesuch = () => {
+  //   CockpitPO.getGesuchNew().click();
+  // };
 }
--- frontend/libs/shared/util-fn/e2e-helpers/src/lib/page-objects/form.po.ts
+++ frontend/libs/shared/util-fn/e2e-helpers/src/lib/page-objects/form.po.ts
@@ -1,2 +1,8 @@
 export const getSubmitButton = (subject?: HTMLElement) =>
   cy.get('button[type=submit]', { withinSubject: subject });
+
+export const getFormControlFactory = (formControlName: string) => {
+  return () => {
+    return cy.getBySel(formControlName);
+  };
+};
