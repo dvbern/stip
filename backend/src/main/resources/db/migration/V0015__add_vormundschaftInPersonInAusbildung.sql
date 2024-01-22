b2b7d97dbf8812d1361a0731b9fcf9694508b7fa
Juri Furer <juri.furer@dvbern.ch>
2024-01-22 16:55:39 +0000

    fix: remove not null and default value from audit table

--- backend/src/main/resources/db/migration/V0015__add_vormundschaftInPersonInAusbildung.sql
+++ backend/src/main/resources/db/migration/V0015__add_vormundschaftInPersonInAusbildung.sql
@@ -1,2 +1,2 @@
 ALTER TABLE person_in_ausbildung  ADD COLUMN vormundschaft BOOLEAN NOT NULL DEFAULT TRUE;
-ALTER TABLE person_in_ausbildung_aud  ADD COLUMN vormundschaft BOOLEAN NOT NULL DEFAULT TRUE;
+ALTER TABLE person_in_ausbildung_aud  ADD COLUMN vormundschaft BOOLEAN;
