b7a627d6d061d746b24cc071b9a7ba8646a43eb9
Severin Spoerri <severin.spoerri@dvbern.ch>
2024-02-02 08:34:08 +0100

    feat(KSTIP-774): new test setup

--- frontend/apps/gesuch-app-e2e/src/stip.setup.ts
+++ frontend/apps/gesuch-app-e2e/src/stip.setup.ts
@@ -1,25 +1,67 @@
-import { expect, test as setup } from '@playwright/test';
+import { test as setup } from '@playwright/test';
+import { addMilliseconds } from 'date-fns';
 
 import { STIP_STORAGE_STATE } from '../playwright.config';
+
+interface KeycloakResponse {
+  access_token: string;
+  expires_in: number;
+  refresh_expires_in: number;
+  refresh_token: string;
+  token_type: string;
+  id_token: string;
+  'not-before-policy': number;
+  session_state: string;
+  scope: string;
+}
+
+export const BEARER_COOKIE = 'access_cookie';
+export const REFRESH_COOKIE = 'refresh_cookie';
 
 setup('authenticate', async ({ page }) => {
   const username = process.env['E2E_USERNAME'] || 'e2e';
   const password = process.env['E2E_PASSWORD'] || 'NKsA.pu*ros6LmDu';
 
-  // Perform authentication steps. Replace these actions with your own.
   await page.goto('');
-  // await page.waitForURL('https://dev-auth-stip.apps.mercury.ocp.dvbern.ch');
   await page.getByLabel('Username or email').fill(username);
   await page.getByLabel('Password', { exact: true }).fill(password);
+
+  const responsePromise = page.waitForResponse(
+    'https://dev-auth-stip.apps.mercury.ocp.dvbern.ch/realms/bern/protocol/openid-connect/token',
+  );
+
   await page.getByRole('button', { name: 'Sign In' }).click();
-  // Wait until the page receives the cookies.
-  //
-  // Sometimes login flow sets cookies in the process of several redirects.
-  // Wait for the final URL to ensure that the cookies are actually set.
-  // await page.waitForURL('https://github.com/');
-  // await page.waitForURL('/gesuch-app-feature-cockpit');
-  // Alternatively, you can wait until the page reaches a state where all cookies are set.
-  await expect(page.locator('h1')).toContainText('Guten Morgen');
+
+  const response = await responsePromise;
+  const body: KeycloakResponse = await response.json();
+
+  // cookies want expiry in seconds as unix time
+  const unixTime =
+    addMilliseconds(Date.now(), body.expires_in).getTime() / 1000;
+
+  await page.context().addCookies([
+    {
+      name: BEARER_COOKIE,
+      value: body.access_token,
+      domain: 'dev-auth-stip.apps.mercury.ocp.dvbern.ch',
+      path: '/realms/bern/',
+      expires: unixTime,
+      httpOnly: false,
+      secure: true,
+      sameSite: 'Lax',
+    },
+    {
+      name: REFRESH_COOKIE,
+      value: body.refresh_token,
+      domain: 'dev-auth-stip.apps.mercury.ocp.dvbern.ch',
+      path: '/realms/bern/',
+      expires: -1,
+      httpOnly: false,
+      secure: true,
+      sameSite: 'Lax',
+    },
+  ]);
+
   // End of authentication steps.
   await page.context().storageState({ path: STIP_STORAGE_STATE });
 });
