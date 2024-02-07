e42f6b4f80d44612e950b619fb45e248b382e320
Juri Furer <juri.furer@dvbern.ch>
2024-02-07 10:05:26 +0100

    feat(KSTIP-587): implement backend

---
+++ backend/src/main/java/ch/dvbern/stip/api/ausbildung/entity/AusbildungEndDateMustBeAfterStartConstraint.java
@@ -0,0 +1,21 @@
+package ch.dvbern.stip.api.ausbildung.entity;
+
+import ch.dvbern.stip.api.personinausbildung.entity.LandCHRequiredConstraintValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+import static ch.dvbern.stip.api.common.validation.ValidationsConstant.VALIDATION_AUSBILDUNG_ENDDATE_AFTER_STARTDATE;
+
+@Target({ ElementType.TYPE, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = AusbildungEndDateMustBeAfterStartConstraintValidator.class)
+@Documented
+public @interface AusbildungEndDateMustBeAfterStartConstraint {
+    String message() default VALIDATION_AUSBILDUNG_ENDDATE_AFTER_STARTDATE;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
