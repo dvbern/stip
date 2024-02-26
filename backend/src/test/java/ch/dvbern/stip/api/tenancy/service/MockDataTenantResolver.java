e252722c7b0ced2fcea5cb07c0b22b782b8df1df
Juri Furer <juri.furer@dvbern.ch>
2024-02-26 09:12:01 +0100

    fix(KSTIP-860): add missing annotation

--- backend/src/test/java/ch/dvbern/stip/api/tenancy/service/MockDataTenantResolver.java
+++ backend/src/test/java/ch/dvbern/stip/api/tenancy/service/MockDataTenantResolver.java
@@ -1,10 +1,12 @@
 package ch.dvbern.stip.api.tenancy.service;
 
+import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.test.Mock;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @Mock
 @ApplicationScoped
+@PersistenceUnitExtension
 public class MockDataTenantResolver extends DataTenantResolver{
     public MockDataTenantResolver() {
         super(null);
