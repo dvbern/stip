349241df46b8953b6efeb776d91a1c6cc7fce074
luca.fondo <luca.fondo@dvbern.ch>
2024-03-05 12:50:26 +0100

    feat(KSTIP-188): Implement CRUD operations in AusbildungsstaetteResource
    Added Create, Read, Update, and Delete methods to AusbildungsstaetteResource implementation. It uses AusbildungsstaetteUpdateDto for update operations. This will provide control over all major operations in the context of training location data.

--- backend/src/main/java/ch/dvbern/stip/api/ausbildung/resource/AusbildungsstaetteResourceImpl.java
+++ backend/src/main/java/ch/dvbern/stip/api/ausbildung/resource/AusbildungsstaetteResourceImpl.java
@@ -4,14 +4,18 @@ import java.util.UUID;
 
 import ch.dvbern.stip.api.ausbildung.service.AusbildungsstaetteService;
 import ch.dvbern.stip.generated.api.AusbildungsstaetteResource;
+import ch.dvbern.stip.generated.dto.AusbildungsstaetteDto;
+import ch.dvbern.stip.generated.dto.AusbildungsstaetteUpdateDto;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 import lombok.RequiredArgsConstructor;
 
 @RequestScoped
 @RequiredArgsConstructor
 public class AusbildungsstaetteResourceImpl implements AusbildungsstaetteResource {
 
+    private final UriInfo uriInfo;
     private final AusbildungsstaetteService ausbildungsstaetteService;
 
     @Override
@@ -22,5 +26,23 @@ public class AusbildungsstaetteResourceImpl implements AusbildungsstaetteResourc
     @Override
     public Response getAusbildungsstaetten() {
         return Response.ok(ausbildungsstaetteService.getAusbildungsstaetten()).build();
+    }
+
+    @Override
+    public Response createAusbildungsstaette(AusbildungsstaetteUpdateDto ausbildungsstaette) {
+        AusbildungsstaetteDto created = ausbildungsstaetteService.createAusbildungsstaette(ausbildungsstaette);
+        return Response.created(uriInfo.getAbsolutePathBuilder().path(created.getId().toString()).build()).build();
+    }
+
+    @Override
+    public Response updateAusbildungsstaette(UUID ausbildungsstaetteId, AusbildungsstaetteUpdateDto ausbildungsstaette) {
+        ausbildungsstaetteService.updateAusbildungsstaette(ausbildungsstaetteId, ausbildungsstaette);
+        return Response.accepted().build();
+    }
+
+    @Override
+    public Response deleteAusbildungsstaette(UUID ausbildungsstaetteId) {
+        ausbildungsstaetteService.deleteAusbildungsstaette(ausbildungsstaetteId);
+        return Response.noContent().build();
     }
 }
