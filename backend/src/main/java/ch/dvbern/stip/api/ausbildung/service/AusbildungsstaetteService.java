fb2ffa1c9e28b21c99e22680d58ba81457248c9b
luca.fondo <luca.fondo@dvbern.ch>
2024-03-05 12:50:41 +0100

    feat(KSTIP-188): Add CRUD operations to AusbildungsstaetteService
    Implemented create, read, update, and delete (CRUD) methods in the AusbildungsstaetteService class. The class now uses the AusbildungsstaetteUpdateDto object for its update operations. This enhancement will improve the management of training location data.

--- backend/src/main/java/ch/dvbern/stip/api/ausbildung/service/AusbildungsstaetteService.java
+++ backend/src/main/java/ch/dvbern/stip/api/ausbildung/service/AusbildungsstaetteService.java
@@ -1,10 +1,15 @@
 package ch.dvbern.stip.api.ausbildung.service;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
+import ch.dvbern.stip.api.ausbildung.entity.Ausbildungsgang;
+import ch.dvbern.stip.api.ausbildung.entity.Ausbildungsstaette;
 import ch.dvbern.stip.api.ausbildung.repo.AusbildungsstaetteRepository;
+import ch.dvbern.stip.generated.dto.AusbildungsgangDto;
 import ch.dvbern.stip.generated.dto.AusbildungsstaetteDto;
+import ch.dvbern.stip.generated.dto.AusbildungsstaetteUpdateDto;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +21,11 @@ public class AusbildungsstaetteService {
     private final AusbildungsstaetteRepository ausbildungsstaetteRepository;
     private final AusbildungsstaetteMapper ausbildungsstaetteMapper;
 
+    public AusbildungsstaetteDto findById(UUID ausbildungsstetteId) {
+        var ausbildungsstaette = ausbildungsstaetteRepository.findById(ausbildungsstetteId);
+        return ausbildungsstaetteMapper.toDto(ausbildungsstaette);
+    }
+
     @Transactional
     public Collection<AusbildungsstaetteDto> getAusbildungsstaetten() {
         return ausbildungsstaetteRepository.findAll()
@@ -25,9 +35,30 @@ public class AusbildungsstaetteService {
             .toList();
     }
 
-    public AusbildungsstaetteDto findById(UUID ausbildungsstetteId) {
-        var ausbildungsstaette = ausbildungsstaetteRepository.findById(ausbildungsstetteId);
+    @Transactional
+    public AusbildungsstaetteDto createAusbildungsstaette(AusbildungsstaetteUpdateDto ausbildungsstaetteDto) {
+        Ausbildungsstaette ausbildungsstaette = new Ausbildungsstaette();
+        persistAusbildungsstaette(ausbildungsstaetteDto, ausbildungsstaette);
         return ausbildungsstaetteMapper.toDto(ausbildungsstaette);
+    }
+
+    @Transactional
+    public void updateAusbildungsstaette(UUID ausbildungsstaetteId, AusbildungsstaetteUpdateDto ausbildungsstaetteUpdateDto) {
+        Ausbildungsstaette ausbildungsstaetteToUpdate = ausbildungsstaetteRepository.findById(ausbildungsstaetteId);
+        persistAusbildungsstaette(ausbildungsstaetteUpdateDto, ausbildungsstaetteToUpdate);
+    }
+
+    @Transactional
+    public void deleteAusbildungsstaette(UUID ausbildungsstaetteId) {
+        Ausbildungsstaette ausbildungsstaette = ausbildungsstaetteRepository.findById(ausbildungsstaetteId);
+        ausbildungsstaetteRepository.delete(ausbildungsstaette);
+    }
+
+    private void persistAusbildungsstaette(
+        AusbildungsstaetteUpdateDto ausbildungsstaetteUpdate,
+        Ausbildungsstaette ausbildungsstaetteToUpdate) {
+        ausbildungsstaetteMapper.partialUpdate(ausbildungsstaetteUpdate, ausbildungsstaetteToUpdate);
+        ausbildungsstaetteRepository.persist(ausbildungsstaetteToUpdate);
     }
 
 }
