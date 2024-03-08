0009966699b54ddd2c34d9d970808609a9385300
Juri Furer <juri.furer@dvbern.ch>
2024-03-08 11:37:11 +0100

    feat(KSTIP-618): add tests

--- backend/src/test/java/ch/dvbern/stip/api/gesuch/service/GesuchFormularMapperTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuch/service/GesuchFormularMapperTest.java
@@ -1,11 +1,13 @@
 package ch.dvbern.stip.api.gesuch.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import ch.dvbern.stip.api.ausbildung.service.AusbildungMapperImpl;
 import ch.dvbern.stip.api.auszahlung.service.AuszahlungMapperImpl;
 import ch.dvbern.stip.api.common.service.DateMapperImpl;
 import ch.dvbern.stip.api.common.service.EntityReferenceMapperImpl;
+import ch.dvbern.stip.api.einnahmen_kosten.entity.EinnahmenKosten;
 import ch.dvbern.stip.api.einnahmen_kosten.service.EinnahmenKostenMapperImpl;
 import ch.dvbern.stip.api.eltern.service.ElternMapperImpl;
 import ch.dvbern.stip.api.eltern.type.ElternTyp;
@@ -25,10 +27,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 class GesuchFormularMapperTest {
     @Test
-    void mapperRemovesVater() {
+    void resetDependentDataRemovesRentenTest() {
+        final var target = new GesuchFormular()
+            .setFamiliensituation(
+                new Familiensituation()
+                    .setElternteilUnbekanntVerstorben(true)
+            )
+            .setEinnahmenKosten(
+                new EinnahmenKosten()
+                    .setRenten(new BigDecimal(1))
+            );
+
+        final var updateFormular = new GesuchFormularUpdateDto();
+        final var updateFamsit = new FamiliensituationUpdateDto();
+        updateFamsit.setElternteilUnbekanntVerstorben(false);
+        updateFormular.setFamiliensituation(updateFamsit);
+
+        final var mapper = createMapper();
+        mapper.resetDependentDataAfterData(updateFormular, target);
+
+        assertThat(target.getEinnahmenKosten().getRenten(), is(nullValue()));
+    }
+
+    @Test
+    void resetDependentDataRemovesVater() {
         // Arrange
         final var elterns = new ArrayList<ElternUpdateDto>();
         final var vater = new ElternUpdateDto();
@@ -40,25 +66,25 @@ class GesuchFormularMapperTest {
 
         final var familiensituation = new FamiliensituationUpdateDto();
 
-        final var gesuchFormular = new GesuchFormularUpdateDto();
-        gesuchFormular.setElterns(elterns);
-        gesuchFormular.setFamiliensituation(familiensituation);
+        final var updateFormular = new GesuchFormularUpdateDto();
+        updateFormular.setElterns(elterns);
+        updateFormular.setFamiliensituation(familiensituation);
 
         var target = new GesuchFormular()
-            .setFamiliensituation(new Familiensituation());
+            .setFamiliensituation(null);
 
         // Act & Assert
         final var mapper = createMapper();
         // Partial update is needed here to "initialise" the target
-        target = mapper.partialUpdate(gesuchFormular, target);
-        mapper.resetDependentDataAfterData(gesuchFormular, target);
+        target = mapper.partialUpdate(updateFormular, target);
+        mapper.resetDependentDataAfterData(updateFormular, target);
 
         // Without werZahltAlimente set, nothing should be cleared
         assertThat(target.getElterns().size(), is(2));
 
         // Setting it so the VATER pays alimony, and then clearing on update should remove the father from the Gesuch
         familiensituation.setWerZahltAlimente(Elternschaftsteilung.VATER);
-        mapper.resetDependentDataAfterData(gesuchFormular, target);
+        mapper.resetDependentDataAfterData(updateFormular, target);
 
         assertThat(target.getElterns().size(), is(1));
         assertThat(target.getElterns().stream().toList().get(0).getElternTyp(), is(ElternTyp.MUTTER));
