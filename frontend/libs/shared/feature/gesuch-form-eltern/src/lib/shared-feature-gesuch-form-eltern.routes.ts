4273ffc9f296e4ac91ca5781fe0fbd5d2b9d946b
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-03-11 15:14:27 +0100

    fix: improved typesafety for canDeactivate on unsaved changes

--- frontend/libs/shared/feature/gesuch-form-eltern/src/lib/shared-feature-gesuch-form-eltern.routes.ts
+++ frontend/libs/shared/feature/gesuch-form-eltern/src/lib/shared-feature-gesuch-form-eltern.routes.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 
-import { checkUnsavedChanges } from '@dv/shared/pattern/unsaved-guard';
+import { routeWithUnsavedChangesGuard } from '@dv/shared/pattern/unsaved-guard';
 
 import { SharedFeatureGesuchFormElternComponent } from './shared-feature-gesuch-form-eltern/shared-feature-gesuch-form-eltern.component';
 
@@ -13,17 +13,16 @@ export const gesuchAppFeatureGesuchFormElternRoutes: Route[] = [
       // always remove { providedIn: 'root' } from the feature specific services
     ],
     children: [
-      {
+      routeWithUnsavedChangesGuard({
         path: ':id',
         title: 'shared.eltern.title',
         component: SharedFeatureGesuchFormElternComponent,
         runGuardsAndResolvers: 'always',
-        canDeactivate: [checkUnsavedChanges],
         data: {
           // reinitialize when navigated to the same route
           shouldReuseRoute: false,
         },
-      },
+      }),
       // add more routes here (siblings)
       // it is also possible to add nested routes as children
       // of this feature root component (or even lazy loaded sub features)
