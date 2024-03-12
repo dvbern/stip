8b98e1ab77c8478db09d3443ddb74b8058f90c06
luca.fondo <luca.fondo@dvbern.ch>
2024-03-12 11:33:38 +0100

    feat(KSTIP-188): Remove UUID import and usage from AusbildungsstaetteCreateDtoSpecModel
    Removed the import statement for java.util.UUID and usage of UUID from the AusbildungsstaetteCreateDtoSpecModel class. This change was necessary because the ID was not required in this class, and it was causing unnecessary complications.

--- backend/src/test/java/ch/dvbern/stip/api/generator/api/model/gesuch/AusbildungsstaetteCreateDtoSpecModel.java
+++ backend/src/test/java/ch/dvbern/stip/api/generator/api/model/gesuch/AusbildungsstaetteCreateDtoSpecModel.java
@@ -1,7 +1,5 @@
 package ch.dvbern.stip.api.generator.api.model.gesuch;
 
-import java.util.UUID;
-
 import ch.dvbern.stip.generated.dto.AusbildungsstaetteCreateDtoSpec;
 import org.instancio.Instancio;
 import org.instancio.Model;
@@ -13,6 +11,5 @@ public class AusbildungsstaetteCreateDtoSpecModel {
 			Instancio.of(AusbildungsstaetteCreateDtoSpec.class)
 					.set(field(AusbildungsstaetteCreateDtoSpec::getNameDe), "Uni Bern")
 					.set(field(AusbildungsstaetteCreateDtoSpec::getNameFr), "Uni Bern")
-					.set(field(AusbildungsstaetteCreateDtoSpec::getId), UUID.fromString("9477487f-3ac4-4d02-b57c-e0cefb292ae5"))
 					.toModel();
 }
