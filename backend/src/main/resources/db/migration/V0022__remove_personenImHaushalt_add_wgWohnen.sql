0402826e00b5369ed63238bbb053534743727f1a
Juri Furer <juri.furer@dvbern.ch>
2024-03-20 14:53:56 +0100

    feat(KSTIP-627): fix typo

--- backend/src/main/resources/db/migration/V0022__remove_personenImHaushalt_add_wgWohnen.sql
+++ backend/src/main/resources/db/migration/V0022__remove_personenImHaushalt_add_wgWohnen.sql
@@ -1,5 +1,5 @@
 ALTER TABLE einnahmen_kosten DROP COLUMN personen_im_haushalt;
 ALTER TABLE einnahmen_kosten_aud DROP COLUMN personen_im_haushalt;
 
-ALTER TABLE einnahmen_kosten ADD COLUMN wg_wohnen BOOLEAN;
-ALTER TABLE einnahmen_kosten_aud ADD COLUMN wg_wohnen BOOLEAN;
+ALTER TABLE einnahmen_kosten ADD COLUMN wg_wohnend BOOLEAN;
+ALTER TABLE einnahmen_kosten_aud ADD COLUMN wg_wohnend BOOLEAN;
