70465ff27c62f631cbb366cd5ec03e08d6e6c34e
luca.fondo <luca.fondo@dvbern.ch>
2024-03-22 14:24:46 +0100

    feat(KSTIP-189): Add Gesuchsjahr entity class
    Implemented a new entity class "Gesuchsjahr" in the backend source. It extends AbstractMandantEntity and includes attributes for year label, training year period, technical year and status validity along with necessary annotations and constraints. The class further includes getter, setter, all-args and no-args constructor annotations.

---
+++ backend/src/main/java/ch/dvbern/stip/api/gesuchsjahr/entity/Gesuchsjahr.java
@@ -0,0 +1,56 @@
+package ch.dvbern.stip.api.gesuchsjahr.entity;
+
+import ch.dvbern.stip.api.common.entity.AbstractMandantEntity;
+import ch.dvbern.stip.api.common.type.GueltigkeitStatus;
+import ch.dvbern.stip.api.common.util.DateRange;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+@Audited
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Gesuchsjahr extends AbstractMandantEntity {
+
+	@Column(name = "bezeichnung_de")
+	private String bezeichnungDe;
+
+	@Column(name = "bezeichnung_fr")
+	private String bezeichnungFr;
+
+	@Embedded
+	@NotNull
+	@Valid
+	@AttributeOverrides({
+			@AttributeOverride(
+					name = "gueltigAb",
+					column = @Column(name = "ausbildungsjahr_start")
+			),
+			@AttributeOverride(
+					name = "gueltigBis",
+					column = @Column(name = "ausbildungsjahr_ende")
+			)
+	})
+	private DateRange ausbildungsjahr;
+
+	@Column(name = "technisches_jahr")
+	private Integer technischesJahr;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "gueltigkeit_status")
+	private GueltigkeitStatus gueltigkeitStatus;
+}
