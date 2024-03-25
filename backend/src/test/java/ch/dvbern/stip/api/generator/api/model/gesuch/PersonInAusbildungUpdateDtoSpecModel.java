678b53a77e44e9f7099c8adec6072331c6ad8a6d
Juri Furer <juri.furer@dvbern.ch>
2024-03-25 13:35:45 +0100

    fix(KSTIP-875): fix conflicts

--- backend/src/test/java/ch/dvbern/stip/api/generator/api/model/gesuch/PersonInAusbildungUpdateDtoSpecModel.java
+++ backend/src/test/java/ch/dvbern/stip/api/generator/api/model/gesuch/PersonInAusbildungUpdateDtoSpecModel.java
@@ -1,10 +1,16 @@
 package ch.dvbern.stip.api.generator.api.model.gesuch;
 
-import ch.dvbern.stip.api.util.TestUtil;
-import ch.dvbern.stip.generated.dto.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import ch.dvbern.stip.api.util.TestUtil;
+import ch.dvbern.stip.generated.dto.AnredeDtoSpec;
+import ch.dvbern.stip.generated.dto.GesuchFormularUpdateDtoSpec;
+import ch.dvbern.stip.generated.dto.LandDtoSpec;
+import ch.dvbern.stip.generated.dto.PersonInAusbildungUpdateDtoSpec;
+import ch.dvbern.stip.generated.dto.SpracheDtoSpec;
+import ch.dvbern.stip.generated.dto.WohnsitzDtoSpec;
+import ch.dvbern.stip.generated.dto.ZivilstandDtoSpec;
 
 import static ch.dvbern.stip.api.util.TestConstants.AHV_NUMMER_VALID_PERSON_IN_AUSBILDUNG;
 
@@ -35,61 +41,6 @@ public final class PersonInAusbildungUpdateDtoSpecModel {
             model.setKorrespondenzSprache(TestUtil.getRandomElementFromArray(SpracheDtoSpec.values()));
         });
 
-<<<<<<< HEAD
-    public static final Model<PersonInAusbildungUpdateDtoSpec> personInAusbildungUpdateDtoSpecModel =
-        Instancio.of(PersonInAusbildungUpdateDtoSpec.class)
-            .set(
-                field(PersonInAusbildungUpdateDtoSpec::getAdresse),
-                Instancio.create(AdresseSpecModel.adresseSpecModel))
-            .set(field(PersonInAusbildungUpdateDtoSpec::getIdentischerZivilrechtlicherWohnsitz), false)
-            .set(field(PersonInAusbildungUpdateDtoSpec::getNationalitaet), LandDtoSpec.CH)
-            .set(field(PersonInAusbildungUpdateDtoSpec::getWohnsitz), WohnsitzDtoSpec.MUTTER_VATER)
-            .set(field(PersonInAusbildungUpdateDtoSpec::getNiederlassungsstatus), null)
-            .set(field(PersonInAusbildungUpdateDtoSpec::getEmail), "valid@mailbucket.dvbern.ch")
-            .set(
-                field(PersonInAusbildungUpdateDtoSpec::getSozialversicherungsnummer),
-                AHV_NUMMER_VALID_PERSON_IN_AUSBILDUNG
-            )
-            .generate(
-                field(PersonInAusbildungUpdateDtoSpec::getVorname),
-                gen -> gen.oneOf("Sarah", "Elijah", "Nu'ab", "André", "Özgür")
-            )
-            .set(
-                field(PersonInAusbildungUpdateDtoSpec::getGeburtsdatum),
-                LocalDate.now().minusYears(16)
-            )
-            .generate(
-                field(PersonInAusbildungUpdateDtoSpec::getNachname),
-                gen -> gen.oneOf("Müller", "Sánchez", "Sato", "Singh", "Li", "García", "Nguyen")
-            )
-            .generate(
-                field(PersonInAusbildungUpdateDtoSpec::getWohnsitzAnteilMutter),
-                gen -> gen.ints().range(0, 100).as(BigDecimal::valueOf)
-            )
-            .assign(Assign.valueOf(PersonInAusbildungUpdateDtoSpec::getWohnsitzAnteilMutter)
-                .to(PersonInAusbildungUpdateDtoSpec::getWohnsitzAnteilVater)
-                .as((BigDecimal i) -> BigDecimal.valueOf(100).subtract(i)))
-            .ignore(field(PersonInAusbildungUpdateDtoSpec::getVermoegenVorjahr))
-            .toModel();
-    public static final Model<GesuchFormularUpdateDtoSpec> gesuchFormularUpdateDtoSpecPersonInAusbildungModel =
-        Instancio.of(
-                GesuchFormularUpdateDtoSpec.class)
-            .set(
-                field(GesuchFormularUpdateDtoSpec::getPersonInAusbildung),
-                Instancio.create(personInAusbildungUpdateDtoSpecModel)
-            )
-            .ignore(field(GesuchFormularUpdateDtoSpec::getAusbildung))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getElterns))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getGeschwisters))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getLebenslaufItems))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getEinnahmenKosten))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getAuszahlung))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getFamiliensituation))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getKinds))
-            .ignore(field(GesuchFormularUpdateDtoSpec::getPartner))
-            .toModel();
-=======
     public static final GesuchFormularUpdateDtoSpec gesuchFormularUpdateDtoSpecPersonInAusbildung =
         TestUtil.createUpdateDtoSpec(GesuchFormularUpdateDtoSpec::new, (model, faker) -> model.setPersonInAusbildung(personInAusbildungUpdateDtoSpec));
->>>>>>> feature/KSTIP-919
 }
