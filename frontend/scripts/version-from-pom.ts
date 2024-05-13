3fe76f748005493ff0be86e7491a7ecbfa880f3c
Juri Furer <juri.furer@dvbern.ch>
2024-05-13 10:22:29 +0200

    feat(KSTIP-783): add versions.ts file

---
+++ frontend/scripts/version-from-pom.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { writeFileSync } from 'node:fs';
+
+import { resolve } from 'path';
+
+const version = process.env.VERSION;
+if (!version) {
+  throw Error("'VERSION' environment variable is not set! Aborting build.");
+}
+
+const file = resolve(__dirname, '..', 'libs', 'shared', 'version.ts');
+writeFileSync(
+  file,
+  `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECK INTO GIT
+export const VERSION = '${version}';
+`,
+  {
+    flag: 'w',
+  },
+);
