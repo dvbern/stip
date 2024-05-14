52dcf2a4bf4f2cc903c87148a6dc8689d316af54
Juri Furer <juri.furer@dvbern.ch>
2024-05-14 15:57:13 +0200

    feat(KSTIP-1016): MR changes

--- backend/src/test/java/ch/dvbern/stip/api/gesuch/service/GesuchServiceTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuch/service/GesuchServiceTest.java
@@ -733,13 +733,11 @@ class GesuchServiceTest {
         tranche.getGesuch().setGesuchStatus(Gesuchstatus.KOMPLETT_EINGEREICHT);
 
         when(gesuchRepository.requireById(any())).thenReturn(tranche.getGesuch());
-        when(gesuchRepository.findGesucheBySvNummer(any())).thenReturn(
-            Stream.of(
-                (Gesuch) new Gesuch()
+        when(gesuchRepository.findGesucheBySvNummer(any())).thenReturn(Stream.of((Gesuch)
+            new Gesuch()
                 .setGesuchStatus(Gesuchstatus.KOMPLETT_EINGEREICHT)
                 .setId(UUID.randomUUID())
-            )
-        );
+        ));
 
         ValidationReportDto reportDto = gesuchService.validateGesuchEinreichen(tranche.getGesuch().getId());
 
