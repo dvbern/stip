b431912e93331a66a18cc133ea5a6520f7750b57
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-06-05 15:07:38 +0200

    fix(KSTIP-1032): reduces complexity and size of main function

--- frontend/libs/shared/pattern/http-error-interceptor/src/lib/http-error.interceptor.ts
+++ frontend/libs/shared/pattern/http-error-interceptor/src/lib/http-error.interceptor.ts
@@ -24,7 +24,7 @@ export interface DvGlobalHttpErrorInterceptorFnOptions {
   type: 'without' | 'globalOnly' | 'globalAndLocal';
 }
 
-const handledError = <T>(value?: Observable<T>) => ({
+const handledError = <T>(value?: Observable<HttpResponse<T>>) => ({
   handled: true,
   result: value,
 });
@@ -46,42 +46,10 @@ export function withDvGlobalHttpErrorInterceptorFn({
       const oauth = inject(OAuthService);
       const router = inject(Router);
 
-      const knownHandlers: ((error: SharedModelError) => {
-        handled: boolean;
-        result?: Observable<HttpResponse<unknown>>;
-      })[] = [
+      const knownHandlers = [
         // This list is ordered by priority, the first handler that returns a result will prevent the others from being executed
-        // but the `handled` flag does not prevent the others from being executed, just informs that the error was handled in some way
-        function handleUnknownHttpError(error) {
-          if (error.type === 'unknownHttpError') {
-            if (error.status === 401 && !req.context.get(IGNORE_ERRORS)) {
-              oauth.initLoginFlow(router.url);
-              return handledError(EMPTY);
-            }
-            // Check for 403 FORBIDDEN
-            if (error.status === 403) {
-              store.handleForbiddenError(error);
-              return handledError();
-            }
-            // Check for 404 NOT FOUND if a handler is provided
-            const notFoundHandler = req.context.get(HANDLE_NOT_FOUND);
-            if (notFoundHandler && error.status === 404) {
-              notFoundHandler(error);
-              return handledError();
-            }
-          }
-          return unhandledError;
-        },
-
-        function handle404(error) {
-          if (
-            req.context.get(IGNORE_NOT_FOUND_ERRORS) &&
-            error.status === 404
-          ) {
-            return handledError(of(new HttpResponse({})));
-          }
-          return unhandledError;
-        },
+        handleUnknownHttpError(req, oauth, router, store),
+        handle404(req),
       ];
 
       return next(req).pipe(
@@ -123,3 +91,38 @@ export function withDvGlobalHttpErrorInterceptorFn({
     return [HttpErrorInterceptor];
   }
 }
+
+const handleUnknownHttpError = (
+  req: HttpRequest<unknown>,
+  oauth: OAuthService,
+  router: Router,
+  notificationStore: GlobalNotificationStore,
+) => {
+  return (error: SharedModelError) => {
+    if (error.type === 'unknownHttpError') {
+      if (error.status === 401 && !req.context.get(IGNORE_ERRORS)) {
+        oauth.initLoginFlow(router.url);
+        return handledError(EMPTY);
+      }
+      // Check for 403 FORBIDDEN
+      if (error.status === 403) {
+        notificationStore.handleForbiddenError(error);
+        return handledError();
+      }
+      // Check for 404 NOT FOUND if a handler is provided
+      const notFoundHandler = req.context.get(HANDLE_NOT_FOUND);
+      if (notFoundHandler && error.status === 404) {
+        notFoundHandler(error);
+        return handledError();
+      }
+    }
+    return unhandledError;
+  };
+};
+
+const handle404 = (req: HttpRequest<unknown>) => (error: SharedModelError) => {
+  if (req.context.get(IGNORE_NOT_FOUND_ERRORS) && error.status === 404) {
+    return handledError(of(new HttpResponse({})));
+  }
+  return unhandledError;
+};
