92c0578c93445c5757e03659eda678351ea2755c
Fabrice Jakob <fabrice.jakob@dvbern.ch>
2024-06-20 17:48:37 +0200

    feat(KSTIP-1055): included a package dropped in merge

--- backend/src/main/java/ch/dvbern/stip/api/gesuch/service/GesuchService.java
+++ backend/src/main/java/ch/dvbern/stip/api/gesuch/service/GesuchService.java
@@ -26,6 +26,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import ch.dvbern.stip.api.benutzer.entity.Rolle;
