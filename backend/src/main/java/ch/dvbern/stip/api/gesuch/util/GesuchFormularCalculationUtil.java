342de599a6038a62016e0ce09d428600b5dd1ed1
Juri Furer <juri.furer@dvbern.ch>
2024-06-24 10:56:54 +0200

    feat(KSTIP-1055): use new date for calc and remove Joda-Time

--- backend/src/main/java/ch/dvbern/stip/api/gesuch/util/GesuchFormularCalculationUtil.java
+++ backend/src/main/java/ch/dvbern/stip/api/gesuch/util/GesuchFormularCalculationUtil.java
@@ -1,29 +1,23 @@
 package ch.dvbern.stip.api.gesuch.util;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
+import java.time.temporal.ChronoUnit;
 
 import ch.dvbern.stip.api.gesuch.entity.GesuchFormular;
 import lombok.experimental.UtilityClass;
-import org.joda.time.Instant;
-import org.joda.time.Years;
+
 @UtilityClass
 public class GesuchFormularCalculationUtil {
-    public int calculateNumberOfYearsBetween(final Date date1, final Date date2) {
-        return Math.abs(Years.yearsBetween(Instant.ofEpochMilli(date1.getTime()),Instant.ofEpochMilli(date2.getTime())).getYears());
-    }
-    public int calculateAgeAtLocalDate(final GesuchFormular gesuchFormular, final LocalDate referenceDate) {
-        LocalDate geburtsDatumGS = gesuchFormular.getPersonInAusbildung().getGeburtsdatum();
-        Date geburtsDatumGSAsDate = Date.from(geburtsDatumGS.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Date vorjahrGesuchsjahrDatumAsDate = Date.from(referenceDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        return calculateNumberOfYearsBetween(vorjahrGesuchsjahrDatumAsDate, geburtsDatumGSAsDate);
+    public int calculateNumberOfYearsBetween(final LocalDate date1, final LocalDate date2) {
+        // Casting is ok, there have been more than 2147483647 years, but we just truncate to that
+        return (int) ChronoUnit.YEARS.between(date1, date2);
     }
 
     public LocalDate getVorjahrGesuchsjahrAsLocalDate(final GesuchFormular gesuchFormular) {
         int vorjahrGesuchsjahr = LocalDate.now().getYear() - 1;
         if (gesuchFormular.getTranche() != null) {
-            vorjahrGesuchsjahr = gesuchFormular.getTranche().getGesuch().getGesuchsperiode().getGesuchsjahr().getTechnischesJahr() - 1;
+            vorjahrGesuchsjahr =
+                gesuchFormular.getTranche().getGesuch().getGesuchsperiode().getGesuchsjahr().getTechnischesJahr() - 1;
         }
         return LocalDate.of(vorjahrGesuchsjahr, 12, 31);
     }
@@ -32,6 +26,10 @@ public class GesuchFormularCalculationUtil {
         if (gesuchFormular.getPersonInAusbildung() == null) {
             return true;
         }
-        return calculateAgeAtLocalDate(gesuchFormular, getVorjahrGesuchsjahrAsLocalDate(gesuchFormular)) >= 18;
+
+        return calculateNumberOfYearsBetween(
+            gesuchFormular.getPersonInAusbildung().getGeburtsdatum(),
+            getVorjahrGesuchsjahrAsLocalDate(gesuchFormular)
+        ) >= 18;
     }
 }
