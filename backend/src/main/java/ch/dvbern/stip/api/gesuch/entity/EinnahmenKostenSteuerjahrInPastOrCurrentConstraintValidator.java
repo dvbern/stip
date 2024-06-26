0d04391581c27d89cc926d4afd323744ea5220f0
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-06-26 10:59:45 +0200

    feat(KSTIP-1055): fix validation for einnahmekosten steuerjahr

--- backend/src/main/java/ch/dvbern/stip/api/gesuch/entity/EinnahmenKostenSteuerjahrInPastOrCurrentConstraintValidator.java
+++ backend/src/main/java/ch/dvbern/stip/api/gesuch/entity/EinnahmenKostenSteuerjahrInPastOrCurrentConstraintValidator.java
@@ -3,18 +3,15 @@ package ch.dvbern.stip.api.gesuch.entity;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.time.Year;
-
 public class EinnahmenKostenSteuerjahrInPastOrCurrentConstraintValidator
 implements ConstraintValidator<EinnahmenKostenSteuerjahrInPastOrCurrentConstraint, GesuchFormular> {
-    private static final Integer CURRENT_YEAR = Year.now().getValue();
 
     @Override
     public boolean isValid(GesuchFormular gesuchFormular, ConstraintValidatorContext constraintValidatorContext) {
         if(gesuchFormular.getEinnahmenKosten() == null || gesuchFormular.getEinnahmenKosten().getSteuerjahr() == null){
             return true;
         }
-        Integer steuerjahr = gesuchFormular.getEinnahmenKosten().getSteuerjahr();
-        return steuerjahr <= CURRENT_YEAR;
+        return gesuchFormular.getEinnahmenKosten().getSteuerjahr() <= gesuchFormular.getTranche().getGesuch().getGesuchsperiode().getGesuchsjahr().getTechnischesJahr() -1;
+
     }
 }
