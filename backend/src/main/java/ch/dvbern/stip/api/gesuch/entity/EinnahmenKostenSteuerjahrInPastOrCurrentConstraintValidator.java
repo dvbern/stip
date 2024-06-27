1259f92c317f7f2243556b07e5fe561b1378d801
Juri Furer <juri.furer@dvbern.ch>
2024-06-27 09:49:54 +0200

    feat(KSTIP-1055): reformat and create helper for Steuerjahr

--- backend/src/main/java/ch/dvbern/stip/api/gesuch/entity/EinnahmenKostenSteuerjahrInPastOrCurrentConstraintValidator.java
+++ backend/src/main/java/ch/dvbern/stip/api/gesuch/entity/EinnahmenKostenSteuerjahrInPastOrCurrentConstraintValidator.java
@@ -1,17 +1,21 @@
 package ch.dvbern.stip.api.gesuch.entity;
 
+import ch.dvbern.stip.api.gesuchsjahr.service.GesuchsjahrUtil;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class EinnahmenKostenSteuerjahrInPastOrCurrentConstraintValidator
-implements ConstraintValidator<EinnahmenKostenSteuerjahrInPastOrCurrentConstraint, GesuchFormular> {
+    implements ConstraintValidator<EinnahmenKostenSteuerjahrInPastOrCurrentConstraint, GesuchFormular> {
 
     @Override
     public boolean isValid(GesuchFormular gesuchFormular, ConstraintValidatorContext constraintValidatorContext) {
-        if(gesuchFormular.getEinnahmenKosten() == null || gesuchFormular.getEinnahmenKosten().getSteuerjahr() == null){
+        if (gesuchFormular.getEinnahmenKosten() == null ||
+            gesuchFormular.getEinnahmenKosten().getSteuerjahr() == null
+        ) {
             return true;
         }
-        return gesuchFormular.getEinnahmenKosten().getSteuerjahr() <= gesuchFormular.getTranche().getGesuch().getGesuchsperiode().getGesuchsjahr().getTechnischesJahr() -1;
 
+        final var gesuchsjahr = gesuchFormular.getTranche().getGesuch().getGesuchsperiode().getGesuchsjahr();
+        return gesuchFormular.getEinnahmenKosten().getSteuerjahr() <= GesuchsjahrUtil.getDefaultSteuerjahr(gesuchsjahr);
     }
 }
