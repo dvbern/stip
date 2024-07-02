1e0f206ccab60da118d0431c2f78ef69e0f37720
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-07-02 11:32:02 +0200

    fix(ci-e2e): skip steuerdaten tab in e2e

--- frontend/apps/gesuch-app-e2e/src/tests/gesuch/gesuch-erstellen.spec.ts
+++ frontend/apps/gesuch-app-e2e/src/tests/gesuch/gesuch-erstellen.spec.ts
@@ -226,7 +226,7 @@ test.describe('Neues gesuch erstellen', () => {
 
     await familiyPO.elems.buttonSaveContinue.click();
 
-    // Step 5: Eltern =============================================================
+    // Step 5.1: Eltern =============================================================
     await expectStepTitleToContainText('Eltern', page);
     const elternPO = new ElternPO(page);
     await expect(elternPO.elems.loading).toBeHidden();
@@ -235,6 +235,11 @@ test.describe('Neues gesuch erstellen', () => {
     await elternPO.addMutter(mutter);
 
     await elternPO.elems.buttonContinue.click();
+
+    // Step 5.2: Steuerdaten Eltern =================================================
+    await expectStepTitleToContainText('Steuerdaten für', page);
+    // Skipping for now as no .po for steuerdaten exists yet
+    await page.getByTestId('step-nav-geschwister').click();
 
     // Step 6: Geschwister  ========================================================
     await expectStepTitleToContainText('Geschwister', page);
