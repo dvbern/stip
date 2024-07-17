999dabec58c547d66f73e6d454d85ec8d0073738
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-07-17 16:16:48 +0200

    feat(KSTIP-1215): small changes in setAndValidateSteuerdatenUpdateLegality

--- backend/src/main/java/ch/dvbern/stip/api/gesuch/service/GesuchService.java
+++ backend/src/main/java/ch/dvbern/stip/api/gesuch/service/GesuchService.java
@@ -148,12 +148,11 @@ public class GesuchService {
 
         final var steuerdatenList = trancheToUpdate.getGesuchFormular().getSteuerdaten().stream().filter(tab -> tab.getSteuerdatenTyp() != null).toList();
 
-        for (final var currentSteuerdatenUpdateDto : steuerdatenUpdateDtos){
-            setAndValidateSteuerdatenTabUpdateLegality(currentSteuerdatenUpdateDto, steuerdatenList.stream().filter(tab -> tab.getId().equals(currentSteuerdatenUpdateDto.getId())).findFirst().orElse(null),gesuchsjahr);
+        for (final var steuerdatenUpdateDto : steuerdatenUpdateDtos){
+            setAndValidateSteuerdatenTabUpdateLegality(steuerdatenUpdateDto, steuerdatenList.stream().filter(tab -> tab.getId().equals(steuerdatenUpdateDto.getId())).findFirst().orElse(null),gesuchsjahr);
         }
     }
-    @Transactional
-    public void setAndValidateSteuerdatenTabUpdateLegality(final SteuerdatenUpdateDto steuerdatenUpdateDto, final Steuerdaten steuerdaten, Gesuchsjahr gesuchsjahr){
+    private void setAndValidateSteuerdatenTabUpdateLegality(final SteuerdatenUpdateDto steuerdatenUpdateDto, final Steuerdaten steuerdatenTabs, Gesuchsjahr gesuchsjahr){
         final var benutzerRollenIdentifiers = benutzerService.getCurrentBenutzer()
             .getRollen()
             .stream()
@@ -164,21 +163,21 @@ public class GesuchService {
         Integer veranlagungsCodeToSet = 0;
 
         if (!CollectionUtils.containsAny(benutzerRollenIdentifiers,  Arrays.asList(OidcConstants.ROLE_SACHBEARBEITER, OidcConstants.ROLE_ADMIN))){
-            if(steuerdaten != null){
+            if(steuerdatenTabs != null){
                 steuerjahrToSet = Objects.requireNonNullElse(
-                    steuerdaten.getSteuerjahr(),
+                    steuerdatenTabs.getSteuerjahr(),
                     steuerjahrToSet
                 );
                 veranlagungsCodeToSet = Objects.requireNonNullElse(
-                    steuerdaten.getVeranlagungsCode(),
+                    steuerdatenTabs.getVeranlagungsCode(),
                     veranlagungsCodeToSet
                 );
             }
         } else {
             if (steuerdatenUpdateDto.getSteuerjahr() == null) {
-                if (steuerdaten != null) {
+                if (steuerdatenTabs != null) {
                     steuerjahrToSet = Objects.requireNonNullElse(
-                        steuerdaten.getSteuerjahr(),
+                        steuerdatenTabs.getSteuerjahr(),
                         steuerjahrToSet
                     );
                 }
@@ -186,9 +185,9 @@ public class GesuchService {
                 steuerjahrToSet = steuerdatenUpdateDto.getSteuerjahr();
             }
             if (steuerdatenUpdateDto.getVeranlagungscode() == null) {
-                if (steuerdaten != null) {
+                if (steuerdatenTabs != null) {
                     veranlagungsCodeToSet = Objects.requireNonNullElse(
-                        steuerdaten.getVeranlagungsCode(),
+                        steuerdatenTabs.getVeranlagungsCode(),
                         veranlagungsCodeToSet
                     );
                 }
