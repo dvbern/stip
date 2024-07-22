daabebcce85337de645af1c6bc7ca1bb45d0dfe3
Fabrice Jakob <fabrice.jakob@dvbern.ch>
2024-07-22 13:09:18 +0200

    feat(KSTIP-993): now it should be actually fixed

--- backend/src/main/java/ch/dvbern/stip/api/gesuch/entity/DocumentsRequiredFehlendeDokumenteConstraintValidator.java
+++ backend/src/main/java/ch/dvbern/stip/api/gesuch/entity/DocumentsRequiredFehlendeDokumenteConstraintValidator.java
@@ -1,16 +1,19 @@
 package ch.dvbern.stip.api.gesuch.entity;
 
 import ch.dvbern.stip.api.dokument.type.Dokumentstatus;
-import jakarta.transaction.Transactional;
+import ch.dvbern.stip.api.gesuch.service.GesuchService;
+import jakarta.inject.Inject;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class DocumentsRequiredFehlendeDokumenteConstraintValidator
     implements ConstraintValidator<DocumentsRequiredFehlendeDokumenteConstraint, Gesuch> {
 
+    @Inject
+    GesuchService gesuchService;
+
     @Override
-    @Transactional
     public boolean isValid(Gesuch gesuch, ConstraintValidatorContext context) {
-        return gesuch.getGesuchDokuments().stream().allMatch(dok -> dok.getStatus() != Dokumentstatus.AUSSTEHEND);
+        return gesuchService.getGesuchDokumenteForGesuch(gesuch.getId()).stream().allMatch(gesuchDokumentDto -> gesuchDokumentDto.getStatus() != Dokumentstatus.AUSSTEHEND);
     }
 }
