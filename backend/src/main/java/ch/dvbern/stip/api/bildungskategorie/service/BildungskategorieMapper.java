fef9e843aca1030bc6edd64f0372b669ddfbe142
Juri Furer <juri.furer@dvbern.ch>
2024-08-05 08:15:40 +0200

    feat(KSTIP-1226): remove unnecessary mapping annotation

--- backend/src/main/java/ch/dvbern/stip/api/bildungskategorie/service/BildungskategorieMapper.java
+++ backend/src/main/java/ch/dvbern/stip/api/bildungskategorie/service/BildungskategorieMapper.java
@@ -4,10 +4,8 @@ import ch.dvbern.stip.api.bildungskategorie.entity.Bildungskategorie;
 import ch.dvbern.stip.api.common.service.MappingConfig;
 import ch.dvbern.stip.generated.dto.BildungskategorieDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(config = MappingConfig.class)
-public abstract class BildungskategorieMapper {
-    @Mapping(target = "bildungsstufe", expression = "java(bildungskategorie.getBildungsstufe())")
-	abstract BildungskategorieDto toDto(Bildungskategorie bildungskategorie);
+public interface BildungskategorieMapper {
+    BildungskategorieDto toDto(Bildungskategorie bildungskategorie);
 }
