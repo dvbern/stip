38d52c25d83ffe917729f0de4ac2f17b9c20b893
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-08-06 09:11:36 +0200

    feat(KSTIP-1215): refactor unit tests

--- backend/src/test/java/ch/dvbern/stip/api/gesuch/service/ValidateUpdateLegalityUtilTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuch/service/ValidateUpdateLegalityUtilTest.java
@@ -1,27 +1,45 @@
 package ch.dvbern.stip.api.gesuch.service;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-
+import org.junit.jupiter.params.provider.CsvSource;
 import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class ValidateUpdateLegalityUtilTest {
-    @CsvFileSource(resources = "data.csv", numLinesToSkip = 1)
+    @CsvSource({
+        //role, dtoValue, existingValue, defaultValue, expected
+        "Gesuchsteller,1,2,0,2",
+        "Sachbearbeiter,1,2,0,1",
+        "Admin,1,2,0,1",
+    })
     @ParameterizedTest
-    void getAndValidateLegalityValueTest(String role, String dtoValue, String existingValue, String defaultValue, String expected) {
-        assertThat(ValidateUpdateLegalityUtil.getAndValidateLegalityValue(Set.of(role),dtoValue,existingValue,defaultValue), is(expected));
+    void getAndValidateLegalityValueTest(String role, String dtoValue, String existingValue,
+                                         String defaultValue, String expected) {
+        assertThat(ValidateUpdateLegalityUtil.getAndValidateLegalityValue(Set.of(role),dtoValue,
+            existingValue,defaultValue), is(expected));
     }
-    @CsvFileSource(resources = "dataNullValues.csv", numLinesToSkip = 1)
+    @CsvSource({
+        //role,dtoValue,defaultValue,expected
+        "Gesuchsteller,1,0,0",
+        "Sachbearbeiter,1,0,1",
+        "Admin,1,0,1"
+    })
     @ParameterizedTest
-    void getAndValidateLegalityValueNoExistingValueTest(String role, String dtoValue, String defaultValue, String expected) {
-        assertThat(ValidateUpdateLegalityUtil.getAndValidateLegalityValue(Set.of(role),dtoValue,null,defaultValue), is(expected));
+    void getAndValidateLegalityValueNoExistingValueTest(String role, String dtoValue,
+                                                        String defaultValue, String expected) {
+        assertThat(ValidateUpdateLegalityUtil.getAndValidateLegalityValue(Set.of(role),
+            dtoValue,null,defaultValue), is(expected));
     }
-    @CsvFileSource(resources = "dataDefaultValues.csv", numLinesToSkip = 1)
+    @CsvSource({
+        //role,defaultValue,expected
+        "Gesuchsteller,0,0",
+        "Sachbearbeiter,0,0",
+        "Admin,0,0"
+    })
     @ParameterizedTest
     void getAndValidateLegalityValueDefaultValueTest(String role,String defaultValue, String expected) {
-        assertThat(ValidateUpdateLegalityUtil.getAndValidateLegalityValue(Set.of(role),null,null,defaultValue), is(expected));
+        assertThat(ValidateUpdateLegalityUtil.getAndValidateLegalityValue(Set.of(role),
+            null,null,defaultValue), is(expected));
     }
 }
