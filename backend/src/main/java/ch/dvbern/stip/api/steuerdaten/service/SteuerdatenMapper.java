7ae3739e921aa888f9492e8a31292c20cd782482
Juri Furer <juri.furer@dvbern.ch>
2024-08-12 13:00:43 +0200

    feat(KSTIP-1215): formatting

--- backend/src/main/java/ch/dvbern/stip/api/steuerdaten/service/SteuerdatenMapper.java
+++ backend/src/main/java/ch/dvbern/stip/api/steuerdaten/service/SteuerdatenMapper.java
@@ -1,17 +1,16 @@
 package ch.dvbern.stip.api.steuerdaten.service;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import ch.dvbern.stip.api.common.service.MappingConfig;
 import ch.dvbern.stip.api.steuerdaten.entity.Steuerdaten;
 import ch.dvbern.stip.generated.dto.SteuerdatenDto;
 import ch.dvbern.stip.generated.dto.SteuerdatenUpdateDto;
 import jakarta.ws.rs.NotFoundException;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 @Mapper(config = MappingConfig.class)
 public interface SteuerdatenMapper {
@@ -20,8 +19,11 @@ public interface SteuerdatenMapper {
     SteuerdatenDto toDto(Steuerdaten steuerdaten);
 
     Steuerdaten partialUpdate(SteuerdatenUpdateDto steuerdatenDto, @MappingTarget Steuerdaten steuerdaten);
-    default Set<Steuerdaten> map(final List<SteuerdatenUpdateDto> steuerdatenUpdateDtos,
-                                 final @MappingTarget Set<Steuerdaten> steuerdatenSet) {
+
+    default Set<Steuerdaten> map(
+        final List<SteuerdatenUpdateDto> steuerdatenUpdateDtos,
+        final @MappingTarget Set<Steuerdaten> steuerdatenSet
+    ) {
         if (steuerdatenUpdateDtos.isEmpty()) {
             steuerdatenSet.clear();
         }
