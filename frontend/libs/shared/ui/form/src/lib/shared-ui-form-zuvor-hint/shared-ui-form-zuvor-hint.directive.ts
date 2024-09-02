e5f044827fa4c72b17ec9139576f5078cde84487
Severin Spoerri <severin.spoerri@dvbern.ch>
2024-09-02 11:28:46 +0200

    feat(KSTIP-1181): zuvor hint manage existing hints with css, add blue dot instead of text prefix

--- frontend/libs/shared/ui/form/src/lib/shared-ui-form-zuvor-hint/shared-ui-form-zuvor-hint.directive.ts
+++ frontend/libs/shared/ui/form/src/lib/shared-ui-form-zuvor-hint/shared-ui-form-zuvor-hint.directive.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   Directive,
   Input,
   Renderer2,
@@ -7,79 +6,42 @@ import {
   ViewContainerRef,
   inject,
 } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
+/**
+ * @description
+ * Directive to show a the previous values of Formfields.
+ * - Must be placed on a mat-hint component.
+ * - Must be used in combination within a mat-form-field.
+ * - If there is another hint, the hint will be replaced if there is a previous value.
+ * Ohter hints must be placed after this hint element.
+ * - The following hit must also have the attribute `align="end"` so material does not throw an error.
+ *
+ * @example
+ * <mat-hint
+ *  *dvZuvorHint="previousValue"
+ *  data-testid="form-person-sozialversicherungsnummer-zuvor-hint"
+ *  class="zuvor-hint"
+ * >
+ * </mat-hint>
+ * <mat-hint align="end" translate>shared.form.einnahmenkosten.nettoerwerbseinkommen.info</mat-hint>
+ */
 @Directive({
   selector: '[dvZuvorHint]',
   standalone: true,
 })
-export class SharedUiZuvorHintDirective implements AfterViewInit {
+export class SharedUiZuvorHintDirective {
   private viewContainerRef = inject(ViewContainerRef);
   private renderer = inject(Renderer2);
   private templateRef = inject(TemplateRef);
 
-  translate = inject(TranslateService);
-  private _value: string | undefined;
-
-  private spacer: Node | undefined = undefined;
-  private prevHint: Node | undefined = undefined;
-
   @Input() set dvZuvorHint(value: string | undefined) {
-    this._value = value;
-
     if (value) {
-      const translated = this.translate.instant('shared.form.zuvor.hint', {
-        value: value,
-      });
       const ref = this.viewContainerRef.createEmbeddedView(this.templateRef);
       if (ref.rootNodes.length > 0) {
-        ref.rootNodes[0] as HTMLElement;
-        this.renderer.setProperty(ref.rootNodes[0], 'innerText', translated);
+        this.renderer.setProperty(ref.rootNodes[0], 'innerText', value);
       }
-      this.hideExisitingHint();
     } else {
       this.viewContainerRef.clear();
-      this.showExisitingHint();
-    }
-  }
-
-  ngAfterViewInit() {
-    if (this._value) {
-      this.hideExisitingHint();
-    }
-  }
-
-  hideExisitingHint() {
-    const parentNodes: NodeList | undefined =
-      this.viewContainerRef.element.nativeElement.parentNode?.childNodes;
-
-    if (parentNodes) {
-      this.prevHint = Array.from(parentNodes).find(
-        (node) =>
-          node.nodeType === Node.ELEMENT_NODE &&
-          (node as Element).classList.contains('mat-mdc-form-field-hint') &&
-          !(node as Element).classList.contains('zuvor-hint'),
-      );
-
-      this.spacer = Array.from(parentNodes).find(
-        (node) =>
-          node.nodeType === Node.ELEMENT_NODE &&
-          (node as Element).classList.contains(
-            'mat-mdc-form-field-hint-spacer',
-          ),
-      );
-    }
-
-    if (this.prevHint && this.spacer) {
-      this.renderer.addClass(this.prevHint, 'd-none');
-      this.renderer.addClass(this.spacer, 'd-none');
-    }
-  }
-
-  showExisitingHint() {
-    if (this.prevHint && this.spacer) {
-      this.renderer.removeClass(this.prevHint, 'd-none');
-      this.renderer.removeClass(this.spacer, 'd-none');
     }
   }
 }
