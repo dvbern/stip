9b32dec46e0aa208b1fde70edf1cdd452b13b812
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-09-09 12:55:44 +0200

    feat(KSTIP-1229): Move token "Basic" of header in Provider-Class

--- backend/src/main/java/ch/dvbern/stip/api/sap/util/RequestHeaderProviderUtil.java
+++ backend/src/main/java/ch/dvbern/stip/api/sap/util/RequestHeaderProviderUtil.java
@@ -4,6 +4,6 @@ import org.eclipse.microprofile.config.ConfigProvider;
 
 public interface RequestHeaderProviderUtil {
     default String getAuthHeader(){
-        return ConfigProvider.getConfig().getValue("kstip.sap.auth-header-value",String.class);
+        return "Basic ".concat(ConfigProvider.getConfig().getValue("kstip.sap.auth-header-value",String.class));
     }
 }
