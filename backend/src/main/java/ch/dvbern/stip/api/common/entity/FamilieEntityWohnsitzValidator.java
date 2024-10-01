6503abf832cfd4f56e8bedec5a0a2b80efa0a9ee
Juri Furer <juri.furer@dvbern.ch>
2024-10-01 14:30:52 +0200

    feat(KSTIP-1335): minor formatting changes

--- backend/src/main/java/ch/dvbern/stip/api/common/entity/FamilieEntityWohnsitzValidator.java
+++ backend/src/main/java/ch/dvbern/stip/api/common/entity/FamilieEntityWohnsitzValidator.java
@@ -1,51 +1,56 @@
 package ch.dvbern.stip.api.common.entity;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
+
 import ch.dvbern.stip.api.common.type.Wohnsitz;
 import ch.dvbern.stip.api.familiensituation.entity.Familiensituation;
 import ch.dvbern.stip.api.familiensituation.type.ElternAbwesenheitsGrund;
 import jakarta.enterprise.context.ApplicationScoped;
-
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.Optional;
 
 @ApplicationScoped
 public class FamilieEntityWohnsitzValidator {
     // map of valid Wohnsitz values when ElternteilUnbekanntVerstorben is true
     private static final Map<Wohnsitz, Optional<Boolean>> ELTERNTEIL_ABSENT_WOHNSITUATION_VALID_MAP = Map.of(
         // will be evaluated
-        Wohnsitz.MUTTER_VATER,Optional.empty(),
-        Wohnsitz.FAMILIE,Optional.of(false),
+        Wohnsitz.MUTTER_VATER, Optional.empty(),
+        Wohnsitz.FAMILIE, Optional.of(false),
         // every other entry is valid = true
-        Wohnsitz.EIGENER_HAUSHALT,Optional.of(true)
-        );
+        Wohnsitz.EIGENER_HAUSHALT, Optional.of(true)
+    );
 
     // map of valid Wohnsitz values when ElternVerheiratetZusammen is false
-    private static final Map<Wohnsitz,Boolean> ELTERN_SEPARATED_WOHNSITUATION_VALID_MAP = Map.of(
-        Wohnsitz.FAMILIE,false,
+    private static final Map<Wohnsitz, Boolean> ELTERN_SEPARATED_WOHNSITUATION_VALID_MAP = Map.of(
+        Wohnsitz.FAMILIE, false,
         // every other entry is valid = true
-        Wohnsitz.MUTTER_VATER,true,
-        Wohnsitz.EIGENER_HAUSHALT,true
+        Wohnsitz.MUTTER_VATER, true,
+        Wohnsitz.EIGENER_HAUSHALT, true
     );
 
     public boolean isValid(AbstractFamilieEntity familieEntity, Familiensituation familiensituation) {
-        if((familiensituation == null) || (familieEntity == null)){
+        if ((familiensituation == null) || (familieEntity == null)) {
             return true;
         }
-        if(familiensituation.getElternVerheiratetZusammen()){
+
+        if (familiensituation.getElternVerheiratetZusammen()) {
             // when elterns are together or married, the option MUTTER_VATER is not available
             return familieEntity.getWohnsitz() != Wohnsitz.MUTTER_VATER;
         }
-        if(familiensituation.getElternteilUnbekanntVerstorben()){
+
+        if (familiensituation.getElternteilUnbekanntVerstorben()) {
             return ELTERNTEIL_ABSENT_WOHNSITUATION_VALID_MAP.get(familieEntity.getWohnsitz())
-                .orElseGet(() -> isWohnsitzanteilValidWhenOneEltnernteilIsAbsent(familieEntity,familiensituation));
+                .orElseGet(() -> isWohnsitzanteilValidWhenOneElternteilIsAbsent(familieEntity, familiensituation));
         }
+
         return ELTERN_SEPARATED_WOHNSITUATION_VALID_MAP.get(familieEntity.getWohnsitz());
     }
 
-    private boolean isWohnsitzanteilValidWhenOneEltnernteilIsAbsent(AbstractFamilieEntity familieEntity,
-                                                                    Familiensituation familiensituation) {
-        if(!familiensituation.getElternteilUnbekanntVerstorben()){
+    private boolean isWohnsitzanteilValidWhenOneElternteilIsAbsent(
+        AbstractFamilieEntity familieEntity,
+        Familiensituation familiensituation
+    ) {
+        if (!familiensituation.getElternteilUnbekanntVerstorben()) {
             return true;
         }
 
@@ -54,26 +59,28 @@ public class FamilieEntityWohnsitzValidator {
         boolean isAnteilVater100Percent = familieEntity.getWohnsitzAnteilVater()
             .compareTo(BigDecimal.valueOf(100)) == 0;
 
-        final boolean isMutterAbsent = familiensituation.getMutterUnbekanntVerstorben() == ElternAbwesenheitsGrund.VERSTORBEN
-            || familiensituation.getMutterUnbekanntVerstorben() == ElternAbwesenheitsGrund.UNBEKANNT;
-        final boolean isVaterAbsent = familiensituation.getVaterUnbekanntVerstorben() == ElternAbwesenheitsGrund.UNBEKANNT
-            || familiensituation.getVaterUnbekanntVerstorben() == ElternAbwesenheitsGrund.VERSTORBEN;
+        final boolean isMutterAbsent =
+            familiensituation.getMutterUnbekanntVerstorben() == ElternAbwesenheitsGrund.VERSTORBEN
+                || familiensituation.getMutterUnbekanntVerstorben() == ElternAbwesenheitsGrund.UNBEKANNT;
+        final boolean isVaterAbsent =
+            familiensituation.getVaterUnbekanntVerstorben() == ElternAbwesenheitsGrund.UNBEKANNT
+                || familiensituation.getVaterUnbekanntVerstorben() == ElternAbwesenheitsGrund.VERSTORBEN;
         final boolean isMutterExisting = familiensituation.getMutterUnbekanntGrund() == null ||
             familiensituation.getMutterUnbekanntVerstorben().equals(ElternAbwesenheitsGrund.WEDER_NOCH);
         final boolean isVaterExisting = familiensituation.getVaterUnbekanntGrund() == null
-        || familiensituation.getVaterUnbekanntVerstorben().equals(ElternAbwesenheitsGrund.WEDER_NOCH);
+            || familiensituation.getVaterUnbekanntVerstorben().equals(ElternAbwesenheitsGrund.WEDER_NOCH);
 
-            // mutter is absent - vater must be 100%
-            if(isMutterAbsent && isVaterExisting){
-                return isAnteilVater100Percent;
-            }
-            // vater is absent - mutter must be 100%
-            else if(isVaterAbsent && isMutterExisting){
-                    return isAnteilMutter100Percent;
-            } else {
-                // one of both has to be 100%
-                return isAnteilMutter100Percent
-                    || isAnteilVater100Percent;
-            }
+        // mutter is absent - vater must be 100%
+        if (isMutterAbsent && isVaterExisting) {
+            return isAnteilVater100Percent;
+        }
+        // vater is absent - mutter must be 100%
+        else if (isVaterAbsent && isMutterExisting) {
+            return isAnteilMutter100Percent;
+        } else {
+            // one of both has to be 100%
+            return isAnteilMutter100Percent
+                || isAnteilVater100Percent;
+        }
     }
 }
