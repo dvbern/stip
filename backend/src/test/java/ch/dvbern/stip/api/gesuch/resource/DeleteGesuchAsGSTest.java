75ba9b0c4438b6bf66175c7900e1893047261454
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-10-11 08:47:20 +0200

    feat(KSTIP-1527): add endpoint tests for GS

--- backend/src/test/java/ch/dvbern/stip/api/gesuch/resource/DeleteGesuchAsGSTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuch/resource/DeleteGesuchAsGSTest.java
@@ -2,14 +2,12 @@ package ch.dvbern.stip.api.gesuch.resource;
 
 import ch.dvbern.stip.api.benutzer.util.TestAsAdmin;
 import ch.dvbern.stip.api.benutzer.util.TestAsGesuchsteller;
-import ch.dvbern.stip.api.benutzer.util.TestAsSachbearbeiter;
+import ch.dvbern.stip.api.benutzer.util.TestAsGesuchsteller2;
 import ch.dvbern.stip.api.util.*;
 import ch.dvbern.stip.generated.api.DokumentApiSpec;
 import ch.dvbern.stip.generated.api.FallApiSpec;
 import ch.dvbern.stip.generated.api.GesuchApiSpec;
 import ch.dvbern.stip.generated.dto.GesuchDtoSpec;
-import ch.dvbern.stip.generated.dto.GesuchstatusDtoSpec;
-import ch.dvbern.stip.generated.dto.GetGesucheSBQueryTypeDtoSpec;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.core.Response;
@@ -17,12 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.util.Arrays;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 
 @QuarkusTestResource(TestDatabaseEnvironment.class)
 @QuarkusTestResource(TestClamAVEnvironment.class)
@@ -32,7 +24,7 @@ import static org.hamcrest.Matchers.not;
 @RequiredArgsConstructor
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Slf4j
-public class DeleteGesuchTest {
+public class DeleteGesuchAsGSTest {
     private final GesuchApiSpec gesuchApiSpec = GesuchApiSpec.gesuch(RequestSpecUtil.quarkusSpec());
     private final DokumentApiSpec dokumentApiSpec = DokumentApiSpec.dokument(RequestSpecUtil.quarkusSpec());
     private final FallApiSpec fallApiSpec = FallApiSpec.fall(RequestSpecUtil.quarkusSpec());
@@ -54,26 +46,27 @@ public class DeleteGesuchTest {
     }
 
     @Test
-    @TestAsGesuchsteller
+    @TestAsGesuchsteller2
     @Order(3)
-    @StepwiseExtension.AlwaysRun
-    void deleteGesuchAsGS() {
-        TestUtil.deleteGesuch(gesuchApiSpec, gesuch.getId());
-    }
-
-    //@Test
-    @TestAsGesuchsteller
-    //@Order(4)
-    void gesuchEinreichen() {
-        gesuchApiSpec.gesuchEinreichen()
+    void deleteGesuchAsOtherGS() {
+        gesuchApiSpec.deleteGesuch()
             .gesuchIdPath(gesuch.getId())
             .execute(TestUtil.PEEK_IF_ENV_SET)
             .then()
             .assertThat()
-            .statusCode(Response.Status.ACCEPTED.getStatusCode());
+            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());    }
+
+    @Test
+    @TestAsGesuchsteller
+    @Order(4)
+    void deleteGesuchAsGS() {
+        TestUtil.deleteGesuch(gesuchApiSpec, gesuch.getId());
     }
 
-    //Test setup for next delete operation
+    /**
+     * Test setup for next delete operation
+     */
+
     @Test
     @TestAsGesuchsteller
     @Order(5)
@@ -91,7 +84,7 @@ public class DeleteGesuchTest {
     @Test
     @TestAsGesuchsteller
     @Order(7)
-    void gesuchEinreichen2() {
+    void gesuchEinreichen() {
         gesuchApiSpec.gesuchEinreichen()
             .gesuchIdPath(gesuch.getId())
             .execute(TestUtil.PEEK_IF_ENV_SET)
@@ -100,12 +93,16 @@ public class DeleteGesuchTest {
             .statusCode(Response.Status.ACCEPTED.getStatusCode());
     }
 
-    //@Test
-    @TestAsAdmin
-   // @Order(8)
-    @StepwiseExtension.AlwaysRun
-    void deleteGesuch() {
-        TestUtil.deleteGesuch(gesuchApiSpec, gesuch.getId());
+    @Test
+    @TestAsGesuchsteller
+    @Order(8)
+    void deleteGesuchAsGSInStatusEingereicht() {
+        gesuchApiSpec.deleteGesuch()
+            .gesuchIdPath(gesuch.getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Response.Status.UNAUTHORIZED.getStatusCode());
     }
 
 }
