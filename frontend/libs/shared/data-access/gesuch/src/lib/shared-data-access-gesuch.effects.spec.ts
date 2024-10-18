67170d15a410eddbfc7ea2bb8b2c0153962d87b2
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-10-18 14:27:53 +0200

    fix(KSTIP-1538): removed reference to old complex gesuche load test
    As it is not necessary with the new structure where the request is debounced by inputs

--- frontend/libs/shared/data-access/gesuch/src/lib/shared-data-access-gesuch.effects.spec.ts
+++ frontend/libs/shared/data-access/gesuch/src/lib/shared-data-access-gesuch.effects.spec.ts
@@ -40,67 +40,6 @@ describe('sharedDataAccessGesuch Effects', () => {
       });
     });
   });
-
-  //   it('loads gesuch initially and then debounced', () => {
-  //     scheduler.run(({ expectObservable, hot, cold }) => {
-  //       const gesuch1 = { id: '1' } as SharedModelGesuch;
-  //       const gesuch2 = { id: '2' } as SharedModelGesuch;
-
-  //       const gesuchServiceMock = mockGesuchService({
-  //         getGesucheSb$: (
-  //           ...args: Parameters<GesuchService['getGesucheSb$']>
-  //         ) => {
-  //           if (args[0].getGesucheSBQueryType === 'ALLE_BEARBEITBAR_MEINE') {
-  //             return cold('a', { a: [gesuch1] });
-  //           }
-  //           return cold('a', { a: [gesuch1, gesuch2] });
-  //         },
-  //       });
-
-  //       const debounced = `${LOAD_ALL_DEBOUNCE_TIME}ms`;
-
-  //       /**
-  //        * (A) Initial load                                          -> should fire
-  //        * (B) Multiple debounced loads with same filter             -> should fire (only once)
-  //        * (B) Another load with same filter but after debounce time -> should not fire (as it hasn't changed)
-  //        * (C) Debounced load with different filter                  -> should fire
-  //        */
-  //       const actionsMock$ = hot(`abb ${debounced} --bc`, {
-  //         a: SharedDataAccessGesuchEvents.loadAll({
-  //           query: 'ALLE_BEARBEITBAR_MEINE',
-  //         }),
-  //         b: SharedDataAccessGesuchEvents.loadAllDebounced({
-  //           query: 'ALLE',
-  //         }),
-  //         c: SharedDataAccessGesuchEvents.loadAllDebounced({
-  //           query: 'ALLE_BEARBEITBAR_MEINE',
-  //         }),
-  //       });
-
-  //       const effectStream$ = loadAllGesuchs(
-  //         actionsMock$,
-  //         gesuchServiceMock,
-  //         storeUtilMock,
-  //       );
-
-  //       /**
-  //        * (A) Initial load
-  //        * (B) Only one debounced load with more gesuchs
-  //        * (A) Debounced load with less gesuchs again
-  //        */
-  //       expectObservable(effectStream$).toBe(
-  //         `a- ${debounced} b ${debounced} ---a`,
-  //         {
-  //           a: SharedDataAccessGesuchEvents.gesuchsLoadedSuccess({
-  //             gesuchs: [gesuch1],
-  //           }),
-  //           b: SharedDataAccessGesuchEvents.gesuchsLoadedSuccess({
-  //             gesuchs: [gesuch1, gesuch2],
-  //           }),
-  //         },
-  //       );
-  //     });
-  //   });
 });
 
 const mockGesuchService = (
