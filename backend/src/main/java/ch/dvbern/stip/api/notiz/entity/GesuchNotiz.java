23843d94f87a1d2d19e9c6098de2caa8c7af0e8a
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-10-31 16:15:11 +0100

    feat(KSTIP-1018): use constant for max length + use allowAll annotation

--- backend/src/main/java/ch/dvbern/stip/api/notiz/entity/GesuchNotiz.java
+++ backend/src/main/java/ch/dvbern/stip/api/notiz/entity/GesuchNotiz.java
@@ -6,11 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.envers.Audited;
 
 import java.util.UUID;
+
+import static ch.dvbern.stip.api.common.util.Constants.DB_DEFAULT_MAX_LENGTH;
 
 @Getter
 @Setter
@@ -27,9 +30,12 @@ public class GesuchNotiz extends AbstractMandantEntity {
     @Column(name = "gesuch_id")
     private UUID gesuchId;
 
-    @Column(name = "betreff", length = 100)
+    @Column(name = "betreff")
+    @Size(max = DB_DEFAULT_MAX_LENGTH)
+
     private String betreff;
 
     @Column(name = "text")
+    @Size(max = DB_DEFAULT_MAX_LENGTH)
     private String text;
 }
--- backend/src/main/java/ch/dvbern/stip/api/notiz/resource/GesuchNotizResourceImpl.java
+++ backend/src/main/java/ch/dvbern/stip/api/notiz/resource/GesuchNotizResourceImpl.java
@@ -2,6 +2,7 @@ package ch.dvbern.stip.api.notiz.resource;
 
 import java.util.UUID;
 
+import ch.dvbern.stip.api.common.authorization.AllowAll;
 import ch.dvbern.stip.api.common.authorization.GesuchNotizAuthorizer;
 import ch.dvbern.stip.api.common.util.OidcConstants;
 import ch.dvbern.stip.api.notiz.service.GesuchNotizService;
@@ -19,22 +20,23 @@ public class GesuchNotizResourceImpl implements GesuchNotizResource {
     private final GesuchNotizService service;
     private final GesuchNotizAuthorizer authorizer;
 
+    @AllowAll
     @RolesAllowed(OidcConstants.ROLE_SACHBEARBEITER)
     @Override
     public Response createNotiz(GesuchNotizCreateDto gesuchNotizCreateDto) {
-        authorizer.allowAllow();
         final var notiz = service.create(gesuchNotizCreateDto);
         return Response.ok(notiz).build();
     }
 
+    @AllowAll
     @RolesAllowed(OidcConstants.ROLE_SACHBEARBEITER)
     @Override
     public Response deleteNotiz(UUID notizId) {
-        authorizer.allowAllow();
         service.delete(notizId);
         return Response.noContent().build();
     }
 
+    @AllowAll
     @RolesAllowed(OidcConstants.ROLE_SACHBEARBEITER)
     @Override
     public Response getNotiz(UUID notizId) {
@@ -42,18 +44,18 @@ public class GesuchNotizResourceImpl implements GesuchNotizResource {
         return Response.ok(service.getById(notizId)).build();
     }
 
+    @AllowAll
     @RolesAllowed(OidcConstants.ROLE_SACHBEARBEITER)
     @Override
     public Response getNotizen(UUID gesuchId) {
-        authorizer.allowAllow();
         final var notizen = service.getAllByGesuchId(gesuchId);
         return Response.ok(notizen).build();
     }
 
+    @AllowAll
     @RolesAllowed(OidcConstants.ROLE_SACHBEARBEITER)
     @Override
     public Response updateNotiz(GesuchNotizUpdateDto gesuchNotizUpdateDto) {
-        authorizer.allowAllow();
         final var notiz = service.update(gesuchNotizUpdateDto);
         return Response.ok(notiz).build();
     }
