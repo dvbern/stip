d6780e4e4afa14c227c74d62fe575a9ce349ac35
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-10-31 16:21:45 +0100

    feat(KSTIP-1018): remove useless @Valid annotation

--- backend/src/main/java/ch/dvbern/stip/api/notiz/service/GesuchNotizService.java
+++ backend/src/main/java/ch/dvbern/stip/api/notiz/service/GesuchNotizService.java
@@ -9,7 +9,6 @@ import ch.dvbern.stip.generated.dto.GesuchNotizDto;
 import ch.dvbern.stip.generated.dto.GesuchNotizUpdateDto;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,36 +18,36 @@ public class GesuchNotizService {
     private final GesuchNotizMapper gesuchNotizMapper;
 
     @Transactional
-    public List<GesuchNotizDto> getAllByGesuchId(@Valid final UUID gesuchId) {
+    public List<GesuchNotizDto> getAllByGesuchId(final UUID gesuchId) {
         return gesuchNotizRepository.findAllByGesuchId(gesuchId).stream().map(gesuchNotizMapper::toDto).toList();
     }
 
     @Transactional
-    public GesuchNotizDto getById(@Valid final UUID notizId) {
+    public GesuchNotizDto getById(final UUID notizId) {
         final var notiz = gesuchNotizRepository.requireById(notizId);
         return gesuchNotizMapper.toDto(notiz);
     }
 
     @Transactional
-    public void delete(@Valid final UUID gesuchNotizId) {
+    public void delete(final UUID gesuchNotizId) {
         final var notiz = gesuchNotizRepository.requireById(gesuchNotizId);
         gesuchNotizRepository.delete(notiz);
     }
 
     @Transactional
-    public void deleteAllByGesuchId(@Valid final UUID gesuchId) {
+    public void deleteAllByGesuchId(final UUID gesuchId) {
         gesuchNotizRepository.findAllByGesuchId(gesuchId).forEach(gesuchNotizRepository::delete);
     }
 
     @Transactional
-    public GesuchNotizDto create(@Valid final GesuchNotizCreateDto createDto) {
+    public GesuchNotizDto create(final GesuchNotizCreateDto createDto) {
         final var notiz = gesuchNotizMapper.toEntity(createDto);
         gesuchNotizRepository.persistAndFlush(notiz);
         return gesuchNotizMapper.toDto(notiz);
     }
 
     @Transactional
-    public GesuchNotizDto update(@Valid final GesuchNotizUpdateDto gesuchNotizUpdateDto) {
+    public GesuchNotizDto update(final GesuchNotizUpdateDto gesuchNotizUpdateDto) {
         var gesuchNotiz = gesuchNotizRepository.requireById(gesuchNotizUpdateDto.getId());
         gesuchNotizMapper.partialUpdate(gesuchNotizUpdateDto, gesuchNotiz);
         return gesuchNotizMapper.toDto(gesuchNotiz);
