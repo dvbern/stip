6485752de3590fca74834f9c8b32afe12e8cc160
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-11-20 15:56:22 +0100

    fix(KSTIP-1056): show unauthorized site if non sb/jurist/admin... user opens the SB App

--- frontend/apps/sachbearbeitung-app/src/app/app.routes.ts
+++ frontend/apps/sachbearbeitung-app/src/app/app.routes.ts
@@ -5,96 +5,112 @@ import { hasRoles, isAllowedTo } from '@dv/shared/pattern/status-guard';
 
 export const appRoutes: Route[] = [
   {
-    path: 'sachbearbeitung-app-feature-infos-notizen',
+    path: 'unauthorized',
     loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/infos-notizen').then(
-        (m) => m.sachbearbeitungAppFeatureInfosNotizenRoutes,
+      import('@dv/sachbearbeitung-app/feature/unauthorized').then(
+        (m) => m.sachbearbeitungAppFeatureUnauthorizedRoutes,
       ),
   },
   {
-    path: 'sachbearbeitung-app-feature-administration-sozialdienst',
-    loadChildren: () =>
-      import(
-        '@dv/sachbearbeitung-app/feature/administration-sozialdienst'
-      ).then(
-        (m) => m.sachbearbeitungAppFeatureAdministrationSozialdienstRoutes,
-      ),
-  },
-  {
-    path: 'sachbearbeitung-app-feature-infos-protokoll',
-    loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/infos-protokoll').then(
-        (m) => m.sachbearbeitungAppFeatureInfosProtokollRoutes,
-      ),
-  },
-  {
-    path: 'administration',
-    canActivate: [hasBenutzer, hasRoles(['Admin'])],
-    title: 'sachbearbeitung-app.admin.title',
-    loadComponent: () =>
-      import('@dv/sachbearbeitung-app/feature/administration').then(
-        (m) => m.SachbearbeitungAppFeatureAdministrationComponent,
-      ),
-    loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/administration').then(
-        (m) => m.sachbearbeitungAppFeatureAdministrationRoutes,
-      ),
-  },
-  {
-    path: 'sachbearbeitung-app-feature-cockpit',
-    canActivate: [hasBenutzer],
-    title: 'sachbearbeitung-app.cockpit.title',
-    loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/cockpit').then(
-        (m) => m.sachbearbeitungAppFeatureCockpitRoutes,
-      ),
-  },
-  {
-    path: 'gesuch',
-    canActivate: [hasBenutzer],
-    title: 'sachbearbeitung-app.gesuch-form.title',
-    loadComponent: () =>
-      import('@dv/sachbearbeitung-app/feature/gesuch-form').then(
-        (m) => m.SachbearbeitungAppFeatureGesuchFormComponent,
-      ),
-    loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/gesuch-form').then(
-        (m) => m.sachbearbeitungAppFeatureGesuchFormRoutes,
-      ),
-  },
-  {
-    path: 'verfuegung',
-    canActivate: [hasBenutzer, isAllowedTo('viewVerfuegung')],
-    title: 'sachbearbeitung-app.verfuegung.title',
-    loadComponent: () =>
-      import('@dv/sachbearbeitung-app/feature/verfuegung').then(
-        (m) => m.SachbearbeitungAppFeatureVerfuegungComponent,
-      ),
-    loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/verfuegung').then(
-        (m) => m.sachbearbeitungAppFeatureVerfuegungRoutes,
-      ),
-  },
-  {
-    path: 'infos',
-    canActivate: [hasBenutzer],
-    title: 'sachbearbeitung-app.infos.title',
-    loadComponent: () =>
-      import('@dv/sachbearbeitung-app/feature/infos').then(
-        (m) => m.SachbearbeitungAppFeatureInfosComponent,
-      ),
-    loadChildren: () =>
-      import('@dv/sachbearbeitung-app/feature/infos').then(
-        (m) => m.sachbearbeitungAppFeatureInfosRoutes,
-      ),
-  },
-  {
-    path: 'download',
-    canActivate: [hasBenutzer],
-    loadChildren: () =>
-      import('@dv/shared/feature/download').then(
-        (m) => m.sharedFeatureDownloadRoutes,
-      ),
+    path: '',
+    canActivate: [
+      hasBenutzer,
+      hasRoles(['Admin', 'Jurist', 'Sachbearbeiter'], '/unauthorized'),
+    ],
+    children: [
+      {
+        path: 'sachbearbeitung-app-feature-infos-notizen',
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/infos-notizen').then(
+            (m) => m.sachbearbeitungAppFeatureInfosNotizenRoutes,
+          ),
+      },
+      {
+        path: 'sachbearbeitung-app-feature-administration-sozialdienst',
+        loadChildren: () =>
+          import(
+            '@dv/sachbearbeitung-app/feature/administration-sozialdienst'
+          ).then(
+            (m) => m.sachbearbeitungAppFeatureAdministrationSozialdienstRoutes,
+          ),
+      },
+      {
+        path: 'sachbearbeitung-app-feature-infos-protokoll',
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/infos-protokoll').then(
+            (m) => m.sachbearbeitungAppFeatureInfosProtokollRoutes,
+          ),
+      },
+      {
+        path: 'administration',
+        canActivate: [hasBenutzer, hasRoles(['Admin'])],
+        title: 'sachbearbeitung-app.admin.title',
+        loadComponent: () =>
+          import('@dv/sachbearbeitung-app/feature/administration').then(
+            (m) => m.SachbearbeitungAppFeatureAdministrationComponent,
+          ),
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/administration').then(
+            (m) => m.sachbearbeitungAppFeatureAdministrationRoutes,
+          ),
+      },
+      {
+        path: 'sachbearbeitung-app-feature-cockpit',
+        canActivate: [hasBenutzer],
+        title: 'sachbearbeitung-app.cockpit.title',
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/cockpit').then(
+            (m) => m.sachbearbeitungAppFeatureCockpitRoutes,
+          ),
+      },
+      {
+        path: 'gesuch',
+        canActivate: [hasBenutzer],
+        title: 'sachbearbeitung-app.gesuch-form.title',
+        loadComponent: () =>
+          import('@dv/sachbearbeitung-app/feature/gesuch-form').then(
+            (m) => m.SachbearbeitungAppFeatureGesuchFormComponent,
+          ),
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/gesuch-form').then(
+            (m) => m.sachbearbeitungAppFeatureGesuchFormRoutes,
+          ),
+      },
+      {
+        path: 'verfuegung',
+        canActivate: [hasBenutzer, isAllowedTo('viewVerfuegung')],
+        title: 'sachbearbeitung-app.verfuegung.title',
+        loadComponent: () =>
+          import('@dv/sachbearbeitung-app/feature/verfuegung').then(
+            (m) => m.SachbearbeitungAppFeatureVerfuegungComponent,
+          ),
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/verfuegung').then(
+            (m) => m.sachbearbeitungAppFeatureVerfuegungRoutes,
+          ),
+      },
+      {
+        path: 'infos',
+        canActivate: [hasBenutzer],
+        title: 'sachbearbeitung-app.infos.title',
+        loadComponent: () =>
+          import('@dv/sachbearbeitung-app/feature/infos').then(
+            (m) => m.SachbearbeitungAppFeatureInfosComponent,
+          ),
+        loadChildren: () =>
+          import('@dv/sachbearbeitung-app/feature/infos').then(
+            (m) => m.sachbearbeitungAppFeatureInfosRoutes,
+          ),
+      },
+      {
+        path: 'download',
+        canActivate: [hasBenutzer],
+        loadChildren: () =>
+          import('@dv/shared/feature/download').then(
+            (m) => m.sharedFeatureDownloadRoutes,
+          ),
+      },
+    ],
   },
 ];
 
