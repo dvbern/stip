079909c2fadd6e32d5192a5cdb31feafde5eb9b0
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2024-11-28 13:23:15 +0100

    feat(KSTIP-1644): add other templates

--- backend/src/main/java/ch/dvbern/stip/stipdecision/decider/BernStipDecider.java
+++ backend/src/main/java/ch/dvbern/stip/stipdecision/decider/BernStipDecider.java
@@ -156,6 +156,85 @@ public class BernStipDecider implements BaseStipDecider {
         };
     }
 
+    private static TemplateInstance getArt32BBVText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.art32BBVFr();
+            case DEUTSCH -> Templates.art32BBVDe();
+        };
+    }
+
+    private static TemplateInstance getAusbildungPBIText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.ausbildungBPI1Fr();
+            case DEUTSCH -> Templates.ausbildungBPI1De();
+        };
+    }
+
+    private static TemplateInstance getKeinWohnsitzImKantonBEText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.keinWohnsitzImKantonBEFr();
+            case DEUTSCH -> Templates.keinWohnsitzImKantonBEDe();
+        };
+    }
+
+    private static TemplateInstance getMehrereAusbildungswechselText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.mehrereAusbildungswechselFr();
+            case DEUTSCH -> Templates.mehrereAusbildungswechselDe();
+        };
+    }
+
+    private static TemplateInstance getNichtBerechtigterPersonenkreisText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.nichtBerechtigterPersonenkreisFr();
+            case DEUTSCH -> Templates.nichtBerechtigterPersonenkreisDe();
+        };
+    }
+
+    private static TemplateInstance getNichtEintretenText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.nichtEintretenFr();
+            case DEUTSCH -> Templates.nichtEintretenDe();
+        };
+    }
+
+    private static TemplateInstance getSchuljahr9Sekstufe1Text(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.schuljahr9Sekstufe1Fr();
+            case DEUTSCH -> Templates.schuljahr9Sekstufe1De();
+        };
+    }
+
+    private static TemplateInstance getZweitausbildungText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.zweitausbildungFr();
+            case DEUTSCH -> Templates.zweitausbildungDe();
+        };
+    }
+
+    private static TemplateInstance getZweiteAusbildungGleicherStufeBVSVorbildungText(
+        final Sprache korrespondenzSprache
+    ) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.zweiteAusbildungGleicherStufeBVSVorbildungFr();
+            case DEUTSCH -> Templates.zweiteAusbildungGleicherStufeBVSVorbildungDe();
+        };
+    }
+
+    private static TemplateInstance getZweiteEBALehreText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.zweiteEbaLehreFr();
+            case DEUTSCH -> Templates.zweiteEbaLehreDe();
+        };
+    }
+
+    private static TemplateInstance getZweitesHochschulstudiumText(final Sprache korrespondenzSprache) {
+        return switch (korrespondenzSprache) {
+            case FRANZOESISCH -> Templates.zweitesHochschulstudiumFr();
+            case DEUTSCH -> Templates.zweitesHochschulstudiumDe();
+        };
+    }
+
     @CheckedTemplate
     static final class Templates {
         private Templates() {}
@@ -179,5 +258,49 @@ public class BernStipDecider implements BaseStipDecider {
         public static native TemplateInstance piaAelter35JahreDe();
 
         public static native TemplateInstance piaAelter35JahreFr();
+
+        public static native TemplateInstance art32BBVDe();
+
+        public static native TemplateInstance art32BBVFr();
+
+        public static native TemplateInstance ausbildungBPI1De();
+
+        public static native TemplateInstance ausbildungBPI1Fr();
+
+        public static native TemplateInstance keinWohnsitzImKantonBEDe();
+
+        public static native TemplateInstance keinWohnsitzImKantonBEFr();
+
+        public static native TemplateInstance mehrereAusbildungswechselDe();
+
+        public static native TemplateInstance mehrereAusbildungswechselFr();
+
+        public static native TemplateInstance nichtBerechtigterPersonenkreisDe();
+
+        public static native TemplateInstance nichtBerechtigterPersonenkreisFr();
+
+        public static native TemplateInstance nichtEintretenDe();
+
+        public static native TemplateInstance nichtEintretenFr();
+
+        public static native TemplateInstance schuljahr9Sekstufe1De();
+
+        public static native TemplateInstance schuljahr9Sekstufe1Fr();
+
+        public static native TemplateInstance zweitausbildungDe();
+
+        public static native TemplateInstance zweitausbildungFr();
+
+        public static native TemplateInstance zweiteAusbildungGleicherStufeBVSVorbildungDe();
+
+        public static native TemplateInstance zweiteAusbildungGleicherStufeBVSVorbildungFr();
+
+        public static native TemplateInstance zweiteEbaLehreDe();
+
+        public static native TemplateInstance zweiteEbaLehreFr();
+
+        public static native TemplateInstance zweitesHochschulstudiumDe();
+
+        public static native TemplateInstance zweitesHochschulstudiumFr();
     }
 }
