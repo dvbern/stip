69538b977867294ff3691e5bdb7be1b57390c50c
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-11-29 10:10:33 +0100

    fix(KSTIP-1434): allow gesuchsteller sozialdienst read

--- backend/src/main/java/ch/dvbern/stip/api/sozialdienst/resource/SozialdienstResourceImpl.java
+++ backend/src/main/java/ch/dvbern/stip/api/sozialdienst/resource/SozialdienstResourceImpl.java
@@ -58,7 +58,7 @@ public class SozialdienstResourceImpl implements SozialdienstResource {
     }
 
     @AllowAll
-    @RolesAllowed({ OidcConstants.ROLE_ADMIN })
+    @RolesAllowed({ OidcConstants.ROLE_GESUCHSTELLER, OidcConstants.ROLE_ADMIN })
     @Override
     public Response getAllSozialdienste() {
         final var sozialdienste = sozialdienstService.getAllSozialdienst();
