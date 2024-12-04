e7ed24e6c1b6a41a95c456c4f688aff9040ac4c6
Philipp Schärer <philipp.schaerer@dvbern.ch>
2024-12-04 15:00:19 +0100

    fix(KSTIP-1585): added test for status changes

--- frontend/libs/sachbearbeitung-app/pattern/gesuch-header/src/lib/sachbearbeitung-app-pattern-gesuch-header/sachbearbeitung-app-pattern-gesuch-header.component.spec.ts
+++ frontend/libs/sachbearbeitung-app/pattern/gesuch-header/src/lib/sachbearbeitung-app-pattern-gesuch-header/sachbearbeitung-app-pattern-gesuch-header.component.spec.ts
@@ -1,18 +1,30 @@
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
 import { provideRouter } from '@angular/router';
 import { provideMockStore } from '@ngrx/store/testing';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { of } from 'rxjs';
 
+import { GesuchStore } from '@dv/sachbearbeitung-app/data-access/gesuch';
 import { provideSharedPatternJestTestSetup } from '@dv/shared/pattern/jest-test-setup';
+import { StatusUebergang } from '@dv/shared/util/gesuch';
 
 import { SachbearbeitungAppPatternGesuchHeaderComponent } from './sachbearbeitung-app-pattern-gesuch-header.component';
 
 describe('SachbearbeitungAppPatternGesuchHeaderComponent', () => {
   let component: SachbearbeitungAppPatternGesuchHeaderComponent;
   let fixture: ComponentFixture<SachbearbeitungAppPatternGesuchHeaderComponent>;
+  const setStatus$ = {
+    BEARBEITUNG_ABSCHLIESSEN: jest.fn(),
+    BEREIT_FUER_BEARBEITUNG: jest.fn(),
+    NEGATIVE_VERFUEGUNG_ERSTELLEN: jest.fn(),
+    VERFUEGT: jest.fn(),
+    VERSENDET: jest.fn(),
+    ZURUECKWEISEN: jest.fn(),
+  } satisfies Record<StatusUebergang, unknown>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -34,8 +46,20 @@ describe('SachbearbeitungAppPatternGesuchHeaderComponent', () => {
           },
         }),
         provideSharedPatternJestTestSetup(),
+        {
+          provide: MatDialog,
+          useValue: {
+            open: () => ({ afterClosed: () => of({}) }),
+          },
+        },
       ],
-    }).compileComponents();
+    })
+      .overrideProvider(GesuchStore, {
+        useValue: {
+          setStatus$,
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(
       SachbearbeitungAppPatternGesuchHeaderComponent,
@@ -50,4 +74,21 @@ describe('SachbearbeitungAppPatternGesuchHeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it.each([
+    ['BEARBEITUNG_ABSCHLIESSEN'],
+    ['BEREIT_FUER_BEARBEITUNG'],
+    ['NEGATIVE_VERFUEGUNG_ERSTELLEN'],
+    ['VERFUEGT'],
+    ['VERSENDET'],
+    ['ZURUECKWEISEN'],
+  ] satisfies [StatusUebergang][])(
+    'should call setStatus$[%s] when using setStatusUebergang',
+    (nextStatus) => {
+      component.setStatusUebergang(nextStatus, 'gesuchId');
+      expect(setStatus$[nextStatus]).toHaveBeenCalledWith({
+        gesuchId: 'gesuchId',
+      });
+    },
+  );
 });
