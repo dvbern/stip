f1242d91023c25e1d05bf326f27e639f686d02a8
Juri Furer <juri.furer@dvbern.ch>
2025-01-09 15:19:23 +0100

    feat(KSTIP-1465): fix oldest query

--- backend/src/main/java/ch/dvbern/stip/api/unterschriftenblatt/service/UnterschriftenblattService.java
+++ backend/src/main/java/ch/dvbern/stip/api/unterschriftenblatt/service/UnterschriftenblattService.java
@@ -28,6 +28,7 @@ import ch.dvbern.stip.api.dokument.entity.Dokument;
 import ch.dvbern.stip.api.dokument.repo.DokumentRepository;
 import ch.dvbern.stip.api.gesuch.entity.Gesuch;
 import ch.dvbern.stip.api.gesuch.repo.GesuchRepository;
+import ch.dvbern.stip.api.gesuch.type.Gesuchstatus;
 import ch.dvbern.stip.api.gesuchtranche.entity.GesuchTranche;
 import ch.dvbern.stip.api.gesuchtranche.repo.GesuchTrancheHistoryRepository;
 import ch.dvbern.stip.api.steuerdaten.service.SteuerdatenTabBerechnungsService;
@@ -118,7 +119,7 @@ public class UnterschriftenblattService {
     @Transactional
     public List<UnterschriftenblattDokumentTyp> getUnterschriftenblaetterToUpload(final Gesuch gesuch) {
         final var initialTranche = gesuchTrancheHistoryRepository
-            .getLatestWhereGesuchStatusChangedToVerfuegt(gesuch.getId());
+            .findOldestHistoricTrancheOfGesuchWhereStatusChangedTo(gesuch.getId(), Gesuchstatus.VERFUEGT);
 
         GesuchTranche toGetFor;
         if (initialTranche.isPresent()) {
