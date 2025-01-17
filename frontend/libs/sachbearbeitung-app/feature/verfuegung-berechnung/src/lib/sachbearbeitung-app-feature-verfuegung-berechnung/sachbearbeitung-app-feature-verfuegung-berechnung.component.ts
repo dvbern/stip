d6a665a95e6588f09e7285897f0cbad8401710c6
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-01-17 13:51:26 +0100

    fix(KSTIP-1406): remove unnecessary console log and full with for abgelehnt block

--- frontend/libs/sachbearbeitung-app/feature/verfuegung-berechnung/src/lib/sachbearbeitung-app-feature-verfuegung-berechnung/sachbearbeitung-app-feature-verfuegung-berechnung.component.ts
+++ frontend/libs/sachbearbeitung-app/feature/verfuegung-berechnung/src/lib/sachbearbeitung-app-feature-verfuegung-berechnung/sachbearbeitung-app-feature-verfuegung-berechnung.component.ts
@@ -69,7 +69,7 @@ export class SachbearbeitungAppFeatureVerfuegungBerechnungComponent {
   berechnungenSig = computed(() => {
     const index = this.indexSig();
     const view = this.berechnungStore.berechnungZusammenfassungViewSig();
-    console.log('view', { view });
+
     if (view.loading || view.berechnungsresultate.length === 0) {
       return { loading: view.loading, list: [] };
     }
