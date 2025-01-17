c4e816cd3b659c3b3d02c29a9a1db017f9fce9a7
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2025-01-17 11:26:24 +0100

    feat(KSTIP-1866): Validator fix für KSTIP-1866

--- backend/src/main/java/ch/dvbern/stip/api/darlehen/entity/DarlehenRequiredIfVolljaehrigConstraintValidator.java
+++ backend/src/main/java/ch/dvbern/stip/api/darlehen/entity/DarlehenRequiredIfVolljaehrigConstraintValidator.java
@@ -19,17 +19,37 @@ package ch.dvbern.stip.api.darlehen.entity;
 
 import java.util.Objects;
 
+import ch.dvbern.stip.api.gesuch.util.GesuchValidatorUtil;
 import ch.dvbern.stip.api.gesuchformular.entity.GesuchFormular;
 import ch.dvbern.stip.api.gesuchformular.util.GesuchFormularCalculationUtil;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import static ch.dvbern.stip.api.common.validation.ValidationsConstant.VALIDATION_DARLEHEN_REQUIRED_VOLLJAEHRIG_MESSAGE;
+
 public class DarlehenRequiredIfVolljaehrigConstraintValidator
     implements ConstraintValidator<DarlehenRequiredIfVolljaehrigConstraint, GesuchFormular> {
+    private String property;
+
+    @Override
+    public void initialize(DarlehenRequiredIfVolljaehrigConstraint constraintAnnotation) {
+        property = constraintAnnotation.property();
+    }
+
     @Override
     public boolean isValid(GesuchFormular value, ConstraintValidatorContext context) {
         final var isNotVolljaehrig = !GesuchFormularCalculationUtil.isPersonInAusbildungVolljaehrig(value);
         // not volljaehrig -> darlehen has to be null, else: darlehen must NOT be null
-        return isNotVolljaehrig ? (Objects.isNull(value.getDarlehen())) : Objects.nonNull(value.getDarlehen());
+        final var isValid =
+            isNotVolljaehrig ? (Objects.isNull(value.getDarlehen())) : Objects.nonNull(value.getDarlehen());
+        if (!isValid) {
+            // validation fails
+            GesuchValidatorUtil.addProperty(
+                context,
+                VALIDATION_DARLEHEN_REQUIRED_VOLLJAEHRIG_MESSAGE,
+                property
+            );
+        }
+        return isValid;
     }
 }
