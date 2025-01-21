960b52773b735114fa524535db6a3d7955f1b575
Juri Furer <juri.furer@dvbern.ch>
2025-01-21 12:54:41 +0100

    feat(KSTIP-1750): re-add wrongly removed interface

--- backend/src/main/java/ch/dvbern/stip/api/common/validation/RequiredDocumentsProducer.java
+++ backend/src/main/java/ch/dvbern/stip/api/common/validation/RequiredDocumentsProducer.java
@@ -15,30 +15,14 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package ch.dvbern.stip.api.ausbildung.entity;
+package ch.dvbern.stip.api.common.validation;
 
-import java.util.HashSet;
 import java.util.Set;
 
-import ch.dvbern.stip.api.common.validation.RequiredDocumentsProducer;
 import ch.dvbern.stip.api.dokument.type.DokumentTyp;
 import ch.dvbern.stip.api.gesuchformular.entity.GesuchFormular;
-import jakarta.enterprise.context.ApplicationScoped;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-@ApplicationScoped
-public class AusbildungRequiredDocumentsProducer implements RequiredDocumentsProducer {
-    @Override
-    public Pair<String, Set<DokumentTyp>> getRequiredDocuments(GesuchFormular formular) {
-        final var ausbildung = formular.getAusbildung();
-        if (ausbildung == null) {
-            return ImmutablePair.of("", Set.of());
-        }
-
-        final var requiredDocs = new HashSet<DokumentTyp>();
-        requiredDocs.add(DokumentTyp.AUSBILDUNG_BESTAETIGUNG_AUSBILDUNGSSTAETTE);
-
-        return ImmutablePair.of("ausbildung", requiredDocs);
-    }
+public interface RequiredDocumentsProducer {
+    Pair<String, Set<DokumentTyp>> getRequiredDocuments(final GesuchFormular formular);
 }
--- backend/src/test/java/ch/dvbern/stip/api/dokument/DokumentResourcesTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/dokument/DokumentResourcesTest.java
@@ -205,6 +205,7 @@ class DokumentResourcesTest {
             .statusCode(Status.OK.getStatusCode())
             .extract()
             .body()
+
             .as(GesuchDokumentDto.class);
         // get newly created type
         // check if empty dokument has been created
