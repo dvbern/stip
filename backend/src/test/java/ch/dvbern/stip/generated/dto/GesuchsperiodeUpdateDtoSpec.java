ba33c9bea67d92dade1e39b31c89a4e3b56f20dc
Juri Furer <juri.furer@dvbern.ch>
2025-01-30 09:24:17 +0100

    feat(KSTIP-1907): regenerate backend

--- backend/src/test/java/ch/dvbern/stip/generated/dto/GesuchsperiodeUpdateDtoSpec.java
+++ backend/src/test/java/ch/dvbern/stip/generated/dto/GesuchsperiodeUpdateDtoSpec.java
@@ -79,7 +79,8 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
   GesuchsperiodeUpdateDtoSpec.JSON_PROPERTY_REDUZIERUNG_DES_GRUNDBEDARFS,
   GesuchsperiodeUpdateDtoSpec.JSON_PROPERTY_LIMITE_ALTER_ANTRAGSSTELLER_HALBIERUNG_ELTERNBEITRAG,
   GesuchsperiodeUpdateDtoSpec.JSON_PROPERTY_ZWEITER_AUSZAHLUNGSTERMIN_MONAT,
-  GesuchsperiodeUpdateDtoSpec.JSON_PROPERTY_ZWEITER_AUSZAHLUNGSTERMIN_TAG
+  GesuchsperiodeUpdateDtoSpec.JSON_PROPERTY_ZWEITER_AUSZAHLUNGSTERMIN_TAG,
+  GesuchsperiodeUpdateDtoSpec.JSON_PROPERTY_FRIST_NACHREICHEN_DOKUMENTE
 })
 @JsonTypeName("GesuchsperiodeUpdate")
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
@@ -236,6 +237,9 @@ public class GesuchsperiodeUpdateDtoSpec {
 
   public static final String JSON_PROPERTY_ZWEITER_AUSZAHLUNGSTERMIN_TAG = "zweiterAuszahlungsterminTag";
   private Integer zweiterAuszahlungsterminTag;
+
+  public static final String JSON_PROPERTY_FRIST_NACHREICHEN_DOKUMENTE = "fristNachreichenDokumente";
+  private Integer fristNachreichenDokumente;
 
   public GesuchsperiodeUpdateDtoSpec() {
   }
@@ -1565,6 +1569,32 @@ public class GesuchsperiodeUpdateDtoSpec {
     this.zweiterAuszahlungsterminTag = zweiterAuszahlungsterminTag;
   }
 
+
+  public GesuchsperiodeUpdateDtoSpec fristNachreichenDokumente(Integer fristNachreichenDokumente) {
+    
+    this.fristNachreichenDokumente = fristNachreichenDokumente;
+    return this;
+  }
+
+   /**
+   * Get fristNachreichenDokumente
+   * @return fristNachreichenDokumente
+  **/
+  @jakarta.annotation.Nonnull
+  @JsonProperty(JSON_PROPERTY_FRIST_NACHREICHEN_DOKUMENTE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public Integer getFristNachreichenDokumente() {
+    return fristNachreichenDokumente;
+  }
+
+
+  @JsonProperty(JSON_PROPERTY_FRIST_NACHREICHEN_DOKUMENTE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public void setFristNachreichenDokumente(Integer fristNachreichenDokumente) {
+    this.fristNachreichenDokumente = fristNachreichenDokumente;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -1624,12 +1654,13 @@ public class GesuchsperiodeUpdateDtoSpec {
         Objects.equals(this.reduzierungDesGrundbedarfs, gesuchsperiodeUpdate.reduzierungDesGrundbedarfs) &&
         Objects.equals(this.limiteAlterAntragsstellerHalbierungElternbeitrag, gesuchsperiodeUpdate.limiteAlterAntragsstellerHalbierungElternbeitrag) &&
         Objects.equals(this.zweiterAuszahlungsterminMonat, gesuchsperiodeUpdate.zweiterAuszahlungsterminMonat) &&
-        Objects.equals(this.zweiterAuszahlungsterminTag, gesuchsperiodeUpdate.zweiterAuszahlungsterminTag);
+        Objects.equals(this.zweiterAuszahlungsterminTag, gesuchsperiodeUpdate.zweiterAuszahlungsterminTag) &&
+        Objects.equals(this.fristNachreichenDokumente, gesuchsperiodeUpdate.fristNachreichenDokumente);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(bezeichnungDe, bezeichnungFr, einreichfrist, fiskaljahr, gesuchsjahrId, gesuchsperiodeStart, gesuchsperiodeStopp, aufschaltterminStart, aufschaltterminStopp, einreichefristNormal, einreichefristReduziert, ausbKostenSekII, ausbKostenTertiaer, freibetragVermoegen, freibetragErwerbseinkommen, einkommensfreibetrag, elternbeteiligungssatz, vermogenSatzAngerechnet, integrationszulage, limiteEkFreibetragIntegrationszulage, stipLimiteMinimalstipendium, person1, personen2, personen3, personen4, personen5, personen6, personen7, proWeiterePerson, kinder0018, jugendlicheErwachsene1925, erwachsene2699, wohnkostenFam1pers, wohnkostenFam2pers, wohnkostenFam3pers, wohnkostenFam4pers, wohnkostenFam5pluspers, wohnkostenPersoenlich1pers, wohnkostenPersoenlich2pers, wohnkostenPersoenlich3pers, wohnkostenPersoenlich4pers, wohnkostenPersoenlich5pluspers, preisProMahlzeit, maxSaeule3a, anzahlWochenLehre, anzahlWochenSchule, vermoegensanteilInProzent, reduzierungDesGrundbedarfs, limiteAlterAntragsstellerHalbierungElternbeitrag, zweiterAuszahlungsterminMonat, zweiterAuszahlungsterminTag);
+    return Objects.hash(bezeichnungDe, bezeichnungFr, einreichfrist, fiskaljahr, gesuchsjahrId, gesuchsperiodeStart, gesuchsperiodeStopp, aufschaltterminStart, aufschaltterminStopp, einreichefristNormal, einreichefristReduziert, ausbKostenSekII, ausbKostenTertiaer, freibetragVermoegen, freibetragErwerbseinkommen, einkommensfreibetrag, elternbeteiligungssatz, vermogenSatzAngerechnet, integrationszulage, limiteEkFreibetragIntegrationszulage, stipLimiteMinimalstipendium, person1, personen2, personen3, personen4, personen5, personen6, personen7, proWeiterePerson, kinder0018, jugendlicheErwachsene1925, erwachsene2699, wohnkostenFam1pers, wohnkostenFam2pers, wohnkostenFam3pers, wohnkostenFam4pers, wohnkostenFam5pluspers, wohnkostenPersoenlich1pers, wohnkostenPersoenlich2pers, wohnkostenPersoenlich3pers, wohnkostenPersoenlich4pers, wohnkostenPersoenlich5pluspers, preisProMahlzeit, maxSaeule3a, anzahlWochenLehre, anzahlWochenSchule, vermoegensanteilInProzent, reduzierungDesGrundbedarfs, limiteAlterAntragsstellerHalbierungElternbeitrag, zweiterAuszahlungsterminMonat, zweiterAuszahlungsterminTag, fristNachreichenDokumente);
   }
 
   @Override
@@ -1687,6 +1718,7 @@ public class GesuchsperiodeUpdateDtoSpec {
     sb.append("    limiteAlterAntragsstellerHalbierungElternbeitrag: ").append(toIndentedString(limiteAlterAntragsstellerHalbierungElternbeitrag)).append("\n");
     sb.append("    zweiterAuszahlungsterminMonat: ").append(toIndentedString(zweiterAuszahlungsterminMonat)).append("\n");
     sb.append("    zweiterAuszahlungsterminTag: ").append(toIndentedString(zweiterAuszahlungsterminTag)).append("\n");
+    sb.append("    fristNachreichenDokumente: ").append(toIndentedString(fristNachreichenDokumente)).append("\n");
     sb.append("}");
     return sb.toString();
   }
