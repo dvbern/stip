994e3015023f6a81a5195d87541bbaf24b5fec77
Fabrice Jakob <fabrice.jakob@dvbern.ch>
2025-02-17 09:41:14 +0100

    feat(KSTIP-1735): stash

---
+++ backend/src/main/java/ch/dvbern/stip/api/steuerdaten/resource/SteuerdatenResourceImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.stip.api.steuerdaten.resource;
+
+import java.util.List;
+import java.util.UUID;
+
+import ch.dvbern.stip.api.common.interceptors.Validated;
+import ch.dvbern.stip.generated.api.SteuerdatenResource;
+import ch.dvbern.stip.generated.dto.SteuerdatenUpdateDto;
+import jakarta.enterprise.context.RequestScoped;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequestScoped
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class SteuerdatenResourceImpl implements SteuerdatenResource {
+    @Override
+    public List<SteuerdatenUpdateDto> getSteuerdaten(UUID gesuchTrancheId) {
+        return List.of();
+    }
+
+    @Override
+    public List<SteuerdatenUpdateDto> updateSteuerdaten(
+        UUID gesuchTrancheId,
+        List<SteuerdatenUpdateDto> steuerdatenUpdateDto
+    ) {
+        return List.of();
+    }
+}
