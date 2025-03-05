ec3690914083ec0da03161b57c3cb3f532ff199b
Philipp Schärer <philipp.schaerer@dvbern.ch>
2025-03-05 13:25:16 +0100

    fix(KSTIP-2023): ignore unauthorized errors for nesko update

--- frontend/libs/sachbearbeitung-app/data-access/steuerdaten/src/lib/sachbearbeitung-app-data-access-steuerdaten.store.ts
+++ frontend/libs/sachbearbeitung-app/data-access/steuerdaten/src/lib/sachbearbeitung-app-data-access-steuerdaten.store.ts
@@ -4,11 +4,13 @@ import { patchState, signalStore, withState } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { pipe, switchMap, tap } from 'rxjs';
 
+import { GlobalNotificationStore } from '@dv/shared/global/notification';
 import {
   Steuerdaten,
   SteuerdatenService,
   SteuerdatenTyp,
 } from '@dv/shared/model/gesuch';
+import { handleUnauthorized } from '@dv/shared/util/http';
 import {
   CachedRemoteData,
   cachedPending,
@@ -32,6 +34,7 @@ export class SteuerdatenStore extends signalStore(
   withDevtools('SteuerdatenStore'),
 ) {
   private steuerdatenService = inject(SteuerdatenService);
+  private globalNotificationStore = inject(GlobalNotificationStore);
 
   cachedSteuerdatenListViewSig = computed(() => {
     return fromCachedDataSig(this.cachedSteuerdaten);
@@ -93,14 +96,23 @@ export class SteuerdatenStore extends signalStore(
     pipe(
       switchMap(({ gesuchTrancheId, steuerjahr, steuerdatenTyp, token }) =>
         this.steuerdatenService
-          .updateSteuerdatenFromNesko$({
-            gesuchTrancheId,
-            neskoGetSteuerdatenRequest: {
-              steuerjahr,
-              steuerdatenTyp,
-              token,
+          .updateSteuerdatenFromNesko$(
+            {
+              gesuchTrancheId,
+              neskoGetSteuerdatenRequest: {
+                steuerjahr,
+                steuerdatenTyp,
+                token,
+              },
             },
-          })
+            undefined,
+            undefined,
+            {
+              context: handleUnauthorized((error) => {
+                this.globalNotificationStore.handleHttpRequestFailed([error]);
+              }),
+            },
+          )
           .pipe(
             handleApiResponse((cachedSteuerdaten) =>
               patchState(this, { cachedSteuerdaten }),
