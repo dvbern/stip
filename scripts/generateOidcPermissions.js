af394b578d9398989020cfd871296441ca98a71f
Juri Furer <juri.furer@dvbern.ch>
2025-03-12 09:46:01 +0100

    feat(KSTIP-1967): add keycloak scripts

---
+++ scripts/generateOidcPermissions.js
@@ -0,0 +1,38 @@
+import { readFileSync, writeFileSync, mkdirSync, existsSync } from "fs";
+import { parseArgs } from "util";
+
+const options = {
+  filter: {
+    type: "string",
+    short: "f",
+  },
+};
+
+const args = parseArgs({ options }).values;
+
+const filterDef = JSON.parse(readFileSync(args.filter));
+const filters = filterDef.filters;
+
+const psfs = "public static final String";
+const result = [];
+
+for (const type of filters) {
+  for (const requestedRole of type.roles) {
+    result.push(
+      `${psfs} ${type.postfix}_${requestedRole} = '${filterDef.prefix}_${requestedRole}_${type.postfix}';`
+    );
+  }
+
+  result.push("");
+}
+
+const output = `
+public final class OidcPermissions {
+    ${result.join("\n    ")}
+}
+`;
+
+if (!existsSync("out")) {
+  mkdirSync("out");
+}
+writeFileSync("./out/OidcPermissions.java", output);
