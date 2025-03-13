1164420ca8a9dd1f62727abe50a15b7671e427eb
Philipp Schärer <philipp.schaerer@dvbern.ch>
2025-03-13 16:55:26 +0100

    fix(KSTIP-2043): added new sozialdienst app

---
+++ frontend/apps/sozialdienst-app-e2e/playwright.config.ts
@@ -0,0 +1,30 @@
+import { join } from 'node:path';
+
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+import { baseConfig, getE2eUrls } from '@dv/shared/util-fn/e2e-util';
+
+dotenv.config({ path: join(__dirname, '../../.env') });
+
+const urls = getE2eUrls();
+
+export default defineConfig({
+  ...nxE2EPreset(__filename, { testDir: './src' }),
+  ...baseConfig,
+  workers: 1,
+  use: {
+    ...baseConfig.use,
+    baseURL: urls.soz,
+  },
+  projects: [
+    {
+      name: 'sozialdienst-app-e2e',
+      testDir: `src/tests`,
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+});
---
+++ frontend/libs/sozialdienst-app/pattern/gesuch-step-layout/src/lib/sozialdienst-app-pattern-gesuch-step-layout.component.ts
@@ -0,0 +1,145 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  computed,
+  effect,
+  inject,
+  input,
+} from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Router, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import {
+  TranslateDirective,
+  TranslatePipe,
+  isDefined,
+} from '@ngx-translate/core';
+import { filter, map } from 'rxjs';
+
+import { EinreichenStore } from '@dv/shared/data-access/einreichen';
+import {
+  selectRouteId,
+  selectRouteTrancheId,
+  selectSharedDataAccessGesuchCacheView,
+  selectSharedDataAccessGesuchStepsView,
+  selectSharedDataAccessGesuchsView,
+} from '@dv/shared/data-access/gesuch';
+import { GesuchAenderungStore } from '@dv/shared/data-access/gesuch-aenderung';
+import {
+  SharedDataAccessLanguageEvents,
+  selectLanguage,
+} from '@dv/shared/data-access/language';
+import { PermissionStore } from '@dv/shared/global/permission';
+import { GesuchFormStep } from '@dv/shared/model/gesuch-form';
+import { Language } from '@dv/shared/model/language';
+import { urlAfterNavigationEnd } from '@dv/shared/model/router';
+import {
+  SharedPatternAppHeaderComponent,
+  SharedPatternAppHeaderPartsDirective,
+} from '@dv/shared/pattern/app-header';
+import { SharedPatternGesuchStepNavComponent } from '@dv/shared/pattern/gesuch-step-nav';
+import { SharedPatternMobileSidenavComponent } from '@dv/shared/pattern/mobile-sidenav';
+import { SharedUiIconChipComponent } from '@dv/shared/ui/icon-chip';
+import { SharedUiProgressBarComponent } from '@dv/shared/ui/progress-bar';
+import { getLatestTrancheIdFromGesuchOnUpdate$ } from '@dv/shared/util/gesuch';
+import { SharedUtilGesuchFormStepManagerService } from '@dv/shared/util/gesuch-form-step-manager';
+import { SharedUtilHeaderService } from '@dv/shared/util/header';
+
+@Component({
+  selector: 'dv-sozialdienst-app-pattern-gesuch-step-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    TranslateDirective,
+    TranslatePipe,
+    MatSidenavModule,
+    MatMenuModule,
+    SharedPatternAppHeaderComponent,
+    SharedPatternGesuchStepNavComponent,
+    SharedPatternMobileSidenavComponent,
+    SharedUiProgressBarComponent,
+    SharedUiIconChipComponent,
+    SharedPatternAppHeaderPartsDirective,
+  ],
+  templateUrl: './sozialdienst-app-pattern-gesuch-step-layout.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [SharedUtilHeaderService],
+})
+export class GesuchAppPatternGesuchStepLayoutComponent {
+  stepSig = input<GesuchFormStep | undefined>(undefined, {
+    alias: 'step',
+  });
+
+  navClicked$ = new EventEmitter<{ value: boolean }>();
+
+  private store = inject(Store);
+  private einreichenStore = inject(EinreichenStore);
+  private permissionStore = inject(PermissionStore);
+  private router = inject(Router);
+
+  gesuchAenderungStore = inject(GesuchAenderungStore);
+  headerService = inject(SharedUtilHeaderService);
+  stepManager = inject(SharedUtilGesuchFormStepManagerService);
+  gesuchIdSig = this.store.selectSignal(selectRouteId);
+  trancheIdSig = this.store.selectSignal(selectRouteTrancheId);
+  languageSig = this.store.selectSignal(selectLanguage);
+  viewSig = this.store.selectSignal(selectSharedDataAccessGesuchsView);
+  cacheViewSig = this.store.selectSignal(selectSharedDataAccessGesuchCacheView);
+  stepsViewSig = this.store.selectSignal(selectSharedDataAccessGesuchStepsView);
+  stepsSig = computed(() => {
+    const { cache, trancheTyp } = this.cacheViewSig();
+    const { invalidFormularProps } = this.einreichenStore.validationViewSig();
+    const steps = this.stepsViewSig().steps;
+    const rolesMap = this.permissionStore.rolesMapSig();
+    const validatedSteps = this.stepManager.getValidatedSteps(
+      steps,
+      trancheTyp,
+      cache.gesuch,
+      rolesMap,
+      undefined,
+      invalidFormularProps.validations,
+    );
+    return validatedSteps;
+  });
+  currentStepProgressSig = computed(() => {
+    const stepsFlow = this.stepsViewSig().stepsFlow;
+    return this.stepManager.getStepProgress(stepsFlow, this.stepSig());
+  });
+  currentStepSig = computed(() => {
+    const steps = this.stepsSig();
+    return steps.find((step) => step.route === this.stepSig()?.route);
+  });
+  isTrancheRouteSig = toSignal(
+    urlAfterNavigationEnd(this.router).pipe(
+      map((url) => url.includes('/tranche/')),
+    ),
+  );
+
+  constructor() {
+    getLatestTrancheIdFromGesuchOnUpdate$(this.viewSig)
+      .pipe(filter(isDefined), takeUntilDestroyed())
+      .subscribe((gesuchTrancheId) => {
+        this.einreichenStore.validateSteps$({ gesuchTrancheId });
+      });
+    effect(
+      () => {
+        const gesuchId = this.gesuchIdSig();
+        if (gesuchId) {
+          this.gesuchAenderungStore.getAllTranchenForGesuch$({ gesuchId });
+        }
+      },
+      { allowSignalWrites: true },
+    );
+  }
+
+  handleLanguageChangeHeader(language: Language) {
+    this.store.dispatch(
+      SharedDataAccessLanguageEvents.headerMenuSelectorChange({ language }),
+    );
+  }
+}
