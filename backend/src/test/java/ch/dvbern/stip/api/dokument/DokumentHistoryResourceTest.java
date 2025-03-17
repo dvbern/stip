b687d30da69525ae97fda77e4760665e1250b5e8
Fabrice Jakob <fabrice.jakob@dvbern.ch>
2025-03-17 16:16:00 +0100

    feat(KSTIP-1998): added test

---
+++ backend/src/test/java/ch/dvbern/stip/api/dokument/DokumentHistoryResourceTest.java
@@ -0,0 +1,377 @@
+/*
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.stip.api.dokument;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.UUID;
+
+import ch.dvbern.stip.api.benutzer.util.TestAsGesuchsteller;
+import ch.dvbern.stip.api.benutzer.util.TestAsSachbearbeiter;
+import ch.dvbern.stip.api.generator.api.GesuchTestSpecGenerator;
+import ch.dvbern.stip.api.util.RequestSpecUtil;
+import ch.dvbern.stip.api.util.TestClamAVEnvironment;
+import ch.dvbern.stip.api.util.TestDatabaseEnvironment;
+import ch.dvbern.stip.api.util.TestUtil;
+import ch.dvbern.stip.generated.api.AusbildungApiSpec;
+import ch.dvbern.stip.generated.api.DokumentApiSpec;
+import ch.dvbern.stip.generated.api.FallApiSpec;
+import ch.dvbern.stip.generated.api.GesuchApiSpec;
+import ch.dvbern.stip.generated.api.GesuchTrancheApiSpec;
+import ch.dvbern.stip.generated.dto.DokumentTypDtoSpec;
+import ch.dvbern.stip.generated.dto.DokumenteToUploadDtoSpec;
+import ch.dvbern.stip.generated.dto.GesuchDtoSpec;
+import ch.dvbern.stip.generated.dto.GesuchUpdateDtoSpec;
+import ch.dvbern.stip.generated.dto.GesuchWithChangesDtoSpec;
+import ch.dvbern.stip.generated.dto.GesuchstatusDtoSpec;
+import ch.dvbern.stip.generated.dto.NullableGesuchDokumentDto;
+import ch.dvbern.stip.generated.dto.ValidationReportDtoSpec;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.Response.Status;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static ch.dvbern.stip.api.common.validation.ValidationsConstant.VALIDATION_DOCUMENTS_REQUIRED_MESSAGE;
+import static ch.dvbern.stip.generated.dto.DokumentTypDtoSpec.ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_FAMILIE;
+import static ch.dvbern.stip.generated.dto.DokumentTypDtoSpec.ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_MUTTER;
+import static ch.dvbern.stip.generated.dto.DokumentTypDtoSpec.ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_VATER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTestResource(TestDatabaseEnvironment.class)
+@QuarkusTestResource(TestClamAVEnvironment.class)
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@RequiredArgsConstructor
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DokumentHistoryResourceTest {
+    private final DokumentApiSpec dokumentApiSpec = DokumentApiSpec.dokument(RequestSpecUtil.quarkusSpec());
+    private final GesuchTrancheApiSpec gesuchTrancheApiSpec =
+        GesuchTrancheApiSpec.gesuchTranche(RequestSpecUtil.quarkusSpec());
+
+    public GesuchApiSpec getGesuchApiSpec() {
+        return gesuchApiSpec;
+    }
+
+    private final FallApiSpec fallApiSpec = FallApiSpec.fall(RequestSpecUtil.quarkusSpec());
+    private final AusbildungApiSpec ausbildungApiSpec = AusbildungApiSpec.ausbildung(RequestSpecUtil.quarkusSpec());
+    private final GesuchApiSpec gesuchApiSpec = GesuchApiSpec.gesuch(RequestSpecUtil.quarkusSpec());
+    private UUID gesuchId;
+    private GesuchDtoSpec gesuch;
+    private GesuchWithChangesDtoSpec returnedGesuch;
+    private UUID gesuchTrancheId;
+
+    @Test
+    @TestAsGesuchsteller
+    @Order(1)
+    void test_prepare_gesuch_for_dokument() {
+        gesuch = TestUtil.createGesuchAusbildungFall(fallApiSpec, ausbildungApiSpec, gesuchApiSpec);
+        gesuchId = gesuch.getId();
+        gesuchTrancheId = gesuch.getGesuchTrancheToWorkWith().getId();
+    }
+
+    // for the next tests, gesuch
+    // should be in state IN_BEARBEITUNG_SB
+    @Test
+    @TestAsGesuchsteller
+    @Order(2)
+    void fillGesuch() {
+        final var fullGesuch = GesuchTestSpecGenerator.gesuchUpdateDtoSpecFull();
+        fullGesuch.getGesuchTrancheToWorkWith().setId(gesuch.getGesuchTrancheToWorkWith().getId());
+        fullGesuch.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(0).setWohnkosten(0);
+        fullGesuch.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(1).setWohnkosten(0);
+
+        gesuchApiSpec.updateGesuch()
+            .gesuchIdPath(gesuch.getId())
+            .body(fullGesuch)
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.NO_CONTENT.getStatusCode());
+
+        final var notToUseDocuments = Set.of(
+            ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_VATER,
+            ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_MUTTER,
+            ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_FAMILIE
+        );
+
+        for (final var dokTyp : DokumentTypDtoSpec.values()) {
+            if (notToUseDocuments.contains(dokTyp)) {
+                continue;
+            }
+
+            final var file = TestUtil.getTestPng();
+            TestUtil.uploadFile(dokumentApiSpec, gesuch.getGesuchTrancheToWorkWith().getId(), dokTyp, file);
+        }
+    }
+
+    @Test
+    @TestAsGesuchsteller
+    @Order(3)
+    void gesuchEinreichen() {
+        gesuchApiSpec.gesuchEinreichen()
+            .gesuchTrancheIdPath(gesuch.getGesuchTrancheToWorkWith().getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode());
+    }
+
+    @Test
+    @TestAsSachbearbeiter
+    @Order(4)
+    void gesuchStatusChangeToInBearbeitungSB() {
+        returnedGesuch = gesuchApiSpec.changeGesuchStatusToInBearbeitung()
+            .gesuchTrancheIdPath(gesuch.getGesuchTrancheToWorkWith().getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(GesuchWithChangesDtoSpec.class);
+
+        assertThat(returnedGesuch.getGesuchStatus(), is(GesuchstatusDtoSpec.IN_BEARBEITUNG_SB));
+    }
+
+    @Test
+    @TestAsSachbearbeiter
+    @Order(5)
+    void testMoreDocumentsNeeded() {
+        final var fullGesuch = GesuchTestSpecGenerator.gesuchUpdateDtoSpecFull();
+
+        overrideEssentialsForUpadate(fullGesuch, returnedGesuch);
+
+        fullGesuch.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(0).setWohnkosten(5);
+        fullGesuch.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(1).setWohnkosten(5);
+
+        gesuchApiSpec.updateGesuch()
+            .gesuchIdPath(gesuch.getId())
+            .body(fullGesuch)
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.NO_CONTENT.getStatusCode());
+
+        final var validationReport = gesuchTrancheApiSpec.validateGesuchTranchePagesSB()
+            .gesuchTrancheIdPath(gesuch.getGesuchTrancheToWorkWith().getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(ValidationReportDtoSpec.class);
+
+        assertThat(
+            validationReport.getValidationWarnings().get(0).getMessageTemplate(),
+            equalTo(VALIDATION_DOCUMENTS_REQUIRED_MESSAGE)
+        );
+
+        final var dokumenteToUpload = gesuchTrancheApiSpec.getDocumentsToUploadSB()
+            .gesuchTrancheIdPath(gesuchTrancheId)
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(DokumenteToUploadDtoSpec.class);
+
+        assertThat(dokumenteToUpload.getRequired().get(0), equalTo(ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_FAMILIE));
+    }
+
+    @Test
+    @TestAsGesuchsteller
+    @Order(6)
+    void testGSDoesNotSeeMoreNeeded() {
+        final var validationReport = gesuchTrancheApiSpec.validateGesuchTranchePagesGS()
+            .gesuchTrancheIdPath(gesuch.getGesuchTrancheToWorkWith().getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(ValidationReportDtoSpec.class);
+
+        assertThat(
+            validationReport.getValidationWarnings().size(),
+            equalTo(0)
+        );
+
+        final var dokumenteToUpload = gesuchTrancheApiSpec.getDocumentsToUploadGS()
+            .gesuchTrancheIdPath(gesuchTrancheId)
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(DokumenteToUploadDtoSpec.class);
+
+        assertThat(dokumenteToUpload.getRequired().size(), equalTo(0));
+    }
+
+    @Test
+    @TestAsSachbearbeiter
+    @Order(7)
+    void dokument_ablehnen_or_akzeptieren() {
+        var allDokTypesExceptOne = Arrays.stream(DokumentTypDtoSpec.values()).toList();
+        var modifiableDokTypeList = new ArrayList<>(allDokTypesExceptOne);
+        modifiableDokTypeList.remove(ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_FAMILIE);
+        modifiableDokTypeList.forEach(dokType -> {
+            var dokToAccept = dokumentApiSpec.getGesuchDokumenteForTypSB()
+                .dokumentTypPath(dokType)
+                .gesuchTrancheIdPath(gesuchTrancheId)
+                .execute(TestUtil.PEEK_IF_ENV_SET)
+                .then()
+                .assertThat()
+                .statusCode(Status.OK.getStatusCode())
+                .extract()
+                .body()
+                .as(NullableGesuchDokumentDto.class);
+
+            if (dokToAccept.getValue() != null) {
+                dokumentApiSpec.gesuchDokumentAkzeptieren()
+                    .gesuchDokumentIdPath(dokToAccept.getValue().getId())
+                    .execute(TestUtil.PEEK_IF_ENV_SET)
+                    .then()
+                    .assertThat()
+                    .statusCode(Status.NO_CONTENT.getStatusCode());
+            }
+        });
+    }
+
+    @TestAsSachbearbeiter
+    @Test
+    @Order(8)
+    void fehlendeDokumenteUebermitteln() {
+        gesuchApiSpec.gesuchFehlendeDokumenteUebermitteln()
+            .gesuchTrancheIdPath(gesuchTrancheId)
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode());
+    }
+
+    @Test
+    @TestAsGesuchsteller
+    @Order(9)
+    void testGSDoesSeeMoreNeeded() {
+        final var validationReport = gesuchTrancheApiSpec.validateGesuchTranchePagesGS()
+            .gesuchTrancheIdPath(gesuch.getGesuchTrancheToWorkWith().getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(ValidationReportDtoSpec.class);
+
+        assertThat(
+            validationReport.getValidationWarnings().get(0).getMessageTemplate(),
+            equalTo(VALIDATION_DOCUMENTS_REQUIRED_MESSAGE)
+        );
+
+        final var dokumenteToUpload = gesuchTrancheApiSpec.getDocumentsToUploadGS()
+            .gesuchTrancheIdPath(gesuchTrancheId)
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(DokumenteToUploadDtoSpec.class);
+
+        assertThat(dokumenteToUpload.getRequired().get(0), equalTo(ELTERN_MIETVERTRAG_HYPOTEKARZINSABRECHNUNG_FAMILIE));
+    }
+
+    private GesuchUpdateDtoSpec overrideEssentialsForUpadate(
+        GesuchUpdateDtoSpec toOverride,
+        GesuchWithChangesDtoSpec override
+    ) {
+        toOverride.getGesuchTrancheToWorkWith().setId(gesuch.getGesuchTrancheToWorkWith().getId());
+
+        toOverride.getGesuchTrancheToWorkWith()
+            .getGesuchFormular()
+            .getElterns()
+            .get(0)
+            .setId(
+                override.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(0).getId()
+            );
+
+        toOverride.getGesuchTrancheToWorkWith()
+            .getGesuchFormular()
+            .getElterns()
+            .get(0)
+            .getAdresse()
+            .setId(
+                override.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(0).getAdresse().getId()
+            );
+
+        toOverride.getGesuchTrancheToWorkWith()
+            .getGesuchFormular()
+            .getElterns()
+            .get(1)
+            .setId(
+                override.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(1).getId()
+            );
+
+        toOverride.getGesuchTrancheToWorkWith()
+            .getGesuchFormular()
+            .getElterns()
+            .get(1)
+            .getAdresse()
+            .setId(
+                override.getGesuchTrancheToWorkWith().getGesuchFormular().getElterns().get(1).getAdresse().getId()
+            );
+
+        toOverride.getGesuchTrancheToWorkWith()
+            .getGesuchFormular()
+            .getPersonInAusbildung()
+            .getAdresse()
+            .setId(
+                override.getGesuchTrancheToWorkWith()
+                    .getGesuchFormular()
+                    .getPersonInAusbildung()
+                    .getAdresse()
+                    .getId()
+            );
+
+        toOverride.getGesuchTrancheToWorkWith()
+            .getGesuchFormular()
+            .getPartner()
+            .getAdresse()
+            .setId(
+                override.getGesuchTrancheToWorkWith().getGesuchFormular().getPartner().getAdresse().getId()
+            );
+
+        toOverride.getGesuchTrancheToWorkWith().getGesuchFormular().setKinds(null);
+        toOverride.getGesuchTrancheToWorkWith().getGesuchFormular().setGeschwisters(null);
+        return toOverride;
+    }
+
+}
