bbd6f21137dc706c837586e6d21efd88ccffa884
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2025-03-20 16:05:27 +0100

    feat(KSTIP-2057): reset incorrect status transitions

--- backend/src/main/java/ch/dvbern/stip/api/common/statemachines/gesuchstatus/GesuchStatusConfigProducer.java
+++ backend/src/main/java/ch/dvbern/stip/api/common/statemachines/gesuchstatus/GesuchStatusConfigProducer.java
@@ -17,6 +17,8 @@
 
 package ch.dvbern.stip.api.common.statemachines.gesuchstatus;
 
+import java.util.Optional;
+
 import ch.dvbern.stip.api.common.exception.AppErrorException;
 import ch.dvbern.stip.api.common.statemachines.gesuchstatus.handlers.GesuchStatusStateChangeHandler;
 import ch.dvbern.stip.api.gesuch.entity.Gesuch;
@@ -27,8 +29,6 @@ import com.github.oxo42.stateless4j.transitions.Transition;
 import jakarta.enterprise.inject.Instance;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Optional;
 
 @UtilityClass
 @Slf4j
