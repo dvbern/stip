ac96b76caae34ac6990d5136c56cd4b26a3b5772
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-03-31 15:21:36 +0200

    feat(DVSTIP-17): Add logo and method in service

--- frontend/libs/shared/util/tenant-cache/src/lib/shared-util-tenant-cache.service.ts
+++ frontend/libs/shared/util/tenant-cache/src/lib/shared-util-tenant-cache.service.ts
@@ -1,16 +1,44 @@
-import { Injectable, computed, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
 
 import { TenantInfo } from '@dv/shared/model/gesuch';
+
+const faviconMap = {
+  bern: 'assets/images/logo_kanton_bern_full.svg',
+  dv: 'assets/images/dv-favicon-logo.svg',
+} as const;
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedUtilTenantCacheService {
   private _tenantInfoSig = signal<TenantInfo | null>(null);
+  private document = inject(DOCUMENT);
 
   tenantInfoSig = computed(() => this._tenantInfoSig());
 
+  // effect to set favicon
+  constructor() {
+    effect(() => {
+      const tenantInfo = this._tenantInfoSig();
+      if (tenantInfo) {
+        this.setFavicon(tenantInfo.identifier);
+      }
+    });
+  }
+
   setTenantInfo(tenantInfo: TenantInfo) {
     this._tenantInfoSig.set(tenantInfo);
+  }
+
+  private setFavicon(identifier: string) {
+    const favicon = this.document.querySelector('link[rel="icon"]');
+    if (favicon) {
+      favicon.setAttribute(
+        'href',
+        faviconMap[identifier as keyof typeof faviconMap] ||
+          'assets/images/logo_kanton_bern_full.svg',
+      );
+    }
   }
 }
