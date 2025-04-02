48daf22cc3d22cd6a0ef7bdbc80587367d8db029
Schmidhauser Pablo <pablo.schmidhauser@dvbern.ch>
2025-04-02 12:45:45 +0200

    feature/KSTIP-1627: WIP

--- frontend/libs/sachbearbeitung-app/feature/administration-benutzerverwaltung/src/lib/benutzer-overview/benutzer-overview.component.ts
+++ frontend/libs/sachbearbeitung-app/feature/administration-benutzerverwaltung/src/lib/benutzer-overview/benutzer-overview.component.ts
@@ -4,10 +4,12 @@ import {
   Component,
   DestroyRef,
   computed,
+  effect,
   inject,
   viewChild,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
@@ -15,6 +17,7 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
+import { debounceTime, map } from 'rxjs';
 
 import { BenutzerverwaltungStore } from '@dv/sachbearbeitung-app/data-access/benutzerverwaltung';
 import { SharedModelBenutzer } from '@dv/shared/model/benutzer';
@@ -28,6 +31,12 @@ import {
 import { TypeSafeMatCellDefDirective } from '@dv/shared/ui/table-helper';
 import { SharedUiTruncateTooltipDirective } from '@dv/shared/ui/truncate-tooltip';
 import { paginatorTranslationProvider } from '@dv/shared/util/paginator-translation';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { SharedUiClearButtonComponent } from '@dv/shared/ui/clear-button';
+import { SharedUiMaxLengthDirective } from '@dv/shared/ui/max-length';
+
+const INPUT_DELAY = 600;
 
 @Component({
   standalone: true,
@@ -45,6 +54,11 @@ import { paginatorTranslationProvider } from '@dv/shared/util/paginator-translat
     SharedUiRdIsPendingWithoutCachePipe,
     SharedUiTruncateTooltipDirective,
     SharedUiLoadingComponent,
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    SharedUiClearButtonComponent,
+    SharedUiMaxLengthDirective,
   ],
   templateUrl: './benutzer-overview.component.html',
   styleUrls: ['./benutzer-overview.component.scss'],
@@ -53,6 +67,7 @@ import { paginatorTranslationProvider } from '@dv/shared/util/paginator-translat
 })
 export class BenutzerOverviewComponent {
   private dialog = inject(MatDialog);
+  private formBuilder = inject(NonNullableFormBuilder);
   store = inject(BenutzerverwaltungStore);
   destroyRef = inject(DestroyRef);
 
@@ -61,11 +76,41 @@ export class BenutzerOverviewComponent {
 
   sortSig = viewChild(MatSort);
   paginatorSig = viewChild(MatPaginator);
+
+  filterForm = this.formBuilder.group({
+    name: [<string | null>null],
+    roles: [<string | null>null],
+  });
+
+  private filterFormChangedSig = toSignal(
+    this.filterForm.valueChanges.pipe(
+      debounceTime(INPUT_DELAY),
+      map(() => this.filterForm.getRawValue()),
+    ),
+  );
   benutzerListDataSourceSig = computed(() => {
     const benutzers = this.store.benutzers();
+    console.log('Benutzerdaten aus API', benutzers);
     const datasource = new MatTableDataSource(benutzers.data);
     const sort = this.sortSig();
     const paginator = this.paginatorSig();
+
+    datasource.filterPredicate = (data, filter) => {
+      const { name, roles } = JSON.parse(filter);
+      if (name && !data.name.toLowerCase().includes(name.toLowerCase())) {
+        return false;
+      }
+      if (roles) {
+        const roleNames = [...data.roles.compact, ...data.roles.full]
+          .map((role) => role.name.toLowerCase())
+          .join(',');
+        console.log(roleNames, 'Roles');
+        if (!roleNames.includes(roles.toLowerCase())) {
+          return false;
+        }
+      }
+      return true;
+    };
 
     if (sort) {
       datasource.sort = sort;
@@ -78,6 +123,13 @@ export class BenutzerOverviewComponent {
 
   constructor() {
     this.store.loadAllSbAppBenutzers$();
+    effect(
+      () => {
+        const filterValues = this.filterFormChangedSig();
+        this.benutzerListDataSourceSig().filter = JSON.stringify(filterValues);
+      },
+      { allowSignalWrites: true },
+    );
   }
 
   expandRolesForBenutzer(benutzerId: string) {
