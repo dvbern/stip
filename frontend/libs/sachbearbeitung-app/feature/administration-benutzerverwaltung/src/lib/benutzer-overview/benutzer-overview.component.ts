8adf9ec0639ec15cb7e00f9fe0809dd591ef9926
Schmidhauser Pablo <pablo.schmidhauser@dvbern.ch>
2025-04-03 09:30:55 +0200

    feature/KSTIP-1627: Finished Filtern for Benutzerverwaltung

--- frontend/libs/sachbearbeitung-app/feature/administration-benutzerverwaltung/src/lib/benutzer-overview/benutzer-overview.component.ts
+++ frontend/libs/sachbearbeitung-app/feature/administration-benutzerverwaltung/src/lib/benutzer-overview/benutzer-overview.component.ts
@@ -11,6 +11,8 @@ import {
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
@@ -22,8 +24,10 @@ import { debounceTime, map } from 'rxjs';
 import { BenutzerverwaltungStore } from '@dv/sachbearbeitung-app/data-access/benutzerverwaltung';
 import { SharedModelBenutzer } from '@dv/shared/model/benutzer';
 import { SharedUiBadgeComponent } from '@dv/shared/ui/badge';
+import { SharedUiClearButtonComponent } from '@dv/shared/ui/clear-button';
 import { SharedUiConfirmDialogComponent } from '@dv/shared/ui/confirm-dialog';
 import { SharedUiLoadingComponent } from '@dv/shared/ui/loading';
+import { SharedUiMaxLengthDirective } from '@dv/shared/ui/max-length';
 import {
   SharedUiRdIsPendingPipe,
   SharedUiRdIsPendingWithoutCachePipe,
@@ -31,10 +35,6 @@ import {
 import { TypeSafeMatCellDefDirective } from '@dv/shared/ui/table-helper';
 import { SharedUiTruncateTooltipDirective } from '@dv/shared/ui/truncate-tooltip';
 import { paginatorTranslationProvider } from '@dv/shared/util/paginator-translation';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { SharedUiClearButtonComponent } from '@dv/shared/ui/clear-button';
-import { SharedUiMaxLengthDirective } from '@dv/shared/ui/max-length';
 
 const INPUT_DELAY = 600;
 
