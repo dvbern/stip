2b8ae0ef9916fe4b3ff96836e9c1e3ad802f172b
Juri Furer <juri.furer@dvbern.ch>
2025-04-16 15:18:53 +0200

    feat(DVSTIP-13): update remaining dependencies and fix errors

--- backend/src/test/java/ch/dvbern/stip/api/gesuch/resource/GesuchGetGesucheTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuch/resource/GesuchGetGesucheTest.java
@@ -47,6 +47,7 @@ import ch.dvbern.stip.generated.dto.PaginatedSbDashboardDtoSpec;
 import ch.dvbern.stip.generated.dto.SbDashboardGesuchDtoSpec;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.ResponseBody;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
@@ -327,7 +328,7 @@ class GesuchGetGesucheTest {
             .pageQuery(0)
             .pageSizeQuery(configService.getMaxAllowedPageSize())
             .typQuery(GesuchTrancheTypDtoSpec.TRANCHE)
-            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .execute(ResponseBody::prettyPeek)
             .then()
             .assertThat()
             .statusCode(Status.OK.getStatusCode())
