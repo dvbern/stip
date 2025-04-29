95d17788bff70c50bbadc3a93f01d22a68cb535d
Philipp Schärer <philipp.schaerer@dvbern.ch>
2025-04-29 18:04:22 +0200

    fix(KSTIP-2135): prototype sb/jurist permission map changes

--- frontend/libs/shared/data-access/gesuch/src/lib/shared-data-access-gesuch.selectors.spec.ts
+++ frontend/libs/shared/data-access/gesuch/src/lib/shared-data-access-gesuch.selectors.spec.ts
@@ -140,25 +140,17 @@ describe('selectSharedDataAccessGesuchsView', () => {
       loading: false,
       error: undefined,
     };
-    const result = selectSharedDataAccessGesuchStepsView.projector(
-      state,
-      {
-        deploymentConfig: undefined,
-        compileTimeConfig: {
-          appType: 'gesuch-app',
-          authClientId: 'stip-gesuch-app',
-        },
-        loading: false,
-        error: undefined,
-        isGesuchApp: true,
-        isSachbearbeitungApp: false,
+    const result = selectSharedDataAccessGesuchStepsView.projector(state, {
+      deploymentConfig: undefined,
+      compileTimeConfig: {
+        appType: 'gesuch-app',
+        authClientId: 'stip-gesuch-app',
       },
-      {
-        rolesMap: {},
-        currentBenutzerRd: initial(),
-        lastFetchTs: null,
-      },
-    );
+      loading: false,
+      error: undefined,
+      isGesuchApp: true,
+      isSachbearbeitungApp: false,
+    });
     const elternIndex = result.steps.findIndex(
       (step) => step.route === ELTERN.route,
     );
