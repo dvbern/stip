08439fe5cf615499d9699a7a94473c3925396153
Philipp Schärer <philipp.schaerer@dvbern.ch>
2025-05-09 21:43:31 +0200

    fix(KSTIP-2135): added ausbildung validation
    It now checks if a valid ausbildung is set after einreichen
    The corresponding fields will also be reset in the frontend

---
+++ backend/src/main/java/ch/dvbern/stip/api/gesuchformular/entity/AusbildungIsDefinedConstraintValidator.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.stip.api.gesuchformular.entity;
+
+import ch.dvbern.stip.api.gesuch.util.GesuchValidatorUtil;
+import ch.dvbern.stip.api.gesuchstatus.type.Gesuchstatus;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class AusbildungIsDefinedConstraintValidator
+    implements ConstraintValidator<AusbildungIsDefinedConstraint, GesuchFormular> {
+    private String property = "";
+
+    @Override
+    public void initialize(AusbildungIsDefinedConstraint constraintAnnotation) {
+        property = constraintAnnotation.property();
+    }
+
+    @Override
+    public boolean isValid(
+        GesuchFormular gesuchFormular,
+        ConstraintValidatorContext context
+    ) {
+        final var gesuch = gesuchFormular.getTranche().getGesuch();
+        final var ausbildungIsDefined = gesuch.getGesuchStatus() == Gesuchstatus.IN_BEARBEITUNG_GS
+        || !gesuch.getAusbildung().isAusbildungNichtGefunden();
+
+        if (ausbildungIsDefined) {
+            return true;
+        }
+        return GesuchValidatorUtil.addProperty(
+            context,
+            property
+        );
+    }
+}
