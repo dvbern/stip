dfffa90e2563dd66af07dea994968888b67a00e0
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2025-06-06 08:42:47 +0200

    feat(KSTIP-2026): init service

---
+++ backend/src/main/java/ch/dvbern/stip/api/auszahlung/service/AuszahlungService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.stip.api.auszahlung.service;
+
+import java.util.UUID;
+
+import ch.dvbern.stip.api.auszahlung.repo.AuszahlungRepository;
+import ch.dvbern.stip.generated.dto.AuszahlungDto;
+import ch.dvbern.stip.generated.dto.AuszahlungUpdateDto;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestScoped
+public class AuszahlungService {
+    private final AuszahlungRepository auszahlungRepository;
+    private final AuszahlungMapper auszahlungMapper;
+
+    @Transactional
+    public AuszahlungDto createAuszahlungForGesuch(UUID gesuchId, AuszahlungDto auszahlungDto) {
+        return null;
+    }
+
+    @Transactional
+    public AuszahlungDto getAuszahlungForGesuch(UUID gesuchId) {
+        return null;
+    }
+
+    @Transactional
+    public AuszahlungDto updateAuszahlungForGesuch(UUID gesuchId, AuszahlungUpdateDto auszahlungUpdateDto) {
+        return null;
+    }
+}
