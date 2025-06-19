a049fc76c70f1df62c46a5fbffac1f2c94ce76df
Juri Furer <juri.furer@dvbern.ch>
2025-06-19 16:20:16 +0200

    feat(KSTIP-2204): expand on testing

---
+++ backend/src/test/java/ch/dvbern/stip/api/gesuchformular/util/GetDocumentsForDarlehenArgumentsProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.stip.api.gesuchformular.util;
+
+import java.util.stream.Stream;
+
+import ch.dvbern.stip.api.darlehen.entity.Darlehen;
+import ch.dvbern.stip.api.dokument.type.DokumentTyp;
+import ch.dvbern.stip.generated.dto.DarlehenDto;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+public class GetDocumentsForDarlehenArgumentsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+        return CreatePermutationUtil.createIntegerPermutations(
+            (val) -> new DarlehenDto().anzahlBetreibungen(val),
+            (val) -> new Darlehen().setAnzahlBetreibungen(val),
+            DokumentTyp.DARLEHEN_BETREIBUNGSREGISTERAUSZUG
+        );
+    }
+}
