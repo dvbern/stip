d73eeae267f12c5af897eda5ee0f7196efd8f0b6
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2025-07-24 10:14:47 +0200

    feat(KSTIP-2409): add new GesuchstatusHandler for Gesuchstatus Anspruch_Pruefen

---
+++ backend/src/main/java/ch/dvbern/stip/api/common/statemachines/gesuch/handlers/AnspruchPruefenStatusHandler.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2023 DV Bern AG, Switzerland
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.dvbern.stip.api.common.statemachines.gesuch.handlers;
+
+import ch.dvbern.stip.api.gesuch.entity.Gesuch;
+import ch.dvbern.stip.api.gesuch.service.GesuchService;
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@ApplicationScoped
+@Slf4j
+@RequiredArgsConstructor
+public class AnspruchPruefenStatusHandler implements GesuchStatusChangeHandler {
+    private final GesuchService gesuchService;
+
+    @Override
+    public void handle(Gesuch gesuch) {
+        gesuchService.stipendienAnspruchPruefen(gesuch.getId());
+    }
+}
