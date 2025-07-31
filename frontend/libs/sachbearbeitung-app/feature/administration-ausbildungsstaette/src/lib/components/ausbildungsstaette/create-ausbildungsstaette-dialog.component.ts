ee0bc1d67cb32f636fdb2723e7db1fd918151cd2
Philipp Schärer <philipp.schaerer@dvbern.ch>
2025-07-31 10:05:30 +0200

    fix(KSTIP-2381): added uniqueness check for ausbiludngsstaette

--- frontend/libs/sachbearbeitung-app/feature/administration-ausbildungsstaette/src/lib/components/ausbildungsstaette/create-ausbildungsstaette-dialog.component.ts
+++ frontend/libs/sachbearbeitung-app/feature/administration-ausbildungsstaette/src/lib/components/ausbildungsstaette/create-ausbildungsstaette-dialog.component.ts
@@ -7,8 +7,10 @@ import {
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import {
+  AbstractControl,
   NonNullableFormBuilder,
   ReactiveFormsModule,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import {
@@ -27,6 +29,7 @@ import {
   AusbildungsstaetteCreate,
   AusbildungsstaetteSlim,
 } from '@dv/shared/model/gesuch';
+import { KnownLanguage } from '@dv/shared/model/type-util';
 import {
   SharedUiFormFieldDirective,
   SharedUiFormMessageErrorDirective,
@@ -70,8 +73,26 @@ export class CreateAusbildungsstaetteDialogComponent {
   dialogData = inject<CreateAbschlussData>(MAT_DIALOG_DATA);
   form = this.formBuilder.group({
     schule: [<SchulType | undefined>undefined, Validators.required],
-    nameDe: [<string | null>null, Validators.required],
-    nameFr: [<string | null>null, Validators.required],
+    nameDe: [
+      <string | null>null,
+      [
+        Validators.required,
+        uniqueAusbildungsstaetteValidator(
+          this.dialogData.ausbildungsstaetten,
+          'nameDe',
+        ),
+      ],
+    ],
+    nameFr: [
+      <string | null>null,
+      [
+        Validators.required,
+        uniqueAusbildungsstaetteValidator(
+          this.dialogData.ausbildungsstaetten,
+          'nameFr',
+        ),
+      ],
+    ],
     burNo: [<string | undefined>undefined],
   });
   schulen = [
@@ -122,3 +143,22 @@ export class CreateAusbildungsstaetteDialogComponent {
     this.dialogRef.close();
   }
 }
+
+const uniqueAusbildungsstaetteValidator = (
+  ausbildungsstaetten: AusbildungsstaetteSlim[],
+  property: `name${Capitalize<KnownLanguage>}`,
+): ValidatorFn => {
+  return (control: AbstractControl<string | null>) => {
+    const currentValue = control.value;
+
+    if (!currentValue) {
+      return null;
+    }
+
+    const duplicateExists = ausbildungsstaetten.some(
+      (entry) => entry[property] === currentValue,
+    );
+
+    return duplicateExists ? { notUniqueAusbildungsstaette: true } : null;
+  };
+};
