bc8323f996c3dc25e029072abbee1d616c8eda0c
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-08-18 11:31:07 +0200

    feat(KSTIP-2306): changes to header layout

--- frontend/libs/sachbearbeitung-app/feature/fehlgeschlagene-zahlungen/src/index.ts
+++ frontend/libs/sachbearbeitung-app/feature/fehlgeschlagene-zahlungen/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/lib.routes';
+export * from './lib/sachbearbeitung-app-feature-fehlgeschlagene-zahlungen.routes';
 
 export * from './lib/sachbearbeitung-app-feature-fehlgeschlagene-zahlungen/sachbearbeitung-app-feature-fehlgeschlagene-zahlungen.component';
