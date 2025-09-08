33bd6487b7cbc8f1e818bb13eff83201b8bb460b
Luca Fondo <luca.fondo@dvbern.ch>
2025-09-08 15:47:16 +0200

    feat(KSTIP-1640): implement new nesko exception handling

--- backend/src/main/java/ch/dvbern/stip/api/nesko/type/NeskoSteuerdatenError.java
+++ backend/src/main/java/ch/dvbern/stip/api/nesko/type/NeskoSteuerdatenError.java
@@ -17,14 +17,13 @@
 
 package ch.dvbern.stip.api.nesko.type;
 
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
+import ch.dvbern.stip.api.common.exception.ExceptionConstants;
+import ch.dvbern.stip.api.common.exception.NeskoInternalException;
+import ch.dvbern.stip.api.common.exception.NeskoNotFoundException;
 import ch.dvbern.stip.api.nesko.generated.stipendienauskunftservice.BusinessFault;
 import io.quarkus.logging.Log;
-import io.quarkus.security.UnauthorizedException;
-import jakarta.ws.rs.BadRequestException;
-import jakarta.ws.rs.InternalServerErrorException;
-import jakarta.ws.rs.NotFoundException;
 import jakarta.xml.ws.WebServiceException;
 import jakarta.xml.ws.soap.SOAPFaultException;
 import lombok.Getter;
@@ -35,44 +34,45 @@ import lombok.SneakyThrows;
 @RequiredArgsConstructor
 public enum NeskoSteuerdatenError {
     INVALID_TOKEN(
-    SOAPFaultException.class, "Invalid access token", e -> new UnauthorizedException("Invalid access token", e)
+    SOAPFaultException.class, "Invalid access token",
+    (e, faultCode) -> new NeskoInternalException(ExceptionConstants.NESKO_INVALID_TOKEN, faultCode)
     ),
     SSVN_NOT_FOUND(
     BusinessFault.class, "Die angefragte Person kann nicht oder nicht eindeutig ermittelt werden",
-    e -> new NotFoundException("Keine Steuerdaten für die angegebene SSVN gefunden", e)
+    (e, faultCode) -> new NeskoNotFoundException(ExceptionConstants.NESKO_SSVN_NOT_FOUND, faultCode)
     ),
     STEUERJAHR_NOT_READY(
     BusinessFault.class,
     "Die Steuerdaten der angefragten Person können für das angefragte Steuerjahr nicht ermittelt werden",
-    e -> new NotFoundException("Keine Steuerdaten für das angegebene steuerjahr gefunden", e)
+    (e, faultCode) -> new NeskoNotFoundException(ExceptionConstants.NESKO_STEUERJAHR_NOT_READY, faultCode)
     ),
     STEUERJAHR_PRESENT_OR_FUTURE(
     BusinessFault.class, "Für die angefragte Person kann in NESKO-VA-NP kein Dossier ermittelt werden",
-    e -> new BadRequestException("Das angegebene Steuerjahr ist nicht valid", e)
+    (e, faultCode) -> new NeskoNotFoundException(ExceptionConstants.NESKO_STEUERJAHR_PRESENT_OR_FUTURE, faultCode)
     ),
     STEUERJAHR_PAST_NOT_FOUND(
     BusinessFault.class, "Für die angefragte Person kann im Register keine Haushaltstruktur ermittelt werden",
-    e -> new NotFoundException("Keine Steuerdaten für das angegebene steuerjahr gefunden", e)
+    (e, faultCode) -> new NeskoNotFoundException(ExceptionConstants.NESKO_STEUERJAHR_PAST_NOT_FOUND, faultCode)
     ),
     STEUERJAHR_NOT_PARSEABLE(
     SOAPFaultException.class, "is not facet-valid with respect to pattern '[2-9][0-9][0-9][0-9]'",
-    e -> new BadRequestException("Das angegebene Steuerjahr ist nicht valid", e)
+    (e, faultCode) -> new NeskoNotFoundException(ExceptionConstants.NESKO_STEUERJAHR_NOT_PARSABLE, faultCode)
     ),
     NESKO_SERVICE_UNAVAILABLE(
     WebServiceException.class, "",
-    e -> new InternalServerErrorException("Das angegebene Steuerjahr ist nicht valid", e)
+    (e, faultCode) -> new NeskoInternalException(ExceptionConstants.NESKO_SERVICE_UNAVAILABLE, faultCode)
     ),
     ;
 
     private final Class<? extends Exception> exceptionClass;
     private final String errorStringNeedle;
-    private final Function<Exception, Exception> exceptionSupplier;
+    private final BiFunction<Exception, String, Exception> exceptionSupplier;
 
     @SneakyThrows
     public static void handleException(Exception e) {
         for (NeskoSteuerdatenError error : NeskoSteuerdatenError.values()) {
             if (e.getClass().equals(error.exceptionClass) && e.getMessage().contains(error.errorStringNeedle)) {
-                throw error.exceptionSupplier.apply(e);
+                throw error.exceptionSupplier.apply(e, ((BusinessFault) e).getFaultInfo().getErrorCode());
             }
         }
         Log.error(e.getMessage(), e);
