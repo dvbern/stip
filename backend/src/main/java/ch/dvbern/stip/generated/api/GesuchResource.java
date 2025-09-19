0077c1922757a8dfdb801fdacff72e595ebbacf2
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2025-09-19 13:27:21 +0200

    feat(KSTIP-2663): remove endpoint changeGesuchStatusToDatenschutzbriefAmGenerieren

--- backend/src/main/java/ch/dvbern/stip/generated/api/GesuchResource.java
+++ backend/src/main/java/ch/dvbern/stip/generated/api/GesuchResource.java
@@ -59,11 +59,6 @@ public interface GesuchResource {
     GesuchWithChangesDto changeGesuchStatusToBereitFuerBearbeitung(@PathParam("gesuchTrancheId") UUID gesuchTrancheId,@Valid KommentarDto kommentarDto);
 
     @POST
-    @Path("/status/datenschutzbrief-generieren/{gesuchTrancheId}")
-    @Produces({ "application/json", "text/plain" })
-    GesuchWithChangesDto changeGesuchStatusToDatenschutzbriefAmGenerieren(@PathParam("gesuchTrancheId") UUID gesuchTrancheId);
-
-    @POST
     @Path("/status/datenschutzbrief-druckbereit/{gesuchTrancheId}")
     @Produces({ "application/json", "text/plain" })
     GesuchWithChangesDto changeGesuchStatusToDatenschutzbriefDruckbereit(@PathParam("gesuchTrancheId") UUID gesuchTrancheId);
