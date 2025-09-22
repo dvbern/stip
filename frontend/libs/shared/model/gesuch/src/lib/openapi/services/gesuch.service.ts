0d98d623b0582e40cb7209b629a9bb3ca105ff7c
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-09-22 15:37:55 +0200

    feat(KSTIP-2663): Genereate Service

--- frontend/libs/shared/model/gesuch/src/lib/openapi/services/gesuch.service.ts
+++ frontend/libs/shared/model/gesuch/src/lib/openapi/services/gesuch.service.ts
@@ -62,10 +62,6 @@ export interface GesuchServiceChangeGesuchStatusToBereitFuerBearbeitungRequestPa
     kommentar?: Kommentar;
 }
 
-export interface GesuchServiceChangeGesuchStatusToDatenschutzbriefAmGenerierenRequestParams {
-    gesuchTrancheId: string;
-}
-
 export interface GesuchServiceChangeGesuchStatusToDatenschutzbriefDruckbereitRequestParams {
     gesuchTrancheId: string;
 }
@@ -543,78 +539,6 @@ export class GesuchService {
             {
                 context: localVarHttpContext,
                 body: kommentar,
-                responseType: <any>responseType_,
-                withCredentials: this.configuration.withCredentials,
-                headers: localVarHeaders,
-                observe: <any>observe,
-                reportProgress: reportProgress
-            }
-        );
-    }
-
-    /**
-     * @param requestParameters
-     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
-     * @param reportProgress flag to report request and response progress.
-     */
-     public changeGesuchStatusToDatenschutzbriefAmGenerieren$(requestParameters: GesuchServiceChangeGesuchStatusToDatenschutzbriefAmGenerierenRequestParams, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/plain', context?: HttpContext}): Observable<GesuchWithChanges>;
-     public changeGesuchStatusToDatenschutzbriefAmGenerieren$(requestParameters: GesuchServiceChangeGesuchStatusToDatenschutzbriefAmGenerierenRequestParams, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/plain', context?: HttpContext}): Observable<HttpResponse<GesuchWithChanges>>;
-     public changeGesuchStatusToDatenschutzbriefAmGenerieren$(requestParameters: GesuchServiceChangeGesuchStatusToDatenschutzbriefAmGenerierenRequestParams, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/plain', context?: HttpContext}): Observable<HttpEvent<GesuchWithChanges>>;
-     public changeGesuchStatusToDatenschutzbriefAmGenerieren$(requestParameters: GesuchServiceChangeGesuchStatusToDatenschutzbriefAmGenerierenRequestParams, observe: 'body' | 'response' | 'events' = 'body', reportProgress = false, options?: {httpHeaderAccept?: 'application/json' | 'text/plain', context?: HttpContext}): Observable<any> {
-        const gesuchTrancheId = requestParameters.gesuchTrancheId;
-        if (gesuchTrancheId === null || gesuchTrancheId === undefined) {
-            throw new Error('Required parameter gesuchTrancheId was null or undefined when calling changeGesuchStatusToDatenschutzbriefAmGenerieren$.');
-        }
-
-        let localVarHeaders = this.defaultHeaders;
-
-        let localVarCredential: string | undefined;
-        // authentication (auth-uat-bern) required
-        localVarCredential = this.configuration.lookupCredential('auth-uat-bern');
-        if (localVarCredential) {
-            // using credentials
-        }
-
-        // authentication (auth-dev-bern) required
-        localVarCredential = this.configuration.lookupCredential('auth-dev-bern');
-        if (localVarCredential) {
-            // using credentials
-        }
-
-        let localVarHttpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
-        if (localVarHttpHeaderAcceptSelected === undefined) {
-            // to determine the Accept header
-            const httpHeaderAccepts: string[] = [
-                'application/json',
-                'text/plain'
-            ];
-            localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
-        }
-        if (localVarHttpHeaderAcceptSelected !== undefined) {
-            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
-        }
-
-        let localVarHttpContext: HttpContext | undefined = options && options.context;
-        if (localVarHttpContext === undefined) {
-            localVarHttpContext = new HttpContext();
-        }
-
-
-        let responseType_: 'text' | 'json' | 'blob' = 'json';
-        if (localVarHttpHeaderAcceptSelected) {
-            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
-                responseType_ = 'text';
-            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
-                responseType_ = 'json';
-            } else {
-                responseType_ = 'blob';
-            }
-        }
-
-        const localVarPath = `/gesuch/status/datenschutzbrief-generieren/${this.configuration.encodeParam({name: "gesuchTrancheId", value: gesuchTrancheId, in: "path", style: "simple", explode: false, dataType: "string", dataFormat: "uuid"})}`;
-        return this.httpClient.request<GesuchWithChanges>('post', `${this.configuration.basePath}${localVarPath}`,
-            {
-                context: localVarHttpContext,
                 responseType: <any>responseType_,
                 withCredentials: this.configuration.withCredentials,
                 headers: localVarHeaders,
