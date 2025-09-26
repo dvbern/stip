320ea7cfacd9fff4f39d132b47642fb07b85def5
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-09-26 13:55:13 +0200

    feat(KSTIP-2294): Improved detai view title loading behavior

--- frontend/libs/sachbearbeitung-app/feature/massendruck/src/lib/components/massendruck-detail.component.ts
+++ frontend/libs/sachbearbeitung-app/feature/massendruck/src/lib/components/massendruck-detail.component.ts
@@ -102,6 +102,7 @@ export class MassendruckDetailComponent {
     });
   }
 
+  // todo: check failure handling when backend is ready
   setMasendruckJobVersendet(item: TableItem) {
     if (item.isVersendet) {
       return;
