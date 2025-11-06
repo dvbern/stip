238f084773ee46be0301c1dfff97ccfb789eb177
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-11-06 11:42:24 +0100

    feat(KSTIP-2845): Created Components with dummy data

--- frontend/libs/sachbearbeitung-app/data-access/infos-gesuchsdokumente/src/lib/sachbearbeitung-app-data-access-infos-gesuchsdokumente.store.ts
+++ frontend/libs/sachbearbeitung-app/data-access/infos-gesuchsdokumente/src/lib/sachbearbeitung-app-data-access-infos-gesuchsdokumente.store.ts
@@ -12,12 +12,47 @@ import {
   initial,
 } from '@dv/shared/util/remote-data';
 
+// Dummy types until the contract is updated
+export interface StipendienDokument {
+  id: string;
+  datum: string;
+  versendetVerfuegung?: string;
+  verfuegungsbrief?: string;
+  berechnungsblaetter?: string;
+  gesetzlichesDarlehen?: string;
+}
+
+export interface DarlehenDokument {
+  id: string;
+  datum: string;
+  kategorie: string;
+  darlehensverfuegung?: string;
+  gesetzlichesDarlehen?: string;
+  freiwilligesDarlehen?: string;
+  kommentar?: string;
+}
+
+export interface DatenschutzbriefDokument {
+  id: string;
+  datum: string;
+  kategorie: string;
+  sachbearbeiter: string;
+  person: string;
+  massendruckJobId?: string;
+}
+
 type InfosAdminState = {
   adminDokumente: CachedRemoteData<AdminDokumente>;
+  stipendienDokumente: CachedRemoteData<StipendienDokument[]>;
+  darlehenDokumente: CachedRemoteData<DarlehenDokument[]>;
+  datenschutzbriefeDokumente: CachedRemoteData<DatenschutzbriefDokument[]>;
 };
 
 const initialState: InfosAdminState = {
   adminDokumente: initial(),
+  stipendienDokumente: initial(),
+  darlehenDokumente: initial(),
+  datenschutzbriefeDokumente: initial(),
 };
 
 @Injectable()
@@ -29,6 +64,18 @@ export class InfosGesuchsdokumenteStore extends signalStore(
 
   adminDokumenteViewSig = computed(() => {
     return fromCachedDataSig(this.adminDokumente);
+  });
+
+  stipendienDokumenteViewSig = computed(() => {
+    return fromCachedDataSig(this.stipendienDokumente);
+  });
+
+  darlehenDokumenteViewSig = computed(() => {
+    return fromCachedDataSig(this.darlehenDokumente);
+  });
+
+  datenschutzbriefeDokumenteViewSig = computed(() => {
+    return fromCachedDataSig(this.datenschutzbriefeDokumente);
   });
 
   loadAdminDokumente$ = rxMethod<{ gesuchId: string }>(
@@ -49,6 +96,88 @@ export class InfosGesuchsdokumenteStore extends signalStore(
             ),
           ),
       ),
+    ),
+  );
+
+  // Dummy implementations for loading document lists
+  // TODO: Replace with actual API calls when contract is updated
+  loadStipendienDokumente$ = rxMethod<{ gesuchId: string }>(
+    pipe(
+      tap(() => {
+        patchState(this, (state) => ({
+          stipendienDokumente: cachedPending(state.stipendienDokumente),
+        }));
+      }),
+      tap(() => {
+        // Dummy data - will be replaced with actual API call
+        const dummyData: StipendienDokument[] = [
+          {
+            id: '1',
+            datum: new Date().toISOString(),
+            versendetVerfuegung: 'verfuegung_1.pdf',
+            verfuegungsbrief: 'brief_1.pdf',
+            berechnungsblaetter: 'berechnung_1.pdf',
+            gesetzlichesDarlehen: 'darlehen_1.pdf',
+          },
+        ];
+        patchState(this, {
+          stipendienDokumente: { data: dummyData, type: 'success' },
+        });
+      }),
+    ),
+  );
+
+  loadDarlehenDokumente$ = rxMethod<{ gesuchId: string }>(
+    pipe(
+      tap(() => {
+        patchState(this, (state) => ({
+          darlehenDokumente: cachedPending(state.darlehenDokumente),
+        }));
+      }),
+      tap(() => {
+        // Dummy data - will be replaced with actual API call
+        const dummyData: DarlehenDokument[] = [
+          {
+            id: '1',
+            datum: new Date().toISOString(),
+            kategorie: 'Gesetzlich',
+            darlehensverfuegung: 'darlehen_verfuegung_1.pdf',
+            gesetzlichesDarlehen: 'gesetzlich_1.pdf',
+            kommentar: 'Test Kommentar',
+          },
+        ];
+        patchState(this, {
+          darlehenDokumente: { data: dummyData, type: 'success' },
+        });
+      }),
+    ),
+  );
+
+  loadDatenschutzbriefeDokumente$ = rxMethod<{ gesuchId: string }>(
+    pipe(
+      tap(() => {
+        patchState(this, (state) => ({
+          datenschutzbriefeDokumente: cachedPending(
+            state.datenschutzbriefeDokumente,
+          ),
+        }));
+      }),
+      tap(() => {
+        // Dummy data - will be replaced with actual API call
+        const dummyData: DatenschutzbriefDokument[] = [
+          {
+            id: '1',
+            datum: new Date().toISOString(),
+            kategorie: 'Standard',
+            sachbearbeiter: 'Max Mustermann',
+            person: 'Anna Schmidt',
+            massendruckJobId: 'job-123',
+          },
+        ];
+        patchState(this, {
+          datenschutzbriefeDokumente: { data: dummyData, type: 'success' },
+        });
+      }),
     ),
   );
 }
---
+++ frontend/libs/sachbearbeitung-app/feature/infos-gesuchsdokumente/src/lib/components/stipenedien-dokumente/stipendien-dokumente.component.ts
@@ -0,0 +1,89 @@
+import { DatePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { TranslocoPipe } from '@jsverse/transloco';
+
+import { InfosGesuchsdokumenteStore } from '@dv/sachbearbeitung-app/data-access/infos-gesuchsdokumente';
+import { SharedUiLoadingComponent } from '@dv/shared/ui/loading';
+import { TypeSafeMatCellDefDirective } from '@dv/shared/ui/table-helper';
+import { paginatorTranslationProvider } from '@dv/shared/util/paginator-translation';
+import { isPending } from '@dv/shared/util/remote-data';
+
+const PAGE_SIZE = 10;
+
+@Component({
+  selector: 'dv-stipendien-dokumente',
+  standalone: true,
+  imports: [
+    DatePipe,
+    TranslocoPipe,
+    MatTableModule,
+    MatPaginatorModule,
+    TypeSafeMatCellDefDirective,
+    SharedUiLoadingComponent,
+  ],
+  providers: [paginatorTranslationProvider()],
+  templateUrl: './stipendien-dokumente.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StipendienDokumenteComponent {
+  private infosStore = inject(InfosGesuchsdokumenteStore);
+
+  // eslint-disable-next-line @angular-eslint/no-input-rename
+  gesuchId = input.required<string>({ alias: 'id' });
+
+  displayedColumns = [
+    'datum',
+    'versendetVerfuegung',
+    'verfuegungsbrief',
+    'berechnungsblaetter',
+    'gesetzlichesDarlehen',
+  ];
+
+  pageSig = signal(0);
+  pageSizeSig = signal(PAGE_SIZE);
+
+  viewSig = computed(() => {
+    const allDokumente = this.infosStore.stipendienDokumenteViewSig() ?? [];
+    const loading = isPending(this.infosStore.stipendienDokumente());
+
+    return {
+      loading,
+      allDokumente,
+      totalEntries: allDokumente.length,
+    };
+  });
+
+  paginatedDokumenteSig = computed(() => {
+    const { allDokumente } = this.viewSig();
+    const page = this.pageSig();
+    const pageSize = this.pageSizeSig();
+    const startIndex = page * pageSize;
+    const endIndex = startIndex + pageSize;
+
+    return allDokumente.slice(startIndex, endIndex);
+  });
+
+  constructor() {
+    effect(() => {
+      const gesuchId = this.gesuchId();
+      if (gesuchId) {
+        this.infosStore.loadStipendienDokumente$({ gesuchId });
+      }
+    });
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageSig.set(event.pageIndex);
+    this.pageSizeSig.set(event.pageSize);
+  }
+}
