e00a8624418b870fe634f290932a260fbedba8e0
Philipp Schärer <philipp.schaerer@dvbern.ch>
2025-11-10 16:58:36 +0100

    fix(KSTIP-2897): GesuchEinreichenUniqueSVNummerTest
    It was not possible to run it standalone

--- backend/src/test/java/ch/dvbern/stip/api/gesuch/entity/GesuchEinreichenUniqueSVNummerTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuch/entity/GesuchEinreichenUniqueSVNummerTest.java
@@ -28,6 +28,7 @@ import ch.dvbern.stip.api.util.RequestSpecUtil;
 import ch.dvbern.stip.api.util.TestDatabaseEnvironment;
 import ch.dvbern.stip.api.util.TestUtil;
 import ch.dvbern.stip.generated.api.AusbildungApiSpec;
+import ch.dvbern.stip.generated.api.AuszahlungApiSpec;
 import ch.dvbern.stip.generated.api.DokumentApiSpec;
 import ch.dvbern.stip.generated.api.FallApiSpec;
 import ch.dvbern.stip.generated.api.GesuchApiSpec;
@@ -54,6 +55,7 @@ import static org.hamcrest.Matchers.is;
 public class GesuchEinreichenUniqueSVNummerTest {
     private final GesuchApiSpec gesuchApiSpec = GesuchApiSpec.gesuch(RequestSpecUtil.quarkusSpec());
     private final AusbildungApiSpec ausbildungApiSpec = AusbildungApiSpec.ausbildung(RequestSpecUtil.quarkusSpec());
+    private final AuszahlungApiSpec auszahlungApiSpec = AuszahlungApiSpec.auszahlung(RequestSpecUtil.quarkusSpec());
     private final DokumentApiSpec dokumentApiSpec = DokumentApiSpec.dokument(RequestSpecUtil.quarkusSpec());
     private final FallApiSpec fallApiSpec = FallApiSpec.fall(RequestSpecUtil.quarkusSpec());
 
@@ -111,7 +113,7 @@ public class GesuchEinreichenUniqueSVNummerTest {
     private GesuchDtoSpec createFullGesuch() {
         final var gesuch = TestUtil.createGesuchAusbildungFall(fallApiSpec, ausbildungApiSpec, gesuchApiSpec);
         gesucheToDelete.add(gesuch.getId());
-        TestUtil.fillGesuch(gesuchApiSpec, dokumentApiSpec, gesuch);
+        TestUtil.fillGesuchWithAuszahlung(gesuchApiSpec, dokumentApiSpec, auszahlungApiSpec, gesuch);
 
         return gesuch;
     }
