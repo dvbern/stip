477cfa50dd6dcbf251ab9715c68729b831499f46
Luca Fondo <luca.fondo@dvbern.ch>
2025-12-04 11:46:35 +0100

    fix(KSTIP-2696): add required roles allowed to endpoints

--- backend/src/main/java/ch/dvbern/stip/api/darlehen/resource/DarlehenResourceImpl.java
+++ backend/src/main/java/ch/dvbern/stip/api/darlehen/resource/DarlehenResourceImpl.java
@@ -34,10 +34,17 @@ import ch.dvbern.stip.generated.dto.DarlehenUpdateGsDto;
 import ch.dvbern.stip.generated.dto.DarlehenUpdateSbDto;
 import ch.dvbern.stip.generated.dto.PaginatedSbDarlehenDashboardDto;
 import io.smallrye.mutiny.Uni;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import org.jboss.resteasy.reactive.multipart.FileUpload;
+
+import static ch.dvbern.stip.api.common.util.OidcPermissions.FREIGABESTELLE_GESUCH_UPDATE;
+import static ch.dvbern.stip.api.common.util.OidcPermissions.GS_GESUCH_CREATE;
+import static ch.dvbern.stip.api.common.util.OidcPermissions.GS_GESUCH_READ;
+import static ch.dvbern.stip.api.common.util.OidcPermissions.GS_GESUCH_UPDATE;
+import static ch.dvbern.stip.api.common.util.OidcPermissions.SB_GESUCH_UPDATE;
 
 @RequestScoped
 @RequiredArgsConstructor
@@ -54,54 +61,63 @@ public class DarlehenResourceImpl implements DarlehenResource {
     }
 
     @Override
+    @RolesAllowed(GS_GESUCH_READ)
     public DarlehenDto createDarlehen(UUID fallId) {
         darlehenAuthorizer.canCreateDarlehen(fallId);
         return darlehenService.createDarlehen(fallId);
     }
 
     @Override
+    @RolesAllowed({SB_GESUCH_UPDATE, FREIGABESTELLE_GESUCH_UPDATE})
     public DarlehenDto darlehenAblehen(UUID darlehenId) {
         darlehenAuthorizer.canDarlehenAblehenen(darlehenId);
         return darlehenService.darlehenAblehen(darlehenId);
     }
 
     @Override
+    @RolesAllowed({SB_GESUCH_UPDATE, FREIGABESTELLE_GESUCH_UPDATE})
     public DarlehenDto darlehenAkzeptieren(UUID darlehenId) {
         darlehenAuthorizer.canDarlehenAkzeptieren(darlehenId);
         return darlehenService.darlehenAkzeptieren(darlehenId);
     }
 
     @Override
+    @RolesAllowed(GS_GESUCH_UPDATE)
     public DarlehenDto darlehenEingeben(UUID darlehenId) {
         darlehenAuthorizer.canDarlehenEingeben(darlehenId);
         return darlehenService.darlehenEingeben(darlehenId);
     }
 
     @Override
+    @RolesAllowed(SB_GESUCH_UPDATE)
     public DarlehenDto darlehenFreigeben(UUID darlehenId) {
         darlehenAuthorizer.canDarlehenFreigeben(darlehenId);
         return darlehenService.darlehenFreigeben(darlehenId);
     }
 
     @Override
+    @RolesAllowed(SB_GESUCH_UPDATE)
     public DarlehenDto darlehenZurueckweisen(UUID darlehenId) {
         darlehenAuthorizer.canDarlehenZurueckweisen(darlehenId);
         return darlehenService.darlehenZurueckweisen(darlehenId);
     }
 
     @Override
+    @RolesAllowed(GS_GESUCH_UPDATE)
     public DarlehenDto darlehenUpdateGs(UUID darlehenId, DarlehenUpdateGsDto darlehenUpdateGsDto) {
         darlehenAuthorizer.canDarlehenUpdateGs(darlehenId);
         return darlehenService.darlehenUpdateGs(darlehenId, darlehenUpdateGsDto);
     }
 
     @Override
+    @RolesAllowed(SB_GESUCH_UPDATE)
     public DarlehenDto darlehenUpdateSb(UUID darlehenId, DarlehenUpdateSbDto darlehenUpdateSbDto) {
         darlehenAuthorizer.canDarlehenUpdateSb(darlehenId);
         return darlehenService.darlehenUpdateSb(darlehenId, darlehenUpdateSbDto);
     }
 
     @Override
+    @RolesAllowed(GS_GESUCH_CREATE)
     public Uni<Response> createDarlehenDokument(
         UUID darlehenId,
         DarlehenDokumentType dokumentTyp,
@@ -112,6 +128,7 @@ public class DarlehenResourceImpl implements DarlehenResource {
     }
 
     @Override
+    @RolesAllowed(GS_GESUCH_READ)
     public DarlehenDokumentDto getDarlehenDokument(UUID darlehenId, DarlehenDokumentType dokumentTyp) {
         darlehenAuthorizer.canGetDarlehenDokument();
         return darlehenService.getDarlehenDokument(darlehenId, dokumentTyp);
