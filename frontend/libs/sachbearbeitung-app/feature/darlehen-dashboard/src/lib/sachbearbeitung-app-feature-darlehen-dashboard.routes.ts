968268d6654d47243af098170887863f0d8d7736
Severin Spoerri <severin.spoerri@dvbern.ch>
2025-12-19 10:00:15 +0100

    refactor(KSTIP-2696): clean up

--- frontend/libs/sachbearbeitung-app/feature/darlehen-dashboard/src/lib/sachbearbeitung-app-feature-darlehen-dashboard.routes.ts
+++ frontend/libs/sachbearbeitung-app/feature/darlehen-dashboard/src/lib/sachbearbeitung-app-feature-darlehen-dashboard.routes.ts
@@ -6,19 +6,11 @@ export const sachbearbeitungAppFeatureDarlehenDashboardRoutes: Route[] = [
   {
     path: '',
     pathMatch: 'prefix',
-    providers: [
-      // feature specific services and other providers
-      // always remove { providedIn: 'root' } from the feature specific services
-    ],
     children: [
       {
         path: '',
         component: SachbearbeitungAppFeatureDarlehenDashboardComponent,
-        // title: 'sachbearbeitung-app.darlehen-dashboard.title',
       },
-      // add more routes here (siblings)
-      // it is also possible to add nested routes as children
-      // of this feature root component (or even lazy loaded sub features)
     ],
   },
 ];
