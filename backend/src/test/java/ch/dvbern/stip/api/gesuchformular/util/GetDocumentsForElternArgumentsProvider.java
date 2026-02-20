3d77b37744f6306afbeeadafdeb0bd9143d7f6ff
Philipp Schärer <philipp.schaerer@dvbern.ch>
2026-02-20 11:40:36 +0100

    fix(KSTIP-3113): spottless apply

--- backend/src/test/java/ch/dvbern/stip/api/gesuchformular/util/GetDocumentsForElternArgumentsProvider.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuchformular/util/GetDocumentsForElternArgumentsProvider.java
@@ -28,7 +28,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
-public class  GetDocumentsForElternArgumentsProvider implements ArgumentsProvider {
+public class GetDocumentsForElternArgumentsProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
         return StreamUtil.concat(
