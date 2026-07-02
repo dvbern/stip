1406e1ee05bd8eebde5656caf05557609a0a7150
Philipp Schärer <philipp.schaerer@dvbern.ch>
2026-07-02 09:19:47 +0200

    fix(KSTIP-3639): fixed test where bearbeitungAbschliessen was missing

--- backend/src/test/java/ch/dvbern/stip/api/gesuchtranche/resource/GesuchVerfuegtSeveralInitialTranchenTest.java
+++ backend/src/test/java/ch/dvbern/stip/api/gesuchtranche/resource/GesuchVerfuegtSeveralInitialTranchenTest.java
@@ -43,6 +43,7 @@ import ch.dvbern.stip.generated.dto.UnterschriftenblattDokumentTypDtoSpec;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
@@ -152,6 +153,13 @@ class GesuchVerfuegtSeveralInitialTranchenTest {
             gesuch.getGesuchTrancheToWorkWith().getId(),
             newTranche.getId()
         );
+
+        gesuchApiSpec.bearbeitungAbschliessen()
+            .gesuchTrancheIdPath(gesuch.getGesuchTrancheToWorkWith().getId())
+            .execute(TestUtil.PEEK_IF_ENV_SET)
+            .then()
+            .assertThat()
+            .statusCode(Status.OK.getStatusCode());
     }
 
     @TestAsFreigabestelle
