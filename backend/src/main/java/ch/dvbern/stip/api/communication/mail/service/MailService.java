94d09bb85acbf2cea01a52f6b1d27db662b68026
Luca Fondo <luca.fondo@dvbern.ch>
2026-08-13 10:45:56 +0200

    fix(KSTIP-3686): update ticeket number in todo

--- backend/src/main/java/ch/dvbern/stip/api/communication/mail/service/MailService.java
+++ backend/src/main/java/ch/dvbern/stip/api/communication/mail/service/MailService.java
@@ -105,7 +105,7 @@ public class MailService {
 
         mailAlreadySentCheckerService.sentStandardNotification();
 
-        // TODO(KSTIP-3686): implement link
+        // TODO(KSTIP-3738): implement link
         Templates.getStandardNotification(nachname, vorname, "", language)
             .to(recipients.toArray(String[]::new))
             .subject(TLProducer.defaultBundle().forAppLanguage(language).translate("stip.standard.notification"))
