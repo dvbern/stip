64d874605a708149b62f5f4d0bada9f1bb9e8852
Severin Spoerri <severin.spoerri@dvbern.ch>
2026-08-20 13:41:11 +0200

    feat(KSTIP-3837): Versionen Menu Refactoring

--- frontend/libs/shared/ui/versionen-menu/src/lib/shared-ui-versionen-menu/shared-ui-versionen-menu.component.ts
+++ frontend/libs/shared/ui/versionen-menu/src/lib/shared-ui-versionen-menu/shared-ui-versionen-menu.component.ts
@@ -64,181 +64,98 @@ export class SharedUiVersionenMenuComponent {
     const versionen = this.versionenSig();
     const berechnungId = this.berechnungIdSig();
 
-    if (!versionen || versionen.length === 0) {
-      return undefined;
-    }
-
-    const version = versionen.find(
-      (version) => version.berechnungId === berechnungId,
-    );
-
-    return version;
+    return versionen.find((version) => version.berechnungId === berechnungId);
   });
 
-  private defaultLinkSig = computed((): VersionenLink => {
-    const gesuchId = this.gesuchIdSig();
-    const tabSegments = this.tabRouteSegmentsSig();
-    const originStep = this.originStepSig();
-    const currentVersion = this.currentVersionSig();
+  private selectedMenuLinkSig = signal<VersionenLink | null>(null);
+
+  effectiveLinkSig = computed(
+    () => this.selectedMenuLinkSig() ?? this.defaultLinkSig(),
+  );
+
+  linksSig = computed(() => {
+    const versionen = this.versionenSig();
     const initial = this.initialSig();
     const firstTrancheId = this.firstCurrentTrancheIdSig();
+    const vg = initial?.verfuegtGesuch;
+    const eg = initial?.eingereichtGesuch;
 
-    if (this.isInitialRouteSig() && initial?.verfuegtGesuch) {
-      const vg = initial.verfuegtGesuch;
-      return {
-        routerLink: [
-          '/',
-          'gesuch',
-          ...tabSegments,
-          gesuchId,
-          'initial',
-          vg.tranchen[0].id,
-        ],
-        queryParams: {
-          berechnungId: vg.berechnungId,
-          revision: vg.tranchen[0].revision,
-          originStep,
-        },
-        labelKey: 'verfuegt',
-      };
-    }
-    if (this.isEingereichtRouteSig() && initial?.eingereichtGesuch) {
-      const eg = initial.eingereichtGesuch;
-      return {
-        routerLink: [
-          '/',
-          'gesuch',
-          ...tabSegments,
-          gesuchId,
-          'eingereicht',
-          eg.id,
-        ],
-        queryParams: { revision: eg.revision, originStep },
-        labelKey: 'eingereicht',
-      };
-    }
-    if (currentVersion) {
-      return {
-        routerLink: [
-          '/',
-          'gesuch',
-          ...tabSegments,
-          gesuchId,
-          'tranche',
-          currentVersion.tranchen[0].id,
-        ],
-        queryParams: {
-          berechnungId: currentVersion.berechnungId,
-          revision: currentVersion.tranchen[0].revision,
-          originStep,
-        },
-        labelKey: 'versionen',
-        labelTimestamp: currentVersion.timestamp,
-      };
-    }
     return {
-      routerLink: [
-        '/',
-        'gesuch',
-        ...tabSegments,
-        gesuchId,
-        'tranche',
-        firstTrancheId,
-      ],
-      queryParams: { originStep },
-      labelKey: 'aktuell',
+      aktuell: this.buildLink('tranche', firstTrancheId, 'aktuell'),
+      versionen: versionen.map((version) => ({
+        version,
+        link: this.buildLink('tranche', version.tranchen[0].id, 'versionen', {
+          berechnungId: version.berechnungId,
+          revision: version.tranchen[0].revision,
+          labelTimestamp: version.timestamp,
+        }),
+      })),
+      verfuegt: vg
+        ? this.buildLink('initial', vg.tranchen[0].id, 'verfuegt', {
+            berechnungId: vg.berechnungId,
+            revision: vg.tranchen[0].revision,
+          })
+        : undefined,
+      eingereicht: eg
+        ? this.buildLink('eingereicht', eg.id, 'eingereicht', {
+            revision: eg.revision,
+          })
+        : undefined,
     };
   });
 
-  selectedMenuLinkSig = signal<VersionenLink | null>(null);
+  private defaultLinkSig = computed((): VersionenLink => {
+    const links = this.linksSig();
+    const currentVersion = this.currentVersionSig();
 
-  effectiveLinkSig = computed(() => {
-    return this.selectedMenuLinkSig() ?? this.defaultLinkSig();
+    if (this.isInitialRouteSig() && links.verfuegt) {
+      return links.verfuegt;
+    }
+    if (this.isEingereichtRouteSig() && links.eingereicht) {
+      return links.eingereicht;
+    }
+    if (currentVersion) {
+      const match = links.versionen.find(
+        (item) => item.version.berechnungId === currentVersion.berechnungId,
+      );
+      if (match) {
+        return match.link;
+      }
+    }
+    return links.aktuell;
   });
 
-  setAktuellLink(): void {
-    const gesuchId = this.gesuchIdSig();
-    const tabSegments = this.tabRouteSegmentsSig();
-    const originStep = this.originStepSig();
-    const firstTrancheId = this.firstCurrentTrancheIdSig();
-    this.selectedMenuLinkSig.set({
-      routerLink: [
-        '/',
-        'gesuch',
-        ...tabSegments,
-        gesuchId,
-        'tranche',
-        firstTrancheId,
-      ],
-      queryParams: { originStep },
-      labelKey: 'aktuell',
-    });
+  select(link: VersionenLink): void {
+    this.selectedMenuLinkSig.set(link);
   }
 
-  setVersionLink(version: VerfuegtGesuch): void {
+  private buildLink(
+    kind: 'tranche' | 'initial' | 'eingereicht',
+    trancheId: string | undefined,
+    labelKey: VersionenLink['labelKey'],
+    context?: {
+      berechnungId?: string;
+      revision?: number;
+      labelTimestamp?: string;
+    },
+  ): VersionenLink {
     const gesuchId = this.gesuchIdSig();
     const tabSegments = this.tabRouteSegmentsSig();
     const originStep = this.originStepSig();
-    this.selectedMenuLinkSig.set({
-      routerLink: [
-        '/',
-        'gesuch',
-        ...tabSegments,
-        gesuchId,
-        'tranche',
-        version.tranchen[0].id,
-      ],
+
+    return {
+      routerLink: ['/', 'gesuch', ...tabSegments, gesuchId, kind, trancheId],
       queryParams: {
-        berechnungId: version.berechnungId,
-        revision: version.tranchen[0].revision,
+        ...(context?.berechnungId
+          ? { berechnungId: context.berechnungId }
+          : {}),
+        ...(context?.revision !== undefined
+          ? { revision: context.revision }
+          : {}),
         originStep,
       },
-      labelKey: 'versionen',
-      labelTimestamp: version.timestamp,
-    });
-  }
-
-  setVerfuegtLink(): void {
-    const vg = this.initialSig()?.verfuegtGesuch;
-    if (!vg) return;
-    const gesuchId = this.gesuchIdSig();
-    const tabSegments = this.tabRouteSegmentsSig();
-    const originStep = this.originStepSig();
-    this.selectedMenuLinkSig.set({
-      routerLink: [
-        '/',
-        'gesuch',
-        ...tabSegments,
-        gesuchId,
-        'initial',
-        vg.tranchen[0].id,
-      ],
-      queryParams: {
-        berechnungId: vg.berechnungId,
-        revision: vg.tranchen[0].revision,
-        originStep,
-      },
-      labelKey: 'verfuegt',
-    });
-  }
-
-  setEingereichtLink(): void {
-    const eg = this.initialSig()?.eingereichtGesuch;
-    if (!eg) return;
-    const gesuchId = this.gesuchIdSig();
-    const tabSegments = this.tabRouteSegmentsSig();
-    const originStep = this.originStepSig();
-    this.selectedMenuLinkSig.set({
-      routerLink: [
-        '/',
-        'gesuch',
-        ...tabSegments,
-        gesuchId,
-        'eingereicht',
-        eg.id,
-      ],
-      queryParams: { revision: eg.revision, originStep },
-      labelKey: 'eingereicht',
-    });
+      labelKey,
+      labelTimestamp: context?.labelTimestamp,
+    };
   }
 }
