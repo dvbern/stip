57061fcd6046f538fd63f52a4d5b88742fb07630
Raphael Gosteli <raphael.gosteli@dvbern.ch>
2026-09-07 12:58:18 +0200

    feat(KSTIP-3889): continue navigation if user could not be loaded because we are not authenticated

--- frontend/libs/shared/pattern/global-guards/src/index.ts
+++ frontend/libs/shared/pattern/global-guards/src/index.ts
@@ -1,14 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { from, map } from 'rxjs';
 
-export const hasBenutzer: CanActivateFn = () => {
+export const hasBenutzer: CanActivateFn = (_route, state) => {
   const oauthService = inject(OAuthService);
   if (oauthService.hasValidAccessToken()) {
     return true;
-  } else {
-    oauthService.initLoginFlow();
-    return from(oauthService.tryLoginCodeFlow()).pipe(map(() => false));
   }
+  oauthService.initLoginFlow(state.url);
+  return false;
 };
