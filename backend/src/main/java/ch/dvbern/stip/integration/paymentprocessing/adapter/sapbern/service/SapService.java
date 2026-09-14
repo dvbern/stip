c912e4f69773ea6500c921fbb022bf783f17bd7f
Fabrice Jakob <fabrice.jakob@dvbern.ch>
2026-09-14 16:10:41 +0200

    feat(KSTIP-3934): logic fixed?

--- backend/src/main/java/ch/dvbern/stip/integration/paymentprocessing/adapter/sapbern/service/SapService.java
+++ backend/src/main/java/ch/dvbern/stip/integration/paymentprocessing/adapter/sapbern/service/SapService.java
@@ -489,9 +489,9 @@ public class SapService {
             final var lastBuchhaltungEntry =
                 buchhaltungService.getLatestNotFailedBuchhaltungEntry(gesuch.getAusbildung().getFall().getId());
 
-            var auszahlungsBetrag = relevantStipendienBuchhaltung.getSaldo() / 2;
+            var auszahlungsBetrag = relevantStipendienBuchhaltung.getBetrag() / 2;
             if (isPastSecondPaymentDate(gesuch)) {
-                auszahlungsBetrag = relevantStipendienBuchhaltung.getSaldo();
+                auszahlungsBetrag = relevantStipendienBuchhaltung.getBetrag();
             }
 
             auszahlungsBetrag = Integer.min(auszahlungsBetrag, lastBuchhaltungEntry.getSaldo());
